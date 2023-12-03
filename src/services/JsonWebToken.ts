4925d74cd3424721c177c21c891970078dacd34d
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-12-03 03:23:25 -0500

    Add JsonWebToken Services Sign Method

---
+++ src/services/JsonWebToken.ts
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+import { JWT_ACCESS_TOKEN, JWT_REFRESH_TOKEN } from "../const/env";
+
+export default class JsonWebToken {
+  sign(payload: { name: string; email: string; id: string }): {
+    accessToken: string;
+    refreshToken: string;
+  } {
+    const accessToken: string = jwt.sign(payload, JWT_ACCESS_TOKEN as string, {
+      expiresIn: "120s",
+    });
+
+    const refreshToken: string = jwt.sign(
+      payload,
+      JWT_REFRESH_TOKEN as string,
+      {
+        expiresIn: "1d",
+      }
+    );
+
+    return { accessToken, refreshToken };
+  }
+}
