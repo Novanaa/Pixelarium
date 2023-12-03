2cdd42ef2fbc1fb403b9461a24ca5c3cb14227f5
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-12-03 02:39:58 -0500

    Delete supabase client

--- src/libs/configs/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-import { SUPABASE_URL, SUPABASE_ANON_KEY } from "../../const/env";
-
-const client = createClient(String(SUPABASE_URL), String(SUPABASE_ANON_KEY));
-
-export default client;
