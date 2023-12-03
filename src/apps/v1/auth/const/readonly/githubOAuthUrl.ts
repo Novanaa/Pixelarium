6c7880a2409ccbb955c2704a261e7486a825e826
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-12-03 05:57:59 -0500

    Add constant file for github OAuth url

---
+++ src/apps/v1/auth/const/readonly/githubOAuthUrl.ts
@@ -0,0 +1,3 @@
+import { CLIENT_URL, GITHUB_CLIENT_ID } from "../../../../../const/env";
+
+export const OAuthGithubUrl: string = `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&redirect_uri=${CLIENT_URL}/v1/auth/github/callback&allow_signup=true&scope=user%20user:email`;
