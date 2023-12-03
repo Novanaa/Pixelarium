1cfc2979e6d7a3bb395678c08b435427c9519507
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-12-03 12:54:31 -0500

    Add Url Redirect for Bitbucket OAuth Apps

--- src/apps/v1/auth/routes/auth.routes.ts
+++ src/apps/v1/auth/routes/auth.routes.ts
@@ -8,10 +8,12 @@ import {
   redirectGithubLogin,
   loginWithGithub,
 } from "../controllers/github.auth.controller";
+import { redirectBitbucketLogin } from "../controllers/bitbucket.auth.controller";
 
 router.get("/google", redirectGoogleLogin);
 router.get("/google/callback", loginWithGoogle);
 router.get("/github", redirectGithubLogin);
 router.get("/github/callback", loginWithGithub);
+router.get("/bitbucket", redirectBitbucketLogin);
 
 export default router;
