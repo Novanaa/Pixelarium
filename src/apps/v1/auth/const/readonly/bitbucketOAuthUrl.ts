ad3c6fc6a713049b4fb2fd75d074ff0750076920
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-12-03 19:49:46 -0500

    Add constant url for Bitbucket OAuth Url

--- src/apps/v1/auth/const/readonly/bitbucketOAuthUrl.ts
+++ src/apps/v1/auth/const/readonly/bitbucketOAuthUrl.ts
@@ -1,3 +1,5 @@
 import { BITBUCKET_CLIENT_ID } from "../../../../../const/env";
 
 export const OAuthBitbucketUrl: string = `https://bitbucket.org/site/oauth2/authorize?client_id=${BITBUCKET_CLIENT_ID}&response_type=code`;
+
+export const OAuthBitbucketAccessTokenUrl: string = `https://bitbucket.org/site/oauth2/access_token`;
