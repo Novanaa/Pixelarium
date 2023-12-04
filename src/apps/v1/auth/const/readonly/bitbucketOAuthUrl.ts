313d3f833fab7e7e7cb4e80136057c98bdd911dc
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-12-04 05:55:46 -0500

    Cancel Login with Bitbucket Provider Feature

--- src/apps/v1/auth/const/readonly/bitbucketOAuthUrl.ts
+++
@@ -1,5 +0,0 @@
-import { BITBUCKET_CLIENT_ID } from "../../../../../const/env";
-
-export const OAuthBitbucketUrl: string = `https://bitbucket.org/site/oauth2/authorize?client_id=${BITBUCKET_CLIENT_ID}&response_type=code`;
-
-export const OAuthBitbucketAccessTokenUrl: string = `https://bitbucket.org/site/oauth2/access_token`;
--- src/apps/v1/auth/routes/auth.routes.ts
+++ src/apps/v1/auth/routes/auth.routes.ts
@@ -8,12 +8,10 @@ import {
   redirectGithubLogin,
   loginWithGithub,
 } from "../controllers/github.auth.controller";
-import { redirectBitbucketLogin } from "../controllers/bitbucket.auth.controller";
 
 router.get("/google", redirectGoogleLogin);
 router.get("/google/callback", loginWithGoogle);
 router.get("/github", redirectGithubLogin);
 router.get("/github/callback", loginWithGithub);
-router.get("/bitbucket", redirectBitbucketLogin);
 
 export default router;
