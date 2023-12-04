6504b61a6a89bea65020c92d445092668591ad1b
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-12-04 06:03:08 -0500

    Add promt query params url to OAuth Github Url

--- src/apps/v1/auth/const/readonly/githubOAuthUrl.ts
+++ src/apps/v1/auth/const/readonly/githubOAuthUrl.ts
@@ -4,6 +4,6 @@ import {
   GITHUB_CLIENT_SECRET,
 } from "../../../../../const/env";
 
-export const OAuthGithubUrl: string = `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&redirect_uri=${CLIENT_URL}/v1/auth/github/callback&allow_signup=true&scope=user%20user:email`;
+export const OAuthGithubUrl: string = `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&redirect_uri=${CLIENT_URL}/v1/auth/github/callback&allow_signup=true&scope=user%20user:email&prompt=consent`;
 
 export const OAuthGithubAccessTokenUrl: string = `https://github.com/login/oauth/access_token?client_id=${GITHUB_CLIENT_ID}&client_secret=${GITHUB_CLIENT_SECRET}&redirect_uri=${CLIENT_URL}/v1/auth/github/callback`;
