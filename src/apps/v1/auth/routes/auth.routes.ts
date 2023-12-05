c32e3083a4d63d56b92d7fbc9e25548b2aa119ca
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-12-05 05:02:55 -0500

    Add refresh token rotation

--- src/apps/v1/auth/routes/auth.routes.ts
+++ src/apps/v1/auth/routes/auth.routes.ts
@@ -9,11 +9,13 @@ import {
   loginWithGithub,
 } from "../controllers/github.auth.controller";
 import logout from "../controllers/auth.logout.controller";
+import tokenRotation from "../controllers/auth.token.controller";
 
 router.get("/google", redirectGoogleLogin);
 router.get("/google/callback", loginWithGoogle);
 router.get("/github", redirectGithubLogin);
 router.get("/github/callback", loginWithGithub);
 router.post("/logout", logout);
+router.get("/token", tokenRotation);
 
 export default router;
