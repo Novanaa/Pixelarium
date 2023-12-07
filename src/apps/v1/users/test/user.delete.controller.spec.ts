9c1886745e84c111c02880545d489b283396ba8b
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-12-07 17:29:51 -0500

    Add new feature: Delete User API Endpoint

---
+++ src/apps/v1/users/test/user.delete.controller.spec.ts
@@ -0,0 +1,29 @@
+import { expect, test, describe } from "bun:test";
+import app from "../../../../main";
+import supertest from "supertest";
+import client from "../../../../libs/configs/prisma";
+
+describe("Unit-Testing Delete User API Endpoint", () => {
+  test("should be return 400 status code if the request id params is not numberic", async () => {
+    const request = await supertest(app).delete("/v1/users/test");
+
+    expect(request.status).toBe(400);
+    expect(request.body.status).toBe("KO");
+  });
+  test("should be return 200 status code", async () => {
+    const user = await client.user.findFirst();
+    const request = await supertest(app).delete(`/v1/users/${user?.id}`);
+
+    expect(request.status).toBe(200);
+    expect(request.body.status).toBe("OK");
+  });
+  test("Make sure it can accept application/json", async () => {
+    const user = await client.user.findFirst();
+    const request = await supertest(app)
+      .delete(`/v1/users/${user?.id}`)
+      .set("Content-Type", "application/json");
+
+    expect(request.status).toBe(200);
+    expect(request.body.status).toBe("OK");
+  });
+});
