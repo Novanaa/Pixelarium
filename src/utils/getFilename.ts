7b99aacd50907fa0dc6402cdff1cb12128103160
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-12-07 17:26:47 -0500

    Add utils for get filename

--- src/utils/getFilename.ts
+++ src/utils/getFilename.ts
@@ -1,14 +1,8 @@
 import path from "path";
 import logger from "../libs/configs/logger";
-import FilesSystem from "../services/FilesSystem";
 
 export default function getFilename(pathname: string): string | null {
-  const filesSystem = new FilesSystem();
   try {
-    const isFilepathExist = filesSystem.isExist(pathname);
-
-    if (!isFilepathExist) return null;
-
     const fileName: string = path.basename(pathname);
 
     return fileName;
