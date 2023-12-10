c7579400b418963d5c28f2de17b38b5a3aab3b0a
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-12-10 06:19:04 -0500

    Add unit-testing for getLatestUserProfilePicture users services

---
+++ src/apps/v1/users/test/getLatestUserProfilePicture.spec.ts
@@ -0,0 +1,21 @@
+import { expect, test, describe } from "bun:test";
+import getLatestUserProfilePicture from "../services/getLatestUserProfilePicture";
+import client from "../../../../libs/configs/prisma";
+import { User } from "../../../../../generated/client";
+
+describe("unit-Testing Get Latest User Profile Picture Utils", () => {
+  test("returned value data types must be string", async () => {
+    const user: Awaited<User | null> = await client.user.findFirst();
+    const latestUserProfilePicture: Awaited<string | null | undefined> =
+      await getLatestUserProfilePicture(String(user?.id));
+
+    expect(latestUserProfilePicture).toBeString();
+  });
+  test("returned value should be defined", async () => {
+    const user: Awaited<User | null> = await client.user.findFirst();
+    const latestUserProfilePicture: Awaited<string | null | undefined> =
+      await getLatestUserProfilePicture(String(user?.id));
+
+    expect(latestUserProfilePicture).toBeDefined();
+  });
+});
