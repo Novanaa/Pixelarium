7bafe23705f78836377030167c86b24122bfee4c
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-12-10 04:06:19 -0500

    Add picture fields to specify update data input

--- src/apps/v1/users/services/updateUserData.ts
+++ src/apps/v1/users/services/updateUserData.ts
@@ -4,14 +4,17 @@ import type TUserValidation from "../interfaces/types/UserValidationTypes";
 export default async function updateUserData({
   id,
   data: value,
+  picture,
 }: {
   id: string;
   data: TUserValidation;
+  picture?: string;
 }): Promise<void> {
   await client.user.update({
     where: { id: Number(id) },
     data: {
       ...value,
+      picture: picture || value.picture,
     },
   });
 }
