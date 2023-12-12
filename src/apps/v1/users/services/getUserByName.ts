42b719788da89ad4ec6356a13bf595d6757a41a8
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-12-11 21:29:04 -0500

    Add selection for user field

--- src/apps/v1/users/services/getUserByName.ts
+++ src/apps/v1/users/services/getUserByName.ts
@@ -1,12 +1,26 @@
-import { User } from "../../../../../generated/client";
 import client from "../../../../libs/configs/prisma";
+import type UserWithoutPassword from "../interfaces/types/UserWithoutPasswordTypes";
 
 export default async function getUserByName(
   name: string
-): Promise<User | null> {
-  const user: Awaited<User | null> = await client.user.findUnique({
-    where: { name },
-  });
+): Promise<UserWithoutPassword | null> {
+  const user: Awaited<UserWithoutPassword | null> =
+    await client.user.findUnique({
+      where: { name },
+      select: {
+        id: true,
+        provider_id: true,
+        name: true,
+        email: true,
+        bio: true,
+        picture: true,
+        type: true,
+        site_admin: true,
+        createdAt: true,
+        updatedAt: true,
+        password: false,
+      },
+    });
 
   return user;
 }
