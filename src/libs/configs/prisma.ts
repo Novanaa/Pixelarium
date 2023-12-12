bdeb027b63e475f1c5ffd5600a61c204a9484e7f
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-12-12 00:02:10 -0500

    Add docstring to libs/configuration

--- src/libs/configs/prisma.ts
+++ src/libs/configs/prisma.ts
@@ -2,6 +2,8 @@ import { PrismaClient } from "../../../generated/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { DATABASE_URL } from "../../const/env";
 
+/* The code is creating a new instance of the PrismaClient class and assigning it to the `client`
+constant. */
 const client = new PrismaClient({
   datasources: { db: { url: DATABASE_URL } },
 }).$extends(withAccelerate());
