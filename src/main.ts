9638a2518b7b9356b38413af7a588aec1f7d6954
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-12-12 21:43:38 -0500

    Add API Spec Documentation

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import fileUpload from "express-fileupload";
+import swaggerUi from "swagger-ui-express";
 /* `const app = express();` is creating an instance of the Express.js application. This instance will
 be used to configure and run the server. */
 const app = express();
@@ -25,12 +26,14 @@ app.use(rateLimitter);
 
 // Application Routes
 /* The code block is importing various modules and routes and using them in the Express.js application. */
+import docs from "../docs/openapi.json";
 import { CLIENT_FRONTEND_URL } from "./const/env";
 import requestErrorValidation from "./middlewares/requestErrorValidation";
 import rateLimitter from "./middlewares/rateLimitter";
 import authRoutes from "./apps/v1/auth/routes/auth.routes";
 import userRoutes from "./apps/v1/users/routes/user.routes";
 
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(docs));
 app.use("/v1/auth", authRoutes);
 app.use("/v1/users", userRoutes);
 
