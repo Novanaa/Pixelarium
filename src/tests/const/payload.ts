c0da34314b4b4a5966c99f4865e19b906d9df22b
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-12-18 12:06:21 +0700

    Added constant JWT user payload

---
+++ src/tests/const/payload.ts
@@ -0,0 +1,10 @@
+import type TJwtUserPayload from "../../interfaces/types/JwtUserPayloadTypes";
+
+const payload: TJwtUserPayload = {
+  providerId: 1,
+  name: "Nopaa",
+  email: "nopaaa@gmail.com",
+  picture: "https://avatars.githubusercontent.com/u/123456789?v=4",
+};
+
+export default payload;
