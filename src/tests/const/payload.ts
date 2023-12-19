90530fa1ee5ee91b3bfc22057a17dffe4a568866
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-12-19 14:50:18 +0700

    Updated providerId payload field

--- src/tests/const/payload.ts
+++ src/tests/const/payload.ts
@@ -1,7 +1,7 @@
 import type TJwtUserPayload from "../../interfaces/types/JwtUserPayloadTypes";
 
 const payload: TJwtUserPayload = {
-  providerId: 1,
+  providerId: 10102342,
   name: "Nopaa",
   email: "nopaaa@gmail.com",
   picture: "https://avatars.githubusercontent.com/u/123456789?v=4",
