a7ede4f164afcfaebfce78af8f0dfd22b426d00b
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-12-19 10:18:26 +0700

    Added geneate user client keys when create new user

--- src/apps/v1/auth/services/createUserIfNotExists.ts
+++ src/apps/v1/auth/services/createUserIfNotExists.ts
@@ -1,6 +1,10 @@
 import client from "../../../../libs/configs/prisma";
 import TCreateUser from "../interfaces/types/CreateUserTypes";
+import generateClientId from "../../../../services/generateClientId";
 
+// Function to create a user if it doesn't exist.
+// The function takes an object as an argument, which includes properties like providerId, name, email, etc.
+// These properties are then used to create a new user in the database.
 export default async function createUserIfNotExists({
   providerId,
   name,
@@ -8,6 +12,10 @@ export default async function createUserIfNotExists({
   picture,
   bio,
 }: TCreateUser): Promise<void> {
+  // Generate a unique clientId for the user using the provided providerId
+  const clientId: string = generateClientId(providerId);
+
+  // Create a new user in the database with the provided information
   await client.user.create({
     data: {
       provider_id: providerId,
@@ -15,12 +23,22 @@ export default async function createUserIfNotExists({
       email,
       picture,
       bio,
+      // Create a subscription for the user with an initial status of "deactive" and plan of "none"
       subcription: {
         create: {
           status: "deactive",
           plan: "none",
         },
       },
+      // Create a client key for the user with the generated clientId
+      client_keys: {
+        create: {
+          client_id: clientId,
+        },
+      },
     },
   });
 }
+
+// Note: This function is called whenever a new user is encountered during authentication.
+// It ensures that a user record is created in the database before proceeding with the authentication process.
