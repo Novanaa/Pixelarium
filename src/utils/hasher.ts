836ccb087d602a26748e6ea38fd180c126468a89
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-12-19 08:39:09 +0700

    Added docstring

--- src/utils/hasher.ts
+++ src/utils/hasher.ts
@@ -1,5 +1,11 @@
 import { SupportedCryptoAlgorithms } from "bun";
 
+/**
+ * A factory function to create a new Bun.CryptoHasher instance.
+ *
+ * @param alg - The cryptographic algorithm to use for hashing.
+ * @returns A new Bun.CryptoHasher instance configured to use the specified algorithm.
+ */
 export default function newHasher(alg: SupportedCryptoAlgorithms) {
   const hasher = new Bun.CryptoHasher(alg);
 
