99ac71b9dbfa91a1213fb8d8dbc4d3ab6ea22350
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-12-19 08:42:05 +0700

    Added docstring

--- src/apps/v1/client-keys/utils/createSecretClientPattern.ts
+++ src/apps/v1/client-keys/utils/createSecretClientPattern.ts
@@ -1,3 +1,14 @@
+/**
+ * The function 'createSecretClientPattern' takes in an object with 'name' and 'token' as parameters.
+ * It creates a unique client secret pattern using these parameters.
+ *
+ * @param {{
+ *   name: string;
+ *   token: string;
+ * }} params - The 'params' object contains the 'name' and 'token' of the client.
+ *
+ * @returns {string} - The function returns the client secret pattern.
+ */
 export default function createSecretClientPattern({
   name,
   token,
