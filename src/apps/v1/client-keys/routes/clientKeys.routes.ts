9153926486b884000c87462d39efd0b5ad2028ed
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-12-20 22:13:32 +0700

    Added Remove User Client Keys API Routes

--- src/apps/v1/client-keys/routes/clientKeys.routes.ts
+++ src/apps/v1/client-keys/routes/clientKeys.routes.ts
@@ -2,9 +2,11 @@ import express from "express";
 import jsonWebTokenAuthorization from "../../../../middlewares/jsonWebTokenAuthorization";
 import generateSecretKey from "../controllers/client-keys.post.controller";
 import getClientSecret from "../controllers/client-keys.get.controller";
+import removeClientKey from "../controllers/client-keys.delete.controller";
 const router = express.Router();
 
 router.post("/generate", jsonWebTokenAuthorization, generateSecretKey);
 router.get("/:name", jsonWebTokenAuthorization, getClientSecret);
+router.delete("/", jsonWebTokenAuthorization, removeClientKey);
 
 export default router;
