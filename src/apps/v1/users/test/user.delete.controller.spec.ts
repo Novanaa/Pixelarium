02e9eb43c2f17ecb8359af61559329070dfcb72d
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-12-21 14:17:28 +0700

    Added private access delete user api endpoint unit-test

--- src/apps/v1/users/test/user.delete.controller.spec.ts
+++ src/apps/v1/users/test/user.delete.controller.spec.ts
@@ -3,6 +3,10 @@ import app from "../../../../main";
 import supertest from "supertest";
 import client from "../../../../libs/configs/prisma";
 import generateMocksJWTToken from "../../../../tests/utils/generateMocksJWTToken";
+import { User } from "../../../../../generated/client";
+import TJwtUserPayload from "../../../../interfaces/types/JwtUserPayloadTypes";
+import JsonWebToken from "../../../../services/JsonWebToken";
+import payload from "../../../../tests/const/payload";
 
 describe("Unit-Testing Delete User API Endpoint", () => {
   test("should be return 400 status code if the request id params is not numberic", async () => {
@@ -45,5 +49,46 @@ describe("Unit-Testing Delete User API Endpoint", () => {
 
     expect(request.status).toBe(200);
     expect(request.body.status).toBe("OK");
+  });
+});
+
+describe("Unit-Testing Private Access - Delete User API Endpoint", () => {
+  test("should be return 401 status code if the user doesn't have a session token", async () => {
+    const user: Awaited<User | null> = await client.user.findFirst();
+    const { accessToken: token } = generateMocksJWTToken();
+    const request = await supertest(app)
+      .delete(`/v1/plxm/users/${user?.id}`)
+      .set("Authorization", `Bearer ${token}`);
+
+    expect(request.status).toBe(401);
+    expect(request.body.status).toBe("KO");
+  });
+  test("should be return 422 status code if the subs plan is none", async () => {
+    const jwt: JsonWebToken = new JsonWebToken();
+    const userPayload: TJwtUserPayload = { ...payload, providerId: 898 };
+
+    const user: Awaited<User | null> = await client.user.findFirst();
+    const { accessToken: token, refreshToken } = jwt.sign(userPayload);
+    const request = await supertest(app)
+      .delete(`/v1/plxm/users/${user?.id}`)
+      .set("Authorization", `Bearer ${token}`)
+      .set("Cookie", `session=${refreshToken}`);
+
+    expect(request.status).toBe(422);
+    expect(request.body.status).toBe("KO");
+  });
+  test("should be return 422 status code if the subs plan is deactive", async () => {
+    const jwt: JsonWebToken = new JsonWebToken();
+    const userPayload: TJwtUserPayload = { ...payload, providerId: 123 };
+
+    const user: Awaited<User | null> = await client.user.findFirst();
+    const { accessToken: token, refreshToken } = jwt.sign(userPayload);
+    const request = await supertest(app)
+      .delete(`/v1/plxm/users/${user?.id}`)
+      .set("Authorization", `Bearer ${token}`)
+      .set("Cookie", `session=${refreshToken}`);
+
+    expect(request.status).toBe(422);
+    expect(request.body.status).toBe("KO");
   });
 });
