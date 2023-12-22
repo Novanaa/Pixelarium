644e9b5b1a811aa105400d6dcd8a5b3cd2183409
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-12-22 08:56:18 +0700

    Add client_id and client_secret to private access user update API Endpoint

--- src/apps/v1/users/test/user.update.controller.spec.ts
+++ src/apps/v1/users/test/user.update.controller.spec.ts
@@ -2,10 +2,11 @@ import { expect, test, describe } from "bun:test";
 import app from "../../../../main";
 import supertest from "supertest";
 import client from "../../../../libs/configs/prisma";
-import { User } from "../../../../../generated/client";
+import { ClientKey, User } from "../../../../../generated/client";
 import payload from "../../../../tests/const/payload";
 import JsonWebToken from "../../../../services/JsonWebToken";
 import TJwtUserPayload from "../../../../interfaces/types/JwtUserPayloadTypes";
+import getTestUser from "../../../../tests/utils/getTestUser";
 
 describe("Unit-Testing User Update API Endpoint", () => {
   test("should be return 400 status code if the request id is not a number", async () => {
@@ -120,9 +121,15 @@ describe("Unit-Testing User Update API Endpoint", () => {
 
 describe("Unit-Testing Private Access User Update API Endpoint", () => {
   test("should be return 401 status code if the user doesn't have a session token", async () => {
-    const user: Awaited<User | null> = await client.user.findFirst();
+    const user: Awaited<User | null> = await getTestUser(payload.providerId);
+    const userClientKeys: Awaited<ClientKey | null> =
+      await client.clientKey.findUnique({
+        where: { user_id: user?.id },
+      });
     const request = await supertest(app)
-      .patch(`/v1/plxm/users/${user?.id}`)
+      .patch(
+        `/v1/plxm/users/${user?.id}?client_id=${userClientKeys?.client_id}&client_secret=${userClientKeys?.client_secret}`
+      )
       .field("bio", "test");
 
     expect(request.status).toBe(401);
@@ -132,12 +139,18 @@ describe("Unit-Testing Private Access User Update API Endpoint", () => {
     const jwt: JsonWebToken = new JsonWebToken();
     const userPayload: TJwtUserPayload = { ...payload, providerId: 898 };
 
-    const user: Awaited<User | null> = await client.user.findUnique({
-      where: { provider_id: 898 },
-    });
+    const user: Awaited<User | null> = await getTestUser(
+      userPayload.providerId
+    );
+        const userClientKeys: Awaited<ClientKey | null> =
+          await client.clientKey.findUnique({
+            where: { user_id: user?.id },
+          });
     const { refreshToken } = jwt.sign(userPayload);
     const request = await supertest(app)
-      .patch(`/v1/plxm/users/${user?.id}`)
+      .patch(
+        `/v1/plxm/users/${user?.id}?client_id=${userClientKeys?.client_id}&client_secret=${userClientKeys?.client_secret}`
+      )
       .field("bio", "test")
       .set("Cookie", `session=${refreshToken}`);
 
@@ -148,12 +161,18 @@ describe("Unit-Testing Private Access User Update API Endpoint", () => {
     const jwt: JsonWebToken = new JsonWebToken();
     const userPayload: TJwtUserPayload = { ...payload, providerId: 123 };
 
-    const user: Awaited<User | null> = await client.user.findUnique({
-      where: { provider_id: 123 },
-    });
+    const user: Awaited<User | null> = await getTestUser(
+      userPayload.providerId
+    );
+        const userClientKeys: Awaited<ClientKey | null> =
+          await client.clientKey.findUnique({
+            where: { user_id: user?.id },
+          });
     const { refreshToken } = jwt.sign(userPayload);
     const request = await supertest(app)
-      .patch(`/v1/plxm/users/${user?.id}`)
+      .patch(
+        `/v1/plxm/users/${user?.id}?client_id=${userClientKeys?.client_id}&client_secret=${userClientKeys?.client_secret}`
+      )
       .set("Cookie", `session=${refreshToken}`);
 
     expect(request.status).toBe(422);
