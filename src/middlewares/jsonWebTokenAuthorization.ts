595c8526fb76cbcced93c8da934c0b6b998b53a7
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-12-28 17:52:32 +0700

    Refactored JsonWebToken Authorization Middleware API Responses

--- src/middlewares/jsonWebTokenAuthorization.ts
+++ src/middlewares/jsonWebTokenAuthorization.ts
@@ -1,36 +1,50 @@
+import { JwtPayload } from "jsonwebtoken";
 import { Request, Response, NextFunction } from "express";
 import logger from "../libs/configs/logger";
-import { ErrorsRespones } from "../utils/Response";
 import JsonWebToken from "../services/JsonWebToken";
 import type { TDecodedUser } from "../apps/v1/auth/interfaces/types/DecodedUserTypes";
-import { JwtPayload } from "jsonwebtoken";
+import {
+  httpBadRequestResponse,
+  httpUnauthorizedResponse,
+  httpUnprocessableContentResponse,
+} from "../utils/responses/httpErrorsResponses";
 
 export default function jsonWebTokenAuthorization(
   req: Request,
   res: Response,
   next: NextFunction
 ): Response | void {
-  const Error = new ErrorsRespones();
   const jwt = new JsonWebToken();
   try {
     const { authorization } = req.headers;
     const token: string | undefined =
       authorization && authorization.split(" ")[1];
 
-    if (!token) return Error.unauth(res, "This opearation required a token!");
+    if (!token)
+      return httpUnauthorizedResponse({
+        response: res,
+        errorMessage: "This opearation required a token!",
+      });
 
     const decoded: string | JwtPayload | null = jwt.verify(
       token
     ) as TDecodedUser;
 
-    if (!decoded) return Error.unprocessable(res, "Invalid Signature!");
+    if (!decoded)
+      return httpUnprocessableContentResponse({
+        response: res,
+        errorMessage: "Invalid Signature!",
+      });
 
     if (decoded.exp) {
       const expirationTimestamp: number | undefined = decoded.exp;
       const currentTimestamp = Math.floor(Date.now() / 1000);
 
       if (expirationTimestamp! < currentTimestamp)
-        return Error.unauth(res, "The Token has been expired");
+        return httpUnauthorizedResponse({
+          response: res,
+          errorMessage: "The Token has been expired",
+        });
     }
 
     req.decoded = decoded;
@@ -38,6 +52,6 @@ export default function jsonWebTokenAuthorization(
     return next();
   } catch (err) {
     logger.error(err);
-    return Error.badRequest(res);
+    return httpBadRequestResponse({ response: res });
   }
 }
