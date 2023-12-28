a89932e9dda6fb89a925fc8c5737bac3a32d088d
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-12-28 17:41:36 +0700

    Refactored Delete User Client Keys API Endpoint Responses

--- src/apps/v1/client-keys/controllers/client-keys.delete.controller.ts
+++ src/apps/v1/client-keys/controllers/client-keys.delete.controller.ts
@@ -1,47 +1,82 @@
 import { Request, Response } from "express";
 import client from "../../../../libs/configs/prisma";
 import logger from "../../../../libs/configs/logger";
-import { ErrorsRespones, SuccessRespones } from "../../../../utils/Response";
 import { TDecodedUser } from "../../auth/interfaces/types/DecodedUserTypes";
 import { isUserExistByIdOrProviderId } from "../../../../utils/isUser";
-import { ClientKey, User } from "../../../../../generated/client";
+import { ClientKey } from "../../../../../generated/client";
 import deleteUserClientSecret from "../services/deleteUserClientSecret";
 import findUserClientKeys from "../services/findUserClientKeys";
+import {
+  httpBadRequestResponse,
+  httpNotFoundResponse,
+  httpUnauthorizedResponse,
+  httpUnprocessableContentResponse,
+} from "../../../../utils/responses/httpErrorsResponses";
+import { jsonResult } from "../../../../utils/responses/httpApiResponses";
+import http from "../../../../const/readonly/httpStatusCode";
+import { UserWithOptionalChaining } from "../../../../interfaces/UserWithOptionalChaining";
 
 export default async function removeClientKey(
   req: Request,
   res: Response
 ): Promise<void | Response> {
-  const Error: ErrorsRespones = new ErrorsRespones();
   try {
     const { session } = req.cookies;
 
-    if (!session) return Error.unauth(res, "You must be login first!");
+    if (!session)
+      return httpUnauthorizedResponse({
+        response: res,
+        errorMessage: "You must be login first!",
+      });
 
     const decode = req.decoded as TDecodedUser;
 
-    const user: Awaited<User | null> = await isUserExistByIdOrProviderId({
-      field: "provider_id",
-      value: decode.providerId,
-    });
+    const user: Awaited<UserWithOptionalChaining | null> =
+      await isUserExistByIdOrProviderId({
+        field: "provider_id",
+        value: decode.providerId,
+      });
 
-    if (!user) return Error.notFound(res, "Your request doesn't be found");
+    if (!user)
+      return httpNotFoundResponse({
+        response: res,
+        errorMessage: "Your request doesn't be found",
+      });
 
     const userClientKeys: Awaited<ClientKey | null> = await findUserClientKeys(
       user.id
     );
 
     if (!userClientKeys?.client_secret)
-      return Error.unprocessable(res, "The User Client Secret Ins't Exist.");
+      return httpUnprocessableContentResponse({
+        response: res,
+        errorMessage: "The User Client Secret Ins't Exist.",
+      });
 
-    if (user && userClientKeys?.client_secret) {
+    if (user && userClientKeys.client_secret) {
       await deleteUserClientSecret(user.id);
     }
 
-    return new SuccessRespones().success(res, "deleted");
+    const responseData = {
+      from: {
+        ...user,
+      },
+      deletedClientKeys: {
+        user_client_id: userClientKeys.client_id,
+        user_client_secret: userClientKeys.client_secret,
+      },
+    };
+
+    return jsonResult({
+      response: res,
+      statusCode: http.StatusOk,
+      resultKey: "deleted",
+      dataKey: "data",
+      data: responseData,
+    });
   } catch (err) {
     logger.error(err);
-    return Error.badRequest(res);
+    return httpBadRequestResponse({ response: res });
   } finally {
     await client.$disconnect();
   }
