880822e2cd478ee8ed947e1bf5f52a7f721040df
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-12-28 15:45:49 +0700

    Added simple javascript docstring

--- src/utils/getFutureDateTime.ts
+++ src/utils/getFutureDateTime.ts
@@ -2,6 +2,19 @@ type GetFutureDateTimeType = {
   futureDateTimeInSecond: number;
 };
 
+/**
+ * GetFutureDateTime is a utility function that returns a new Date object representing a date in the future.
+ * It accepts a single argument, an object of type GetFutureDateTimeType.
+ * This object has a single property, futureDateTimeInSecond, which represents the number of seconds into the future that the returned Date object should represent.
+ *
+ * The function starts by creating a new Date object for the current date and time (now).
+ * It then creates a new Date object (futureDateTime) for the date and time that is the number of seconds specified by futureDateTimeInSecond, added to the current date and time.
+ *
+ * Finally, the function returns the futureDateTime object.
+ *
+ * @param getFutureDateTimeType - The input object, of type GetFutureDateTimeType, containing the number of seconds into the future that the returned Date object should represent.
+ * @returns The Date object representing the date and time in the future that corresponds to the specified number of seconds.
+ */
 export default function getFutureDateTime({
   futureDateTimeInSecond,
 }: GetFutureDateTimeType): Date {
