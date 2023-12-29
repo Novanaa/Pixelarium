c6a196e667e15f7c304db9cbc98d83a9f147ac44
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-12-29 11:48:06 +0700

    Removed Delete User API Endpoit from Private Access API Grant

--- src/apps/v1/users/routes/private.user.routes.ts
+++ src/apps/v1/users/routes/private.user.routes.ts
@@ -1,13 +1,10 @@
 import express from "express";
 const router = express.Router();
-import deleteUser from "../controllers/user.delete.controller";
 import updateUser from "../controllers/user.update.controller";
 import singleUser from "../controllers/user.get.controller";
-import jsonWebTokenAuthorization from "../../../../middlewares/jsonWebTokenAuthorization";
 import apiGrantAccess from "../../../../middlewares/apiGrantAccess";
 
 router.get("/:name", singleUser);
 router.patch("/:id", apiGrantAccess, updateUser);
-router.delete("/:id", [jsonWebTokenAuthorization, apiGrantAccess], deleteUser);
 
 export default router;
