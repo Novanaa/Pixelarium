8b589b3c543be578a562b516512bad6cd3317a84
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-12-30 20:50:08 +0700

    Added unit-testing for get user gallery services

---
+++ src/apps/v1/galleries/test/getUserGallery.spec.ts
@@ -0,0 +1,43 @@
+import { test, expect, describe } from "bun:test";
+import getUserGallery from "../services/getUserGallery";
+import payload from "../../../../tests/const/payload";
+import getTestUser from "../../../../tests/utils/getTestUser";
+import { Gallery, User } from "../../../../../generated/client";
+
+describe("Unit-Test Get User Gallery Services", () => {
+  test("should be return user gallery data", async () => {
+    const user: Awaited<User | null> = await getTestUser(payload.providerId);
+    const userGallery: Awaited<Gallery | null> = await getUserGallery(
+      user?.id!
+    );
+
+    console.log(userGallery);
+    expect(userGallery).not.toBeEmpty();
+  });
+  test("retunred value should be defined", async () => {
+    const user: Awaited<User | null> = await getTestUser(payload.providerId);
+    const userGallery: Awaited<Gallery | null> = await getUserGallery(
+      user?.id!
+    );
+
+    console.log(userGallery);
+    expect(userGallery).toBeDefined();
+    expect(userGallery).not.toBeUndefined();
+  });
+  test("retunred value should be not null", async () => {
+    const user: Awaited<User | null> = await getTestUser(payload.providerId);
+    const userGallery: Awaited<Gallery | null> = await getUserGallery(
+      user?.id!
+    );
+
+    console.log(userGallery);
+    expect(userGallery).toBeDefined();
+    expect(userGallery).not.toBeNull();
+  });
+  test("retunred value should be null", async () => {
+    const userGallery: Awaited<Gallery | null> = await getUserGallery(0);
+
+    console.log(userGallery);
+    expect(userGallery).toBeNull();
+  });
+});
