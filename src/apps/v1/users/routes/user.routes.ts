81f08f769ec64c3c411c7003bce9541a05bfa95e
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-12-30 19:36:48 +0700

    Added Verify Session Token Middleware to User Delete API Endpoint

--- src/apps/v1/users/routes/user.routes.ts
+++ src/apps/v1/users/routes/user.routes.ts
@@ -4,9 +4,14 @@ import deleteUser from "../controllers/user.delete.controller";
 import updateUser from "../controllers/user.update.controller";
 import singleUser from "../controllers/user.get.controller";
 import jsonWebTokenAuthorization from "../../../../middlewares/jsonWebTokenAuthorization";
+import verifySessionToken from "../../../../middlewares/verifySessionToken";
 
 router.get("/:name", singleUser);
 router.patch("/:name", updateUser);
-router.delete("/:name", jsonWebTokenAuthorization, deleteUser);
+router.delete(
+  "/:name",
+  [jsonWebTokenAuthorization, verifySessionToken],
+  deleteUser
+);
 
 export default router;
