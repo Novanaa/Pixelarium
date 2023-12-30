298d5b600faaaeb4deae1a01aabdc19e97b7acc5
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-12-30 18:53:57 +0700

    Changed how prisma can find user to updated

--- src/apps/v1/users/services/updateUserData.ts
+++ src/apps/v1/users/services/updateUserData.ts
@@ -2,16 +2,16 @@ import client from "../../../../libs/configs/prisma";
 import type TUserValidation from "../interfaces/types/UserValidationTypes";
 
 export default async function updateUserData({
-  id,
+  name,
   data: value,
   picture,
 }: {
-  id: string;
+  name: string;
   data: TUserValidation;
   picture?: string;
 }): Promise<void> {
   await client.user.update({
-    where: { id: Number(id) },
+    where: { name },
     data: {
       ...value,
       picture: picture || value.picture,
