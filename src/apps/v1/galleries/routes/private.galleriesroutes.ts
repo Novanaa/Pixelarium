91de9f0e887efbc3d62d3be9dbe8871d97b9c0cd
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-12-31 15:29:42 +0700

    Added Private Access Search Query User Gallery Pictures API Endpoint

--- src/apps/v1/galleries/routes/private.galleriesroutes.ts
+++ src/apps/v1/galleries/routes/private.galleriesroutes.ts
@@ -1,7 +1,12 @@
 import express from "express";
-import { gallery } from "../controllers/galleries.get.controller";
+import {
+  gallery,
+  searchPicturesGallery,
+} from "../controllers/galleries.get.controller";
+import apiGrantAccess from "../../../../middlewares/apiGrantAccess";
 const router = express.Router();
 
 router.get("/:name", gallery);
+router.get("/:name/search", apiGrantAccess, searchPicturesGallery);
 
 export default router;
