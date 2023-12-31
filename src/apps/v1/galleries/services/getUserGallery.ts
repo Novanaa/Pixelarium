86fe3f4b549a7b02c234d1c43d472a833078e040
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-12-31 08:12:06 +0700

    Added user gallery pictures data and user gallery types

--- src/apps/v1/galleries/services/getUserGallery.ts
+++ src/apps/v1/galleries/services/getUserGallery.ts
@@ -1,8 +1,12 @@
 // Importing the Gallery model from the generated client
-import { Gallery } from "../../../../../generated/client";
+import { Gallery, Picture } from "../../../../../generated/client";
 
 // Importing the client configuration for accessing the database
 import client from "../../../../libs/configs/prisma";
+
+export interface UserGallery extends Gallery {
+  pictures: Array<Picture>;
+}
 
 /**
  * Fetches the gallery for a given user from the database.
@@ -12,12 +16,22 @@ import client from "../../../../libs/configs/prisma";
  */
 export default async function getUserGallery(
   userId: number
-): Promise<Gallery | null> {
+): Promise<UserGallery> {
   // Use the client to find the gallery with the specified userId.
   // The await keyword ensures that the function will wait for the query to complete before returning the result.
-  const userGallery: Awaited<Gallery | null> = await client.gallery.findUnique({
+  const gallery: Awaited<Gallery | null> = await client.gallery.findUnique({
     where: { user_id: userId },
   });
+
+  const userGalleryPictures: Awaited<Array<Picture>> =
+    await client.picture.findMany({
+      where: { gallery_id: gallery?.id || 0 },
+    });
+
+  const userGallery: UserGallery = {
+    ...gallery!,
+    pictures: userGalleryPictures,
+  };
 
   // Return the user's gallery, or null if no gallery is found for the given userId.
   return userGallery;
