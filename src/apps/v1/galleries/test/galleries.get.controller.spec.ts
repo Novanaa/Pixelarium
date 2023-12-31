da385223ef56844da9e1f0750fd2fc24d2a736f9
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-12-31 15:30:25 +0700

    Added unit-test for search query user gallery pictures

--- src/apps/v1/galleries/test/galleries.get.controller.spec.ts
+++ src/apps/v1/galleries/test/galleries.get.controller.spec.ts
@@ -3,8 +3,15 @@ import app from "../../../../main";
 import supertest from "supertest";
 import getTestUser from "../../../../tests/utils/getTestUser";
 import payload from "../../../../tests/const/payload";
-import { ClientKey, User } from "../../../../../generated/client";
+import {
+  ClientKey,
+  Gallery,
+  Picture,
+  User,
+} from "../../../../../generated/client";
 import getTestUserClientKeys from "../../../../tests/utils/getTestUserClientKeys";
+import findUserGallery from "../utils/findUserGallery";
+import client from "../../../../libs/configs/prisma";
 
 describe("Unit-Testing Get User Gallery API Endpoint", () => {
   test("should be return 200 status code", async () => {
@@ -231,6 +238,100 @@ describe("Verify User Client Keys - Unit-Testing Get User Gallery API Endpoint",
             "123"
           )}?client_id=${userClientKeys?.client_id}&client_secret=${userClientKeys?.client_secret}`
         )
+        .set("Content-Type", "application/json");
+
+    console.log(request.body);
+    expect(request.status).toBe(404);
+    expect(request.body.status).toBe("KO");
+  });
+});
+
+describe("Unit-Testing Search User Gallery Pictures API Endpoint", () => {
+  test("should be return 200 status code", async () => {
+    const user: Awaited<User | null> = await getTestUser(payload.providerId);
+    const gallery: Awaited<Gallery | null> = await findUserGallery(
+      user?.id || 0
+    );
+    const pictures: Awaited<Array<Picture>> = await client.picture.findMany({
+      where: { gallery_id: gallery?.id },
+    });
+    const request: Awaited<supertest.Request | supertest.Response> =
+      await supertest(app).get(
+        `/v1/galleries/${user?.name}/search?q=${pictures[0].title}`
+      );
+
+    console.log(request.body);
+    expect(request.status).toBe(200);
+    expect(request.body.status).toBe("OK");
+  });
+  test("make sure it can accept application/json", async () => {
+    const user: Awaited<User | null> = await getTestUser(payload.providerId);
+    const gallery: Awaited<Gallery | null> = await findUserGallery(
+      user?.id || 0
+    );
+    const pictures: Awaited<Array<Picture>> = await client.picture.findMany({
+      where: { gallery_id: gallery?.id },
+    });
+    const request: Awaited<supertest.Request | supertest.Response> =
+      await supertest(app)
+        .get(`/v1/galleries/${user?.name}/search?q=${pictures[0].title}`)
+        .set("Content-Type", "application/json");
+
+    console.log(request.body);
+    expect(request.status).toBe(200);
+    expect(request.body.status).toBe("OK");
+  });
+  test("returned response pictures data should be not undefined", async () => {
+    const user: Awaited<User | null> = await getTestUser(payload.providerId);
+    const gallery: Awaited<Gallery | null> = await findUserGallery(
+      user?.id || 0
+    );
+    const pictures: Awaited<Array<Picture>> = await client.picture.findMany({
+      where: { gallery_id: gallery?.id },
+    });
+    const request: Awaited<supertest.Request | supertest.Response> =
+      await supertest(app)
+        .get(`/v1/galleries/${user?.name}/search?q=${pictures[0].title}`)
+        .set("Content-Type", "application/json");
+
+    console.log(request.body);
+    expect(request.status).toBe(200);
+    expect(request.body.status).toBe("OK");
+    expect(request.body.data.pictures).toBeDefined();
+    expect(request.body.data.pictures).not.toBeUndefined();
+    expect(request.body.data.pictures).not.toBeNull();
+  });
+  test("returned response total found data should be not undefined", async () => {
+    const user: Awaited<User | null> = await getTestUser(payload.providerId);
+    const gallery: Awaited<Gallery | null> = await findUserGallery(
+      user?.id || 0
+    );
+    const pictures: Awaited<Array<Picture>> = await client.picture.findMany({
+      where: { gallery_id: gallery?.id },
+    });
+    const request: Awaited<supertest.Request | supertest.Response> =
+      await supertest(app)
+        .get(`/v1/galleries/${user?.name}/search?q=${pictures[0].title}`)
+        .set("Content-Type", "application/json");
+
+    console.log(request.body);
+    expect(request.status).toBe(200);
+    expect(request.body.status).toBe("OK");
+    expect(request.body.data.total_found).toBeDefined();
+    expect(request.body.data.total_found).not.toBeUndefined();
+    expect(request.body.data.total_found).not.toBeNull();
+  });
+  test("should be return 404 status code if the user cannot be found", async () => {
+    const user: Awaited<User | null> = await getTestUser(payload.providerId);
+    const gallery: Awaited<Gallery | null> = await findUserGallery(
+      user?.id || 0
+    );
+    const pictures: Awaited<Array<Picture>> = await client.picture.findMany({
+      where: { gallery_id: gallery?.id },
+    });
+    const request: Awaited<supertest.Request | supertest.Response> =
+      await supertest(app)
+        .get(`/v1/galleries/0/search?q=${pictures[0].title}`)
         .set("Content-Type", "application/json");
 
     console.log(request.body);
