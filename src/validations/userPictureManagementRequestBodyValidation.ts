030b6909f046e986401dc8670e2f28668e4fdccb
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-01-01 17:43:42 +0700

    Added user pictures upload management request body validation

---
+++ src/validations/userPictureManagementRequestBodyValidation.ts
@@ -0,0 +1,27 @@
+import { Request } from "express";
+import { default as joi } from "joi";
+import UserPictureManagementRequestBodyValidationTypes from "./interfaces/types/UserPictureManagementRequestBodyValidationTypes";
+
+export default function userPictureManagementRequestBodyValidation(
+  req: Request
+): joi.ValidationResult<UserPictureManagementRequestBodyValidationTypes> {
+  const userPictureManagementValidation = joi.object({
+    expiresInDays: joi.number().default(30),
+    is_private: joi.boolean().default(false),
+    use_external_image_url: joi.boolean().default(false),
+    pictures: joi
+      .array()
+      .items(
+        joi
+          .object({
+            title: joi.string().required(),
+            description: joi.string().required(),
+            image: joi.string(),
+          })
+          .required()
+      )
+      .required(),
+  });
+
+  return userPictureManagementValidation.validate(req.body);
+}
