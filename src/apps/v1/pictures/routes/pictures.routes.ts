014b526f845a52ab84d17052840876744c38b329
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-01-02 16:10:25 +0700

    Registered Upload User Picture Gallery Management Routes

--- src/apps/v1/pictures/routes/pictures.routes.ts
+++ src/apps/v1/pictures/routes/pictures.routes.ts
@@ -1,4 +1,7 @@
 import express from "express";
+import addUserGalleryPicture from "../controllers/upload.pictures.controller";
 const router = express.Router();
+
+router.post("/:name/upload", addUserGalleryPicture);
 
 export default router;
