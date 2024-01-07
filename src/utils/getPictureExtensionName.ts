de63719d00f654a625a67a8940db569e916383e0
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-01-07 17:34:28 +0700

    Added mime file extension type

--- src/utils/getPictureExtensionName.ts
+++ src/utils/getPictureExtensionName.ts
@@ -1,4 +1,5 @@
 import path from "path";
+import mime from "mime";
 
 /**
  * Retrieves the extension name of a picture from a given URL path.
@@ -6,12 +7,12 @@ import path from "path";
  * @param urlpath - The URL path of the picture.
  * @returns The extension name of the picture.
  */
-export default function getPictureExtensionName(urlpath: string) {
+export default function getPictureExtensionName(urlpath: string): string {
   if (!urlpath) throw new Error("Urlpath must be provided!");
 
   const findExtName: string = path.extname(urlpath).slice(1);
 
-  const extName: string = "image/" + findExtName;
+  const extName: string = mime.getType(findExtName) || "image/" + findExtName;
 
   return extName;
 }
