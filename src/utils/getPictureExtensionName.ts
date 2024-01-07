938e77142e1afa44d37f8a075a8eb97054073722
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-01-07 17:44:49 +0700

    Added Validation for Unknown Extension Filename

--- src/utils/getPictureExtensionName.ts
+++ src/utils/getPictureExtensionName.ts
@@ -1,5 +1,6 @@
 import path from "path";
 import mime from "mime";
+import imgExt from "../const/readonly/extentsion";
 
 /**
  * Retrieves the extension name of a picture from a given URL path.
@@ -10,9 +11,12 @@ import mime from "mime";
 export default function getPictureExtensionName(urlpath: string): string {
   if (!urlpath) throw new Error("Urlpath must be provided!");
 
-  const findExtName: string = path.extname(urlpath).slice(1);
+  const findExtName: string = path.extname(urlpath);
 
-  const extName: string = mime.getType(findExtName) || "image/" + findExtName;
+  if (!imgExt.includes(findExtName)) return "Unknown";
+
+  const extName: string =
+    mime.getType(findExtName.slice(1)) || "image/" + findExtName;
 
   return extName;
 }
