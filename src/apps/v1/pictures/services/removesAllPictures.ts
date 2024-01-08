c927a638cb05e9143088f067bac7ca4ad84f7bc9
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-01-08 18:02:30 +0700

    Changed Removes All Pictures Return Types

--- src/apps/v1/pictures/services/removesAllPictures.ts
+++ src/apps/v1/pictures/services/removesAllPictures.ts
@@ -1,19 +1,23 @@
+import { GetBatchResult } from "@prisma/client/runtime/library";
 import client from "../../../../libs/configs/prisma";
 
 /**
  * Removes all pictures with the given unique keys.
  *
  * @param uniquekey - An array of unique keys of the pictures to be removed.
- * @returns A Promise that resolves to void.
+ * @returns A promise that resolves to the number of pictures deleted.
  */
 export default async function removesAllPictures(
   uniquekey: Array<string>
-): Promise<void> {
-  await client.picture.deleteMany({
-    where: {
-      uniquekey: {
-        in: uniquekey,
+): Promise<GetBatchResult> {
+  const deletePictures: Awaited<GetBatchResult> =
+    await client.picture.deleteMany({
+      where: {
+        uniquekey: {
+          in: uniquekey,
+        },
       },
-    },
-  });
+    });
+
+  return deletePictures;
 }
