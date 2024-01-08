ad2b6690b3d1584c63dc43d268dbd8f36b3765a9
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-01-08 20:35:22 +0700

    Registered Delete All User Gallery Pictures API Endpoint

--- src/apps/v1/pictures/routes/pictures.routes.ts
+++ src/apps/v1/pictures/routes/pictures.routes.ts
@@ -1,7 +1,15 @@
 import express from "express";
 import addUserGalleryPicture from "../controllers/upload.pictures.controller";
+import { deleteAllUserPictures } from "../controllers/delete.pictures.controller";
+import jsonWebTokenAuthorization from "../../../../middlewares/jsonWebTokenAuthorization";
+import verifySessionToken from "../../../../middlewares/verifySessionToken";
 const router = express.Router();
 
 router.post("/:name/upload", addUserGalleryPicture);
+router.delete(
+  "/:name/removes",
+  [jsonWebTokenAuthorization, verifySessionToken],
+  deleteAllUserPictures
+);
 
 export default router;
