833d486f175a8798f1ce08ca726f099e822d3df8
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-01-09 10:59:43 +0700

    Registered Delete Single User Gallery Picture API Endpoint

--- src/apps/v1/pictures/routes/pictures.routes.ts
+++ src/apps/v1/pictures/routes/pictures.routes.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import addUserGalleryPicture from "../controllers/upload.pictures.controller";
-import { deleteAllUserPictures } from "../controllers/delete.pictures.controller";
+import {
+  deleteAllUserPictures,
+  deleteSingleExistingUserPicture,
+} from "../controllers/delete.pictures.controller";
 import jsonWebTokenAuthorization from "../../../../middlewares/jsonWebTokenAuthorization";
 import verifySessionToken from "../../../../middlewares/verifySessionToken";
 const router = express.Router();
@@ -10,6 +13,11 @@ router.delete(
   "/:name/removes",
   [jsonWebTokenAuthorization, verifySessionToken],
   deleteAllUserPictures
+);
+router.delete(
+  "/:name/:uniquekey",
+  [jsonWebTokenAuthorization, verifySessionToken],
+  deleteSingleExistingUserPicture
 );
 
 export default router;
