78ad142f3a5825b2535c8e8807368df350e55842
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-01-09 19:06:16 +0700

    Registered Private Access Update User Gallery Picture API Endpoint

--- src/apps/v1/pictures/routes/private.pictures.routes.ts
+++ src/apps/v1/pictures/routes/private.pictures.routes.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import addUserGalleryPicture from "../controllers/upload.pictures.controller";
 import apiGrantAccess from "../../../../middlewares/apiGrantAccess";
+import updateUserPicture from "../controllers/patch.pictures.controller";
 const router = express.Router();
 
 router.post("/:name/upload", apiGrantAccess, addUserGalleryPicture);
+router.patch("/:name/:uniquekey", apiGrantAccess, updateUserPicture);
 
 export default router;
