9cd5bbcdfc25a4059abecece582412a34d764d37
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-01-10 08:47:54 +0700

    Added readonly constant dummy external picture url

---
+++ src/const/readonly/dummyExternalPictureUrl.ts
@@ -0,0 +1,4 @@
+const dummyExternalPictureUrl: string =
+  "https://blog-asset.jakmall.com/blog/content/images/2020/11/20f535c616bbe807a1166e5661b396fd.jpg";
+
+export default dummyExternalPictureUrl;
