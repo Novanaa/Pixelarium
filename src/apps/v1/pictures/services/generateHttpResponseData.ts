3c074bbbda23fe8a18fb47a33842d4d14b162183
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-01-10 19:21:40 +0700

    Added new method for generate update user gallery picture http response data

--- src/apps/v1/pictures/services/generateHttpResponseData.ts
+++ src/apps/v1/pictures/services/generateHttpResponseData.ts
@@ -32,3 +32,31 @@ export function generateUploadPictureResponseData({
 
   return responseData;
 }
+
+export type UpdateUserPictureResponseData = {
+  owner: UserWithOptionalChaining;
+  old_data: Picture;
+  updated_data: Picture;
+};
+
+/**
+ * Generates the response data for uploading a picture.
+ *
+ * @param params - The parameters for generating the response data.
+ * @param params.insertedPicture - The inserted picture.
+ * @param params.user - The user with optional chaining.
+ * @returns The generated response data.
+ */
+export function generateUpdateUserPictureResponseData({
+  old_data,
+  owner,
+  updated_data,
+}: UpdateUserPictureResponseData) {
+  const responseData: UpdateUserPictureResponseData = {
+    owner,
+    old_data,
+    updated_data,
+  };
+
+  return responseData;
+}
