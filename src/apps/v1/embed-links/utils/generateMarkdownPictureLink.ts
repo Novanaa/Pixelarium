f5e745eee430ce78ff31c0d574a20bdfac509958
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-01-12 17:27:42 +0700

    Added Picture Embed Links Utils to Generate Markdown Picture Embed Links

---
+++ src/apps/v1/embed-links/utils/generateMarkdownPictureLink.ts
@@ -0,0 +1,27 @@
+import MarkdownLink from "../interfaces/types/MarkdownLink";
+
+type GenerateMarkdownPictureLinkParams = {
+  title: string;
+  url: string;
+};
+
+/**
+ * Generates a markdown picture link.
+ *
+ * @param {GenerateMarkdownPictureLinkParams} params - The parameters for generating the markdown picture link.
+ * @param {string} params.title - The title of the picture.
+ * @param {string} params.url - The URL of the picture.
+ * @returns {MarkdownLink} The generated markdown picture link.
+ */
+export default function generateMarkdownPictureLink({
+  title,
+  url,
+}: GenerateMarkdownPictureLinkParams): MarkdownLink {
+  const tooltip: string = `![${{ title }}](${url})`;
+
+  const markdownLink: MarkdownLink = {
+    tooltip_link: tooltip,
+  };
+
+  return markdownLink;
+}
