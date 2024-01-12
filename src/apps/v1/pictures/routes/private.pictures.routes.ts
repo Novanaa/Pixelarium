d4ef67877465c3541081fb2db7a22fd7773f0def
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-01-12 20:50:25 +0700

    Registered Private Accesss Pixelarium Get Single User Picture API Endpoint

--- src/apps/v1/pictures/routes/private.pictures.routes.ts
+++ src/apps/v1/pictures/routes/private.pictures.routes.ts
@@ -2,9 +2,11 @@ import express from "express";
 import addUserGalleryPicture from "../controllers/upload.pictures.controller";
 import apiGrantAccess from "../../../../middlewares/apiGrantAccess";
 import updateUserPicture from "../controllers/patch.pictures.controller";
+import singleUserPicture from "../controllers/pictures.get.controller";
 const router = express.Router();
 
 router.post("/:name/upload", apiGrantAccess, addUserGalleryPicture);
 router.patch("/:name/:uniquekey", apiGrantAccess, updateUserPicture);
+router.get("/:uniquekey", singleUserPicture);
 
 export default router;
