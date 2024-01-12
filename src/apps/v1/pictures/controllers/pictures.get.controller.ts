e1449f038c5b0f822b523983556d0578f538881b
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-01-12 21:10:56 +0700

    Added Get Single User Picture Core Logic

--- src/apps/v1/pictures/controllers/pictures.get.controller.ts
+++ src/apps/v1/pictures/controllers/pictures.get.controller.ts
@@ -1,14 +1,59 @@
 import { Request, Response } from "express";
 import logger from "../../../../libs/configs/logger";
-import { httpBadRequestResponse } from "../../../../utils/responses/httpErrorsResponses";
+import {
+  httpBadRequestResponse,
+  httpNotFoundResponse,
+} from "../../../../utils/responses/httpErrorsResponses";
 import client from "../../../../libs/configs/prisma";
+import validatePictureUniquekey from "../../../../utils/validatePictureUniquekey";
+import { EmbedLinks, Picture } from "../../../../../generated/client";
+import getUserPictureByUniquekey from "../services/getUserPictureByUniquekey";
+import getPictureEmbedLinks from "../../embed-links/services/getPictureEmbedLinks";
+import sendJsonResultHttpResponse from "../../../../services/sendJsonResultHttpResponse";
+
+type SingleUserPictureResponseData = {
+  picture: Picture;
+  embed_pictures_links: EmbedLinks;
+};
 
 export default async function singleUserPicture(
   req: Request,
   res: Response
 ): Promise<Response | void> {
   try {
-    res.send("testttt");
+    const { uniquekey } = req.params;
+
+    const pictureUniquekeyValidation: void | Response =
+      validatePictureUniquekey({
+        uniquekey,
+        response: res,
+      });
+
+    if (pictureUniquekeyValidation) return;
+
+    const userPicture: Awaited<Picture | null> =
+      await getUserPictureByUniquekey(uniquekey);
+
+    if (!userPicture) return httpNotFoundResponse({ response: res });
+
+    const pictureEmbedLinks: Awaited<EmbedLinks | null> =
+      await getPictureEmbedLinks(userPicture.id);
+
+    if (!pictureEmbedLinks)
+      return httpBadRequestResponse({
+        response: res,
+        errorMessage: "Unexpected Errors Occurred",
+      });
+
+    const responseData: SingleUserPictureResponseData = {
+      embed_pictures_links: pictureEmbedLinks,
+      picture: userPicture,
+    };
+
+    return sendJsonResultHttpResponse<SingleUserPictureResponseData>({
+      response: res,
+      responseData,
+    });
   } catch (err) {
     logger.error(err);
     return httpBadRequestResponse({ response: res });
