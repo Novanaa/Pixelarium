e847c1b594b85fa9566fad0009e168a04c46727e
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-01-13 22:54:11 +0700

    Registered Add User Favorites Pictures API Routes Endpoint

--- src/apps/v1/favorites-picture/routes/favorites.routes.ts
+++ src/apps/v1/favorites-picture/routes/favorites.routes.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import userFavoritePictures from "../controllers/favorites.get.controller";
+import addFavoritePictureByName from "../controllers/favorites.post.controller";
 const router: express.Router = express.Router();
 
 router.get("/:name", userFavoritePictures);
+router.post("/:name", addFavoritePictureByName);
 
 export default router;
