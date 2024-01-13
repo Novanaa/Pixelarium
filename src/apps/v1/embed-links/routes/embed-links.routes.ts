3cc175a0e34112bbcc744468bc28174c4ff87cc9
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-01-13 09:34:21 +0700

    Registered Add Picture Embed Links API Endpoint

--- src/apps/v1/embed-links/routes/embed-links.routes.ts
+++ src/apps/v1/embed-links/routes/embed-links.routes.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import userPictureEmbedLinks from "../controllers/embed-links.get.controller";
+import addEmbedLinksPicture from "../controllers/embed-links.post.controller";
 const router: express.Router = express.Router();
 
 router.get("/:name/:uniquekey", userPictureEmbedLinks);
+router.post("/:name/:uniquekey", addEmbedLinksPicture);
 
 export default router;
