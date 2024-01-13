e30ee48814e0e72d6c18768e113df9c9f3a0eb78
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-01-13 18:34:30 +0700

    Re-created Use Fetch Utilities

--- src/utils/useFetch.ts
+++ src/utils/useFetch.ts
@@ -1,34 +1,60 @@
 import logger from "../libs/configs/logger";
 
-/**
- * The function `useFetch` is an asynchronous function that makes a request to a specified URL using
- * the specified HTTP method and authorization header, and returns the response as a JSON object of
- * type T, or null if there is an error.
- * @param {string} url - The `url` parameter is a string that represents the URL of the API endpoint
- * you want to fetch data from.
- * @param {string} [method=GET] - The method parameter is used to specify the HTTP method to be used in
- * the request. It is optional and defaults to "GET" if not provided.
- * @param {string} [auth] - The `auth` parameter is used to specify the authorization token that should
- * be included in the request headers. It is an optional parameter and its default value is an empty
- * string. If you want to include an authorization token in the request, you can pass it as a string to
- * the `auth` parameter
- * @returns a Promise that resolves to a value of type T or null.
- */
-export default async function useFetch<T>(
-  url: string,
-  method: string = "GET",
-  auth: string = ""
-): Promise<T | null> {
-  try {
-    const response = await fetch(url, {
-      method,
-      headers: { Accept: "application/json", Authorization: auth },
-    });
-    const result: Awaited<T> = await response.json();
+type UseFetchParams = {
+  url: string;
+  method: "GET" | "POST" | "PATCH" | "DELETE" | "PUT";
+  body?: unknown;
+  headers?: Headers | HeadersInit;
+  options?: UseFetchOptions;
+};
 
-    return result;
-  } catch (err) {
-    logger.error(err);
-    return null;
-  }
+type UseFetchOptions = {
+  credentials?: RequestCredentials;
+  cache: RequestCache;
+};
+
+type UseFetchReturnType = {
+  datas: unknown;
+  errors: Error | undefined;
+};
+
+/**
+ * Performs a fetch request to the specified URL using the provided method, body, headers, and options.
+ *
+ * @param method - The HTTP method to use for the request (GET, POST, PATCH, DELETE, PUT).
+ * @param url - The URL to send the request to.
+ * @param body - The request body data (optional).
+ * @param headers - The request headers (optional).
+ * @param options - Additional options for the fetch request (optional).
+ * @returns An object containing the fetched data and any errors encountered.
+ */
+export default function useFetch<T>({
+  method,
+  url,
+  body = {},
+  headers,
+  options,
+}: UseFetchParams): UseFetchReturnType {
+  let datas, errors;
+
+  const retriveData = async () => {
+    try {
+      const response: Awaited<Response> = await fetch(url, {
+        method,
+        body: JSON.stringify(body),
+        headers,
+        credentials: options?.credentials || "same-origin",
+        cache: options?.cache,
+      });
+      const result: Awaited<T> = await response.json();
+      datas = result;
+    } catch (err) {
+      logger.error(err);
+      errors = err;
+    }
+  };
+
+  retriveData();
+
+  return { datas, errors };
 }
