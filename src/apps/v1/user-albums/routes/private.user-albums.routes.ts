cf5ec46683dc0e03f855ae2761fb43d3bbc4045c
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-01-14 19:53:04 +0700

    Fixed Incorrect Spelling Url Routes

--- src/apps/v1/user-albums/routes/private.user-albums.routes.ts
+++ src/apps/v1/user-albums/routes/private.user-albums.routes.ts
@@ -2,6 +2,6 @@ import express from "express";
 import userAlbums from "../controllers/user-albums.get.controller";
 const router: express.Router = express.Router();
 
-router.get(":/name", userAlbums);
+router.get("/:name", userAlbums);
 
 export default router;
