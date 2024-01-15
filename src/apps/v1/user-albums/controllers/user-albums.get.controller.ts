72d9c6228767fecfb161d8b3b970509d4d95a4af
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-01-15 12:40:04 +0700

    Added user validation

--- src/apps/v1/user-albums/controllers/user-albums.get.controller.ts
+++ src/apps/v1/user-albums/controllers/user-albums.get.controller.ts
@@ -1,14 +1,33 @@
 import { Response, Request } from "express";
 import logger from "../../../../libs/configs/logger";
-import { httpBadRequestResponse } from "../../../../utils/responses/httpErrorsResponses";
+import {
+  httpBadRequestResponse,
+  httpNotFoundResponse,
+} from "../../../../utils/responses/httpErrorsResponses";
 import client from "../../../../libs/configs/prisma";
+import { isUserExistByNameOrEmail } from "../../../../utils/isUser";
+import { UserWithOptionalChaining } from "../../../../interfaces/UserWithOptionalChaining";
+import getUserAlbums from "../services/getUserAlbums";
+import { Album } from "../../../../../generated/client";
 
 export default async function userAlbums(
   req: Request,
   res: Response
 ): Promise<void | Response> {
   try {
-    res.send("testtttt");
+    const { name } = req.params;
+
+    const user: Awaited<UserWithOptionalChaining | null> =
+      await isUserExistByNameOrEmail({ field: "name", value: name });
+
+    if (!user) return httpNotFoundResponse({ response: res });
+
+    delete user.email;
+    delete user.password;
+
+    const userAlbumsList: Awaited<Array<Album>> = await getUserAlbums(user.id);
+
+    res.send(userAlbumsList);
   } catch (err) {
     logger.error(err);
     return httpBadRequestResponse({ response: res });
