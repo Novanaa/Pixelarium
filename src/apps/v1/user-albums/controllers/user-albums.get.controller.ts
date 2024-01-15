f734e381396c4d734b3f8bfff5ffc48745128d78
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-01-15 13:22:04 +0700

    Added Get User Albums API Core Logic

--- src/apps/v1/user-albums/controllers/user-albums.get.controller.ts
+++ src/apps/v1/user-albums/controllers/user-albums.get.controller.ts
@@ -9,6 +9,13 @@ import { isUserExistByNameOrEmail } from "../../../../utils/isUser";
 import { UserWithOptionalChaining } from "../../../../interfaces/UserWithOptionalChaining";
 import getUserAlbums from "../services/getUserAlbums";
 import { Album } from "../../../../../generated/client";
+import sendJsonResultHttpResponse from "../../../../services/sendJsonResultHttpResponse";
+
+type UserAlbumsResponseData = {
+  owner: UserWithOptionalChaining;
+  user_albums: Array<Album>;
+  total_user_albums: number;
+};
 
 export default async function userAlbums(
   req: Request,
@@ -27,7 +34,13 @@ export default async function userAlbums(
 
     const userAlbumsList: Awaited<Array<Album>> = await getUserAlbums(user.id);
 
-    res.send(userAlbumsList);
+    const responseData: UserAlbumsResponseData = {
+      owner: user,
+      user_albums: userAlbumsList,
+      total_user_albums: userAlbumsList.length,
+    };
+
+    return sendJsonResultHttpResponse({ response: res, responseData });
   } catch (err) {
     logger.error(err);
     return httpBadRequestResponse({ response: res });
