f13a35bb7b16ab4a4254a3adb41069c2d6d212a9
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-01-16 01:56:17 +0700

    Registered Private Access Add Existing Picture Album API Routes Endpoint

--- src/apps/v1/user-albums/routes/private.user-albums.routes.ts
+++ src/apps/v1/user-albums/routes/private.user-albums.routes.ts
@@ -3,9 +3,12 @@ import {
   singleUserAlbum,
   userAlbums,
 } from "../controllers/user-albums.get.controller";
+import addAlbumPicture from "../controllers/user-albums.post.controller";
+import apiGrantAccess from "../../../../middlewares/apiGrantAccess";
 const router: express.Router = express.Router();
 
 router.get("/:name", userAlbums);
 router.get("/:name/:albumId", singleUserAlbum);
+router.post("/:name/:albumId/:uniquekey", apiGrantAccess, addAlbumPicture);
 
 export default router;
