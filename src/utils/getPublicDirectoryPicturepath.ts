0a4bda388368883db86a8c7093018ed57acda586
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-01-15 20:11:59 +0700

    Added new albumName params and types

--- src/utils/getPublicDirectoryPicturepath.ts
+++ src/utils/getPublicDirectoryPicturepath.ts
@@ -1,28 +1,34 @@
 import UsageType from "../interfaces/types/UsageTypes";
 import getPictureFilepath from "./getPictureFilepath";
+
+type GetPublicDirectoryPicturepathParams = {
+  usage: UsageType;
+  filename: string;
+  name?: string;
+  albumName?: string;
+};
+
 /**
- * The function `getPublicDirectoryPicturepath` returns the file path for a picture in the public
- * directory based on its usage and filename.
- * @export
- * @param {{
- *   usage: UsageType;
- *   filename: string;
- * }} {
- *   usage,
- *   filename,
- * }
- * @returns a string representing the public directory picture path.
+ * Retrieves the public directory picture path based on the provided parameters.
+ *
+ * @param {GetPublicDirectoryPicturepathParams} params - The parameters for retrieving the public directory picture path.
+ * @param {UsageType} params.usage - The usage type of the picture.
+ * @param {string} params.filename - The filename of the picture.
+ * @param {string} [params.name] - The name associated with the picture.
+ * @param {string} [params.albumName] - The album name associated with the picture.
+ * @returns {string} The public directory picture path.
  */
 export default function getPublicDirectoryPicturepath({
   usage,
   filename,
   name,
-}: {
-  usage: UsageType;
-  filename: string;
-  name?: string;
-}): string {
-  const pictureFilepath: string = getPictureFilepath({ usage, name });
+  albumName,
+}: GetPublicDirectoryPicturepathParams): string {
+  const pictureFilepath: string = getPictureFilepath({
+    usage,
+    name,
+    albumName,
+  });
   const publicDirectoryPicturepath: string = `./public/${pictureFilepath}/${filename}`;
 
   return publicDirectoryPicturepath;
