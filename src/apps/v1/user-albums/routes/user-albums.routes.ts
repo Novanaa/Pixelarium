4a95bf0056f350950f80f88677a2515f12108eb8
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-01-19 02:01:02 +0700

    Added middlewares to delete user album api routes endpoint

--- src/apps/v1/user-albums/routes/user-albums.routes.ts
+++ src/apps/v1/user-albums/routes/user-albums.routes.ts
@@ -9,13 +9,19 @@ import {
 } from "../controllers/user-albums.post.controller";
 import updateUserAlbum from "../controllers/user-albums.patch.controller";
 import deleteUserAlbum from "../controllers/user-albums.delete.controller";
+import jsonWebTokenAuthorization from "../../../../middlewares/jsonWebTokenAuthorization";
+import verifySessionToken from "../../../../middlewares/verifySessionToken";
 const router: express.Router = express.Router();
 
 router.get("/:name", userAlbums);
 router.post("/:name", addUserAlbum);
 router.get("/:name/:albumId", singleUserAlbum);
 router.patch("/:name/:albumId", updateUserAlbum);
-router.delete("/:name/:albumId", deleteUserAlbum);
+router.delete(
+  "/:name/:albumId",
+  [jsonWebTokenAuthorization, verifySessionToken],
+  deleteUserAlbum
+);
 router.post("/:name/:albumId/:uniquekey", addAlbumPicture);
 router.post("/:name/:albumId/:uniquekey", addAlbumPicture);
 
