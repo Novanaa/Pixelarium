137b91b66cf38be4ee0f36dcf72cde588a9fb2bc
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-01-19 01:52:30 +0700

    Added Delete User Album API Core Logic (Uncompleted)

---
+++ src/apps/v1/user-albums/controllers/user-albums.delete.controller.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+import logger from "../../../../libs/configs/logger";
+import { httpBadRequestResponse } from "../../../../utils/responses/httpErrorsResponses";
+import client from "../../../../libs/configs/prisma";
+
+export default async function deleteUserAlbum(
+  req: Request,
+  res: Response
+): Promise<void | Response> {
+  try {
+    res.send("testtt");
+  } catch (err) {
+    logger.error(err);
+    return httpBadRequestResponse({ response: res });
+  } finally {
+    await client.$disconnect();
+  }
+}
