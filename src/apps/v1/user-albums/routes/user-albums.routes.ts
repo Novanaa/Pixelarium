e5a9868d8415517bb63f8a9d6179c9b62803d2b8
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-01-18 16:45:27 +0700

    Registered Create new User Album API Endpoint

--- src/apps/v1/user-albums/routes/user-albums.routes.ts
+++ src/apps/v1/user-albums/routes/user-albums.routes.ts
@@ -3,10 +3,14 @@ import {
   singleUserAlbum,
   userAlbums,
 } from "../controllers/user-albums.get.controller";
-import addAlbumPicture from "../controllers/user-albums.post.controller";
+import {
+  addAlbumPicture,
+  addUserAlbum,
+} from "../controllers/user-albums.post.controller";
 const router: express.Router = express.Router();
 
 router.get("/:name", userAlbums);
+router.post("/:name", addUserAlbum);
 router.get("/:name/:albumId", singleUserAlbum);
 router.post("/:name/:albumId/:uniquekey", addAlbumPicture);
 
