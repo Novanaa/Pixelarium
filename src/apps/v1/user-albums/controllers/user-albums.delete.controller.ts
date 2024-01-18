ca3a147a8b7e7efbb12bbbc5a6f060f0bfcd5aa7
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-01-19 02:27:45 +0700

    Added Delete User Album API Core Logic

--- src/apps/v1/user-albums/controllers/user-albums.delete.controller.ts
+++ src/apps/v1/user-albums/controllers/user-albums.delete.controller.ts
@@ -1,14 +1,80 @@
 import { Request, Response } from "express";
 import logger from "../../../../libs/configs/logger";
-import { httpBadRequestResponse } from "../../../../utils/responses/httpErrorsResponses";
+import {
+  httpBadRequestResponse,
+  httpNotFoundResponse,
+} from "../../../../utils/responses/httpErrorsResponses";
 import client from "../../../../libs/configs/prisma";
+import validateRequestIDParams from "../../../../utils/validateRequestIDParams";
+import { UserWithOptionalChaining } from "../../../../interfaces/UserWithOptionalChaining";
+import { isUserExistByNameOrEmail } from "../../../../utils/isUser";
+import UserAlbums from "../interfaces/UserAlbums";
+import getUserAlbum from "../services/getUserAlbum";
+import deleteUserAlbumData from "../services/deleteUserAlbumData";
+import FilesSystem from "../../../../services/FilesSystem";
+import getPublicDirectoryPicturepath from "../../../../utils/getPublicDirectoryPicturepath";
+import sendJsonResultHttpResponse from "../../../../services/sendJsonResultHttpResponse";
+
+type DeleteUserAlbumResponseData = {
+  owner: UserWithOptionalChaining;
+  deleted_data: UserAlbums;
+};
 
 export default async function deleteUserAlbum(
   req: Request,
   res: Response
 ): Promise<void | Response> {
+  const fs: FilesSystem = new FilesSystem();
   try {
-    res.send("testtt");
+    const { name, albumId } = req.params;
+
+    const albumIdValidation: void | Response = validateRequestIDParams({
+      id: albumId,
+      response: res,
+      errorMessege: "The album id must be a valid id!",
+    });
+
+    if (albumIdValidation) return;
+
+    const user: Awaited<UserWithOptionalChaining | null> =
+      await isUserExistByNameOrEmail({ field: "name", value: name });
+
+    if (!user) return httpNotFoundResponse({ response: res });
+
+    delete user.email;
+    delete user.password;
+
+    const userAlbum: Awaited<UserAlbums | null> = await getUserAlbum(
+      Number(albumId)
+    );
+
+    if (!userAlbum)
+      return httpNotFoundResponse({
+        response: res,
+        errorMessage: "The album doesn't exist",
+      });
+
+    const dirpath: string = getPublicDirectoryPicturepath({
+      usage: "albums",
+      filename: "",
+      name: user.name,
+      albumName: userAlbum.title,
+    });
+
+    await deleteUserAlbumData(userAlbum.id);
+
+    fs.deleteDirectory(dirpath);
+
+    const responseData: DeleteUserAlbumResponseData = {
+      owner: user,
+      deleted_data: userAlbum,
+    };
+
+    return sendJsonResultHttpResponse({
+      response: res,
+      responseData,
+      options: { resultKey: "deleted" },
+    });
   } catch (err) {
     logger.error(err);
     return httpBadRequestResponse({ response: res });
