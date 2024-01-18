01219c45aec3952f8e0fe1f40c34e61d2a3e1fb9
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-01-19 01:53:50 +0700

    Registered Delete User Album API Routes Endpoint

--- src/apps/v1/user-albums/routes/user-albums.routes.ts
+++ src/apps/v1/user-albums/routes/user-albums.routes.ts
@@ -8,12 +8,15 @@ import {
   addUserAlbum,
 } from "../controllers/user-albums.post.controller";
 import updateUserAlbum from "../controllers/user-albums.patch.controller";
+import deleteUserAlbum from "../controllers/user-albums.delete.controller";
 const router: express.Router = express.Router();
 
 router.get("/:name", userAlbums);
 router.post("/:name", addUserAlbum);
 router.get("/:name/:albumId", singleUserAlbum);
 router.patch("/:name/:albumId", updateUserAlbum);
+router.delete("/:name/:albumId", deleteUserAlbum);
+router.post("/:name/:albumId/:uniquekey", addAlbumPicture);
 router.post("/:name/:albumId/:uniquekey", addAlbumPicture);
 
 export default router;
