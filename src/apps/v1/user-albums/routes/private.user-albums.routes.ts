d9a889054a6a0e5e7e25e7dd347bfee5315cc876
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-01-19 00:33:51 +0700

    Registered Private Access Update User Album API Routes Endpoint

--- src/apps/v1/user-albums/routes/private.user-albums.routes.ts
+++ src/apps/v1/user-albums/routes/private.user-albums.routes.ts
@@ -8,11 +8,13 @@ import {
   addUserAlbum,
 } from "../controllers/user-albums.post.controller";
 import apiGrantAccess from "../../../../middlewares/apiGrantAccess";
+import updateUserAlbum from "../controllers/user-albums.patch.controller";
 const router: express.Router = express.Router();
 
 router.get("/:name", userAlbums);
 router.post("/:name", apiGrantAccess, addUserAlbum);
 router.get("/:name/:albumId", singleUserAlbum);
+router.get("/:name/:albumId", apiGrantAccess, updateUserAlbum);
 router.post("/:name/:albumId/:uniquekey", apiGrantAccess, addAlbumPicture);
 
 export default router;
