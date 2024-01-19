e5e577b4e67ece2c4f5df9a88b5c87b0d7ce1ae0
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-01-19 19:55:11 +0700

    Removed unused prisma accelerate

--- src/libs/configs/prisma.ts
+++ src/libs/configs/prisma.ts
@@ -1,11 +1,10 @@
-import { PrismaClient } from "../../../generated/client/edge";
-import { withAccelerate } from "@prisma/extension-accelerate";
+import { PrismaClient } from "../../../generated/client";
 import { DATABASE_URL } from "../../const/env";
 
 /* The code is creating a new instance of the PrismaClient class and assigning it to the `client`
 constant. */
 const client = new PrismaClient({
   datasources: { db: { url: DATABASE_URL } },
-}).$extends(withAccelerate());
+});
 
 export default client;
