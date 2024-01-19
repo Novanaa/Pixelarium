8e8157bd80526c49d8f144d34045d69897d42daf
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-01-19 21:05:47 +0700

    Added Get User Subscription Status API Core Logic

--- src/apps/v1/subscriptions/controllers/subscription.get.controller.ts
+++ src/apps/v1/subscriptions/controllers/subscription.get.controller.ts
@@ -1,6 +1,23 @@
 import { Request, Response } from "express";
 import pricesList from "../../../../../resources/prices-list.json";
+import logger from "../../../../libs/configs/logger";
+import { httpBadRequestResponse } from "../../../../utils/responses/httpErrorsResponses";
+import client from "../../../../libs/configs/prisma";
 
 export function prices(_: Request, res: Response): Response {
   return res.status(200).json(pricesList);
+}
+
+export async function userSubscriptionStatus(
+  req: Request,
+  res: Response
+): Promise<void | Response> {
+  try {
+    res.send("testttt");
+  } catch (err) {
+    logger.error(err);
+    return httpBadRequestResponse({ response: res });
+  } finally {
+    await client.$disconnect();
+  }
 }
