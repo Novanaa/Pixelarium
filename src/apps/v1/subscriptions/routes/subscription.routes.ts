bf74b2f1d7c00a02907badbe82b7b22f116cc128
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-01-19 21:39:08 +0700

    Registered Deactivate User Subscription API Routes Endpoint

--- src/apps/v1/subscriptions/routes/subscription.routes.ts
+++ src/apps/v1/subscriptions/routes/subscription.routes.ts
@@ -3,9 +3,11 @@ import {
   prices,
   userSubscriptionStatus,
 } from "../controllers/subscription.get.controller";
+import deacitivateUserSubscription from "../controllers/subscription.delete.controller";
 const router: express.Router = express.Router();
 
 router.get("/prices", prices);
 router.get("/status/:name", userSubscriptionStatus);
+router.delete("/deactivate/:name", deacitivateUserSubscription);
 
 export default router;
