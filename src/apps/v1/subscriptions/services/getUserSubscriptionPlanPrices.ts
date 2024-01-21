0a9fe0b676dfb1471b54ebd258f5e659bb83ca05
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-01-21 13:14:24 +0700

    Added Get User Subscription Plan Prices Services

---
+++ src/apps/v1/subscriptions/services/getUserSubscriptionPlanPrices.ts
@@ -0,0 +1,32 @@
+import pricesList from "../../../../../resources/prices-list.json";
+
+type Plan = "Gold" | "Diamond" | "Netherite";
+
+export type GetUserSubscriptionPlanPricesParams = {
+  quantity: number;
+  plan: Plan;
+};
+
+/**
+ * Calculates the total price for a user's subscription plan based on the plan and quantity.
+ *
+ * @param {GetUserSubscriptionPlanPricesParams} params - The parameters for calculating the price.
+ * @param {number} params.quantity - The quantity of the subscription plan.
+ * @param {Plan} params.plan - The subscription plan.
+ * @returns {number | null} - The total price for the user's subscription plan, or null if the plan is not found.
+ */
+export default function getUserSubscriptionPlanPrices({
+  plan,
+  quantity = 1,
+}: GetUserSubscriptionPlanPricesParams): number | null {
+  const userSubscriptionPlanPrices = {
+    Gold: pricesList[1].price * quantity,
+    Diamond: pricesList[2].price * quantity,
+    Netherite: pricesList[3].price * quantity,
+  };
+
+  const userSubscriptionPlanPricesMapping: number | null =
+    userSubscriptionPlanPrices[plan] || null;
+
+  return userSubscriptionPlanPricesMapping;
+}
