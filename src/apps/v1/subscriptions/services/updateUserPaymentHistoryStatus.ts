a4b46d25df5950634b11f097afec1da531ce7f8a
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-01-22 19:22:58 +0700

    Added Update User Payment History Status Services

---
+++ src/apps/v1/subscriptions/services/updateUserPaymentHistoryStatus.ts
@@ -0,0 +1,31 @@
+import {
+  PaymentsHistory,
+  subscriptionStatus,
+} from "../../../../../generated/client";
+import client from "../../../../libs/configs/prisma";
+
+type UpdateUserPaymentHistoryStatusParams = {
+  orderId: string;
+  status: subscriptionStatus;
+};
+
+/**
+ * Updates the payment history status for a user.
+ *
+ * @param {UpdateUserPaymentHistoryStatusParams} params - The parameters for updating the payment history status.
+ * @param {string} params.orderId - The ID of the order.
+ * @param {subscriptionStatus} params.status - The new status for the payment history.
+ * @returns {Promise<PaymentsHistory>} - A promise that resolves to the updated payment history.
+ */
+export default async function updateUserPaymentHistoryStatus({
+  orderId,
+  status,
+}: UpdateUserPaymentHistoryStatusParams): Promise<PaymentsHistory> {
+  const updatedData: Awaited<PaymentsHistory> =
+    await client.paymentsHistory.update({
+      where: { order_id: orderId },
+      data: { status },
+    });
+
+  return updatedData;
+}
