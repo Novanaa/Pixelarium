09933885c08c0b135a4f5171130fed7a97971804
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-01-22 20:30:15 +0700

    Registered Get User Payments Histories API Endpoint

--- src/apps/v1/payments-histories/routes/payments-histories.routes.ts
+++ src/apps/v1/payments-histories/routes/payments-histories.routes.ts
@@ -1,4 +1,7 @@
 import express from "express";
+import userPaymentsHistories from "../controllers/payments-histories.get.controller";
 const router: express.Router = express.Router();
+
+router.get("/:name", userPaymentsHistories);
 
 export default router;
