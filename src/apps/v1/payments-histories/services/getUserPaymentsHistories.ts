bda443f960757eeffc2125a8b3aee7024fbcb520
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-01-22 20:43:25 +0700

    Added Get User Payments Histories Data Services

---
+++ src/apps/v1/payments-histories/services/getUserPaymentsHistories.ts
@@ -0,0 +1,16 @@
+import { PaymentsHistory } from "../../../../../generated/client";
+import client from "../../../../libs/configs/prisma";
+
+/**
+ * Retrieves the payment histories for a given user.
+ *
+ * @param user_id - The ID of the user.
+ * @returns A promise that resolves to an array of payment histories.
+ */
+export default async function getUserPaymentsHistories(
+  user_id: number
+): Promise<Array<PaymentsHistory>> {
+  return client.paymentsHistory.findMany({
+    where: { user_id },
+  }) as Promise<Array<PaymentsHistory>>;
+}
