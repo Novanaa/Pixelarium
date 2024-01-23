cb5b1d5073ad9000604776810873c7b182335041
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-01-23 13:04:51 +0700

    Registered Update User Payment History Status API Endpoint

--- src/apps/v1/payments-histories/routes/payments-histories.routes.ts
+++ src/apps/v1/payments-histories/routes/payments-histories.routes.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import userPaymentsHistories from "../controllers/payments-histories.get.controller";
+import updateUserPaymentHistory from "../controllers/payments-histories.patch.controller";
 const router: express.Router = express.Router();
 
 router.get("/:name", userPaymentsHistories);
+router.patch("/:name/:paymentId", updateUserPaymentHistory);
 
 export default router;
