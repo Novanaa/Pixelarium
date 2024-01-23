167f4da2f194904ee3e26072dccfcbc909b7e973
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-01-23 23:35:00 +0700

    Added Add new User Payment History Request Body Schema Validation

---
+++ src/validations/addUserPaymentHistoryRequestBodyValidation.ts
@@ -0,0 +1,26 @@
+import { default as joi } from "joi";
+import AddUserPaymentHistorySchemaValidation from "./interfaces/types/AddUserPaymentHistorySchemaValidation";
+import { Request } from "express";
+import orderIdPattern from "../const/readonly/orderIdPattern";
+import userPlans from "../const/readonly/userPlans";
+
+export default function addUserPaymentHistoryRequestBodyValidation(
+  request: Request
+): joi.ValidationResult<AddUserPaymentHistorySchemaValidation> {
+  const schemaValidation: joi.ObjectSchema<AddUserPaymentHistorySchemaValidation> =
+    joi.object({
+      order_id: joi.string().required().regex(orderIdPattern),
+      interval_count: joi.number().required(),
+      plan: joi.string().valid(userPlans).required(),
+      status: joi.string().valid("failed", "success", "pending").required(),
+      order_date: joi.date().required(),
+      amount: joi
+        .object({
+          USD: joi.number().required(),
+          IDR: joi.number(),
+        })
+        .required(),
+    });
+
+  return schemaValidation.validate(request.body);
+}
