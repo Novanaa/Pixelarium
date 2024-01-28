1505f1436c352a086277003ac68251c3f04ddc2d
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-01-28 03:34:35 -0500

    Refactored some lines of codes

--- src/utils/saveFile.ts
+++ src/utils/saveFile.ts
@@ -2,32 +2,29 @@ import { UploadedFile } from "express-fileupload";
 import { Response } from "express";
 import { httpBadRequestResponse } from "./responses/httpErrorsResponses";
 
-/**
- * The saveFile function saves an uploaded file to a specified path and handles any errors that occur
- * during the process.
- * @export
- * @param {{
- *   file: UploadedFile;
- *   path: string;
- *   response: Response;
- * }} {
- *   file,
- *   path,
- *   except,
- *   response,
- * }
- *
- * @returns void
- */
-export default function saveFile({
-  file,
-  path,
-  response,
-}: {
+type SaveFileParams = {
   file: UploadedFile;
   path: string;
   response: Response;
-}) {
+};
+
+/**
+ * Saves a file to the specified path.
+ *
+ * @param {SaveFileParams} params - The parameters for saving the file.
+ * @param {UploadedFile} params.file - The file to be saved.
+ * @param {string} params.path - The path where the file should be saved.
+ * @param {Response} params.response - The response object to send HTTP responses.
+ *
+ * @returns {Promise<void>} - A promise that resolves when the file is saved successfully.
+ *
+ * @throws {Error} - If there is an error while saving the file.
+ */
+export default async function saveFile({
+  file,
+  path,
+  response,
+}: SaveFileParams): Promise<void> {
   file.mv(path, (err) => {
     if (err)
       return httpBadRequestResponse({
