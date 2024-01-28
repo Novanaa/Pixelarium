7789c7f453f2b81cbe73e1a42f6109eb88df7dbf
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-01-28 07:43:26 -0500

    Wrapped Apps Components with Shadcn-ui Themes Provider

--- pages/src/pages/_app.tsx
+++ pages/src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ThemeProvider } from "@/lib/ThemesProvider";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
