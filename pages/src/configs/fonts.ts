ff675fecd21f65705264d7f99695aee28ac439ff
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-01-29 01:09:49 -0500

    Updated plus jakarta sans fonts configuration

--- pages/src/configs/fonts.ts
+++ pages/src/configs/fonts.ts
@@ -1,31 +1,9 @@
-import { NextFontConfiguration } from "@/interfaces/types/NextFontConfiguration";
-import { NextFont } from "next/dist/compiled/@next/font";
+import { NextFontWithVariable } from "next/dist/compiled/@next/font";
 import { Plus_Jakarta_Sans } from "next/font/google";
 
-const nextFontConfiguration: NextFontConfiguration = {
+export const jakartaSans: NextFontWithVariable = Plus_Jakarta_Sans({
   subsets: ["latin"],
+  variable: "--font-jakarta",
   weight: "400",
   style: ["normal"],
-};
-
-export const jakartaSans: NextFont = Plus_Jakarta_Sans(nextFontConfiguration);
-
-export const jakartaSansMedium: NextFont = Plus_Jakarta_Sans({
-  ...nextFontConfiguration,
-  weight: "500",
-});
-
-export const jakartaSansSemiBold: NextFont = Plus_Jakarta_Sans({
-  ...nextFontConfiguration,
-  weight: "600",
-});
-
-export const jakartaSansBold: NextFont = Plus_Jakarta_Sans({
-  ...nextFontConfiguration,
-  weight: "700",
-});
-
-export const jakartaSansExtraBold: NextFont = Plus_Jakarta_Sans({
-  ...nextFontConfiguration,
-  weight: "800",
 });
