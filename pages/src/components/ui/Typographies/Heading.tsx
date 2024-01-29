5cc5ce17125dfb97209e1f408156696de452a346
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-01-29 00:27:26 -0500

    Removed underline from heading two

--- pages/src/components/ui/Typographies/Heading.tsx
+++ pages/src/components/ui/Typographies/Heading.tsx
@@ -23,7 +23,7 @@ export function HeadingOne({ children }: ReactNodeChild): React.ReactNode {
  */
 export function HeadingTwo({ children }: ReactNodeChild): React.ReactNode {
   return (
-    <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+    <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">
       {children}
     </h2>
   );
