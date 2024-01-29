f43799baa8e42ad23e84a4730b14bcec25abdf8b
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-01-29 00:30:01 -0500

    Updated default ui-themes

--- pages/src/pages/_app.tsx
+++ pages/src/pages/_app.tsx
@@ -6,7 +6,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider
       attribute="class"
-      defaultTheme="system"
+      defaultTheme="dark"
       enableSystem
       disableTransitionOnChange
     >
