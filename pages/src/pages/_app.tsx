e70d641f0ba501d0af93d1adbfb9acf6d6af1398
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-01-29 00:50:17 -0500

    Configured next google plus jakarta sans fonts

--- pages/src/pages/_app.tsx
+++ pages/src/pages/_app.tsx
@@ -1,16 +1,19 @@
 import { ThemeProvider } from "@/lib/ThemesProvider";
+import { jakartaSans } from "@/configs/fonts";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="dark"
-      enableSystem
-      disableTransitionOnChange
-    >
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <main className={jakartaSans.className}>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="dark"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </main>
   );
 }
