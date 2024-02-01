54d9d52393738a7288d81fff99b1e06ea95205d7
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-02-01 03:51:37 -0500

    Added constant readonly cookies options

---
+++ src/apps/v1/auth/const/readonly/cookiesOptions.ts
@@ -0,0 +1,12 @@
+import { CookieOptions } from "express";
+import { NODE_ENV } from "../../../../../const/env";
+
+const isSecured = NODE_ENV == "production" ? true : false;
+
+const cookiesOptions: CookieOptions = {
+  secure: isSecured,
+  httpOnly: true,
+  sameSite: "strict",
+};
+
+export default cookiesOptions;
