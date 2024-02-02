18cc367bdb7b8497a2d3115d1665667377bd3ec6
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-02-02 03:51:46 -0500

    Added default error messege to error exception

--- pages/src/exceptions/errorException.ts
+++ pages/src/exceptions/errorException.ts
@@ -1,3 +1,5 @@
 export default function errorException(messege?: string): Error {
-  throw new Error(messege || "");
+  const defaultErrorMessege: string =
+    "An unexpected error has occurred, preventing the successful execution of the requested operation. Please try again later or contact our support team for further assistance.";
+  throw new Error(messege || defaultErrorMessege);
 }
