7569da4ff82fe757100238d7edf3489489e921d5
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-02-02 07:32:53 -0500

    Updated set user login workflow

--- pages/src/services/setUserLogin.ts
+++ pages/src/services/setUserLogin.ts
@@ -1,31 +1,23 @@
-import axios, { AxiosError } from "axios";
-import apiUrlEndpoint from "@/constant/readonly/apiUrlEndpoint";
-import User from "@/interfaces/types/User";
-import ErrorResponse from "@/interfaces/types/ErrorResponse";
 import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
+import getCookiesData from "@/utils/getCookiesData";
+import CookiesData from "@/interfaces/types/Cookies";
+import defaultErrorMessege from "@/constant/readonly/defaultErrorMessege";
 
 type SetUserLoginParams = {
-  name: string | null;
   router: AppRouterInstance;
 };
 
 export default async function setUserLogin({
-  name,
   router,
 }: SetUserLoginParams): Promise<void> {
   try {
-    const user: Awaited<User | null> = await axios.get(
-      `${apiUrlEndpoint}/v1/users/${name}`,
-    );
+    const cookiesData: Awaited<CookiesData> = await getCookiesData();
 
-    if (user) router.push("/dashboard");
-  } catch (err) {
-    const error = err as AxiosError<ErrorResponse>;
-
-    if (error.response?.data) {
-      const errorMessege: string = error.response?.data.messege as string;
-
-      router.push(`/auth/login?messege=${errorMessege}&type=failed`);
+    const { isLoggedIn, session } = cookiesData.cookies;
+    if (isLoggedIn == "yes" && session) {
+      router.push("/dashboard");
     }
+  } catch (err) {
+    if (err) throw new Error(defaultErrorMessege);
   }
 }
