f39f84f6115c5ed051287f3a44dd6c8b80a32bc5
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-02-02 07:28:17 -0500

    Refactored some lines of codes

--- pages/src/exceptions/errorException.ts
+++ pages/src/exceptions/errorException.ts
@@ -1,5 +1,5 @@
+import defaultErrorMessege from "@/constant/readonly/defaultErrorMessege";
+
 export default function errorException(messege?: string): Error {
-  const defaultErrorMessege: string =
-    "An unexpected error has occurred, preventing the successful execution of the requested operation. Please try again later or contact our support team for further assistance.";
   throw new Error(messege || defaultErrorMessege);
 }
