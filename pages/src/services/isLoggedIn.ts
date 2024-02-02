6709b71383cd9059c807bc973652fa2f4282f46c
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-02-02 07:16:42 -0500

    Refactored some lines of codes

--- pages/src/services/isLoggedIn.ts
+++ pages/src/services/isLoggedIn.ts
@@ -1,17 +1,16 @@
 import CookiesData from "@/interfaces/types/Cookies";
-import axios, { AxiosResponse } from "axios";
+import getCookiesData from "@/utils/getCookiesData";
 
 /**
  * Checks if the user is logged in.
- * 
+ *
  * @returns {Promise<boolean>} A promise that resolves to true if the user is logged in, and false otherwise.
  */
 export default async function isLoggedIn(): Promise<boolean> {
   try {
-    const cookies: Awaited<AxiosResponse<CookiesData>> =
-      await axios.get("/api/v1/cookies");
+    const cookiesData: Awaited<CookiesData> = await getCookiesData();
 
-    const { isLoggedIn, session } = cookies.data.cookies;
+    const { isLoggedIn, session } = cookiesData.cookies;
 
     if ((!isLoggedIn && !session) || !session) return false;
 
