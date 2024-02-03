cccbdfeac900e34124ca5e1f2f6a0ac3a8684893
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-02-02 22:56:42 -0500

    Added configuration for dummy image hostname

--- pages/next.config.js
+++ pages/next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "fastly.picsum.photos",
+        port: "",
+      },
+    ],
+  },
   reactStrictMode: true,
   experimental: {
     missingSuspenseWithCSRBailout: false,
