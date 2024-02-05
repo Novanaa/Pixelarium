f879213c348ac2e62a0183a6406a780ebb5a9246
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-02-05 12:59:20 -0500

    Added direct username params to get user data services

--- pages/src/services/getUserData.ts
+++ pages/src/services/getUserData.ts
@@ -1,20 +1,21 @@
 import axios, { AxiosResponse } from "axios";
 import apiUrlEndpoint from "@/constant/readonly/apiUrlEndpoint";
 import User from "@/interfaces/types/User";
+import getUsername from "@/utils/getUsername";
 
 /**
  * Retrieves user data from the API based on the provided name.
  *
- * @param name - The name of the user to retrieve data for.
  * @returns A Promise that resolves to the user data if successful, or null if an error occurs.
  */
-export default async function getUserData(name: string): Promise<User | null> {
+export default async function getUserData(): Promise<User | null> {
   try {
+    const name: Awaited<string> = await getUsername();
     const user: Awaited<AxiosResponse> = await axios.get(
       `${apiUrlEndpoint}/v1/users/${name}`,
     );
 
-    return user.data as User;
+    return user.data.user as User;
   } catch (err) {
     return null;
   }
