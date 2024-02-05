18a23fd641129c686a9f1e88c124e37adbecfc89
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-02-05 14:18:40 -0500

    Added a several props types

--- pages/src/components/interfaces/types/NavbarMobileMenu.d.ts
+++ pages/src/components/interfaces/types/NavbarMobileMenu.d.ts
@@ -3,6 +3,8 @@ import React from "react";
 type NavbarMobileMenu = {
   isNavbarOpen: boolean;
   setIsNavbarOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  isLoading?: boolean;
+  isLogin?: boolean;
 };
 
 export default NavbarMobileMenu;
