e6d95ed8d5291d5542dbbd9e2a376950bc35f564
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-02-05 13:43:13 -0500

    Updated href link when user successfully logged in

--- pages/src/services/setUserLogin.ts
+++ pages/src/services/setUserLogin.ts
@@ -2,6 +2,8 @@ import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.share
 import getCookiesData from "@/utils/getCookiesData";
 import CookiesData from "@/interfaces/types/Cookies";
 import defaultErrorMessege from "@/constant/readonly/defaultErrorMessege";
+import getUserData from "./getUserData";
+import DecodedUser from "@/interfaces/types/DecodedUser";
 
 type SetUserLoginParams = {
   router: AppRouterInstance;
@@ -12,10 +14,11 @@ export default async function setUserLogin({
 }: SetUserLoginParams): Promise<void> {
   try {
     const cookiesData: Awaited<CookiesData> = await getCookiesData();
+    const user: Awaited<DecodedUser | null> = await getUserData();
 
     const { isLoggedIn, session } = cookiesData.cookies;
     if (isLoggedIn == "yes" && session) {
-      router.push("/dashboard");
+      router.push("/profile/" + user?.name);
     }
   } catch (err) {
     if (err) throw new Error(defaultErrorMessege);
