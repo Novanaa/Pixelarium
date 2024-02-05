c264ee641e7f62e4d0b838a19487f15ccca32881
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-02-05 06:58:16 -0500

    Added Terms of Service Page Metadata

--- pages/src/app/tos/page.tsx
+++ pages/src/app/tos/page.tsx
@@ -2,6 +2,12 @@ import React from "react";
 import renderMDX from "@/services/renderMDX";
 import type { RenderMDXReturnType } from "@/services/renderMDX";
 import MDX from "@/components/modules/MDX";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Pixelarium - Terms of Service",
+  description: "Pixelarium Terms of Service",
+};
 
 async function TermOfServices() {
   const { loadMDXContent }: Awaited<RenderMDXReturnType> = await renderMDX(
