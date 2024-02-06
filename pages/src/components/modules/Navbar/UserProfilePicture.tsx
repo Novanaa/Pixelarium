53ca75f61d8014d609c2732569e58d4a0209bea8
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-02-06 05:57:14 -0500

    Added Picture Profile Sidebar to User Profile Picture Parent Component

--- pages/src/components/modules/Navbar/UserProfilePicture.tsx
+++ pages/src/components/modules/Navbar/UserProfilePicture.tsx
@@ -4,29 +4,41 @@ import UserProfilePictureProps from "@/components/interfaces/types/UserProfilePi
 import { Skeleton } from "@/components/ui/skeleton";
 import DecodedUser from "@/interfaces/types/DecodedUser";
 import getUserData from "@/services/getUserData";
-import Link from "next/link";
-import React, { use } from "react";
+import React, { use, useState } from "react";
+import PictureProfileSidebar from "./PictureProfileSidebar";
 
 const userQuery = getUserData();
 function UserProfilePicture({ isLoading }: UserProfilePictureProps) {
+  const [isSidebarOpen, setIsSidebarOpen] = useState<boolean>(false);
+
   const user = use<DecodedUser | null>(userQuery);
 
   return (
-    <Link href={"/profile/" + user?.name}>
-      {!isLoading ? (
-        <picture>
-          <img
-            src={user?.picture}
-            alt={"@" + user?.name}
-            className="h-8 w-8 rounded-full border"
-            width={8}
-            height={8}
-          />
-        </picture>
-      ) : (
-        <Skeleton className="h-8 w-8 rounded-full" />
-      )}
-    </Link>
+    <>
+      <section>
+        {!isLoading ? (
+          <picture onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
+            <img
+              src={user?.picture}
+              alt={"@" + user?.name}
+              className="h-8 w-8 rounded-full border"
+              width={8}
+              height={8}
+            />
+          </picture>
+        ) : (
+          <Skeleton className="h-8 w-8 rounded-full" />
+        )}
+      </section>
+      {isSidebarOpen ? (
+        <PictureProfileSidebar
+          isOpen={isSidebarOpen}
+          setIsOpen={setIsSidebarOpen}
+          user={user}
+          isLoading={isLoading}
+        />
+      ) : null}
+    </>
   );
 }
 
