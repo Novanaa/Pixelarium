4993646f5d2c5fb01c1249f124a06361d69b82f1
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-02-07 08:25:11 -0500

    Added request headers x-pathname and new configuration matcher

--- pages/src/middleware.ts
+++ pages/src/middleware.ts
@@ -8,6 +8,8 @@ export async function middleware(req: NextRequest): Promise<NextResponse> {
   const user: Awaited<DecodedUser | null> = await getServerSideUserData();
   const isLogin = getServerSideUserLoginStatus();
   const nextUrlPathname: string = req.nextUrl.pathname;
+  const requestHeaders = new Headers(req.headers);
+  requestHeaders.set("x-pathname", nextUrlPathname);
 
   if (nextUrlPathname.startsWith("/auth")) {
     if (nextUrlPathname.startsWith("/auth/login")) {
@@ -27,9 +29,13 @@ export async function middleware(req: NextRequest): Promise<NextResponse> {
       return NextResponse.redirect(new URL(`/profile/${user?.name}`, req.url));
   }
 
-  return NextResponse.next();
+  return NextResponse.next({
+    request: {
+      headers: requestHeaders,
+    },
+  });
 }
 
 export const config = {
-  matcher: ["/profile/:name*", "/auth/(.*)"],
+  matcher: ["/profile/:name*", "/auth/(.*)", "/developers/(.*)"],
 };
