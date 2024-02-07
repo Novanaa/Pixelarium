9b74768c49c7bd0b348e6d0c5fd318f848f77f4f
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-02-07 08:31:48 -0500

    Added Developers APIS Docs Page

---
+++ pages/src/app/developers/apis/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function DevelopersAPISDocs(): React.ReactElement {
+  return (
+    <main className="relative top-[4rem] h-full w-full border px-10">
+      testtt
+    </main>
+  );
+}
