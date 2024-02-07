1c98d91cf09902b855b72ca4e396c0752db1ca2d
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-02-07 10:44:56 -0500

    Added developers docs sidebar component to docs page

--- pages/src/app/developers/apis/page.tsx
+++ pages/src/app/developers/apis/page.tsx
@@ -1,9 +1,10 @@
+import DevelopersDocsSidebar from "@/components/modules/DevelopersDocsSidebar";
 import React from "react";
 
 export default function DevelopersAPISDocs(): React.ReactElement {
   return (
-    <main className="relative top-[4rem] h-full w-full border px-10">
-      testtt
+    <main className="relative top-[3.9rem] flex h-full w-full px-8 2xl:px-16">
+      <DevelopersDocsSidebar />
     </main>
   );
 }
