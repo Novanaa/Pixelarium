00e76761a697760c359929d5c6a9473b8c9cfeec
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-02-09 05:10:34 -0500

    Added a several rehype plugins and configuration

--- pages/src/services/renderMDX.ts
+++ pages/src/services/renderMDX.ts
@@ -11,10 +11,15 @@ import remarkGfm from "remark-gfm";
 import rehypeSlug from "rehype-slug";
 import { VFile } from "vfile";
 import type FrontMatter from "@/interfaces/types/FrontMatter";
+import { visit } from "unist-util-visit";
+import type TocElementData from "@/interfaces/types/Toc";
+// @ts-expect-error Module doesn't support ts
+import addClasses from "rehype-add-classes";
 
 export interface RenderMDXReturnType {
   loadMDXContent: string;
   frontMatter: FrontMatter;
+  toc: Array<TocElementData>;
 }
 
 export default async function renderMDX(
@@ -25,6 +30,7 @@ export default async function renderMDX(
   const { data: frontMatter, content }: matter.GrayMatterFile<string> =
     matter(mdxContent);
 
+  const toc: Array<TocElementData> = [];
   const loadMdxContent: VFile = await unified()
     // @ts-expect-error missing types
     .data("settings", { fragment: true })
@@ -32,17 +38,29 @@ export default async function renderMDX(
     .use(remarkParse)
     .use(remarkRehype)
     .use(rehypePrettyCode, {
-      theme: "dark-plus",
+      theme: "slack-dark",
       keepBackground: false,
     })
     .use(rehypeStringify)
     .use(rehypeSlug)
     .use(remarkGfm)
     .use(rehypeAutolinkHeadings)
+    .use(() => {
+      return (tree) => {
+        visit(tree, "element", (node: TocElementData) => {
+          if (node.tagName == "h2") toc.push(node);
+        });
+      };
+    })
+    .use(addClasses, {
+      a: "anchor",
+      'h1, h2': "heading"
+    })
     .process(content);
 
   return {
     frontMatter: frontMatter as FrontMatter,
     loadMDXContent: String(loadMdxContent.value),
+    toc,
   };
 }
