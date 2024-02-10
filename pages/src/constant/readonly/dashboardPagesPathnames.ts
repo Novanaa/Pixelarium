db7b4c8af3844aa6d1ef11a167164d82dcec6a56
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-02-09 21:55:31 -0500

    Added constant readonlt dashboard pages pathname list

---
+++ pages/src/constant/readonly/dashboardPagesPathnames.ts
@@ -0,0 +1,12 @@
+const dashboardPagesPathnames: Array<string> = [
+  "/accounts/settings",
+  "/profile",
+  "/profile/user?tabActive=favorites",
+  "/profile/user?active=upload",
+  "/profile/user?tabActive=gallery",
+  "/profile/user?tabActive=albums",
+  "/accounts/payment/history",
+  "/accounts/settings/developers",
+];
+
+export default dashboardPagesPathnames;
