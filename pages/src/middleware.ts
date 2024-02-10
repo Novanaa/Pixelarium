e8fca0e41a7a5ac54dc1b84157f68efeb5d89e3f
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-02-09 21:56:17 -0500

    Added a several validation for user profile page and payment history page

--- pages/src/middleware.ts
+++ pages/src/middleware.ts
@@ -3,11 +3,18 @@ import type { NextRequest } from "next/server";
 import getServerSideUserLoginStatus from "./services/getServerSideUserLoginStatus";
 import DecodedUser from "./interfaces/types/DecodedUser";
 import getServerSideUserData from "./services/getServerSideUserData";
+import tabActiveName from "./constant/readonly/tabActiveName";
+import queryString, { ParsedQuery } from "query-string";
 
 export async function middleware(req: NextRequest): Promise<NextResponse> {
   const user: Awaited<DecodedUser | null> = await getServerSideUserData();
   const isLogin = getServerSideUserLoginStatus();
   const nextUrlPathname: string = req.nextUrl.pathname;
+  const nextUrlPathnameWithSearch: string =
+    req.nextUrl.pathname + req.nextUrl.search;
+  const queryParams: string = req.nextUrl.search;
+  const parsedQuery: ParsedQuery<string> = queryString.parse(queryParams);
+  const tabActive: string = String(parsedQuery.tabActive);
   const requestHeaders = new Headers(req.headers);
   requestHeaders.set("x-pathname", nextUrlPathname);
 
@@ -22,11 +29,42 @@ export async function middleware(req: NextRequest): Promise<NextResponse> {
     }
   }
 
+  if (nextUrlPathname.startsWith("/accounts")) {
+    if (!isLogin) return NextResponse.redirect(new URL("/auth/login", req.url));
+  }
+
   if (nextUrlPathname.startsWith("/profile")) {
     if (!isLogin) return NextResponse.redirect(new URL("/auth/login", req.url));
 
     if (nextUrlPathname == "/profile")
       return NextResponse.redirect(new URL(`/profile/${user?.name}`, req.url));
+
+    if (
+      nextUrlPathnameWithSearch.startsWith(
+        `/profile/${user?.name}?tabActive`,
+      ) &&
+      !tabActiveName.includes(tabActive)
+    )
+      return NextResponse.redirect(
+        new URL(`/profile/${user?.name}?tabActive=gallery`, req.url),
+      );
+  }
+
+  if (nextUrlPathname.startsWith("/profile/user")) {
+    if (tabActive) {
+      if (!tabActiveName.includes(tabActive))
+        return NextResponse.redirect(
+          new URL(`/profile/${user?.name}?tabActive=gallery`, req.url),
+        );
+
+      if (
+        nextUrlPathnameWithSearch.startsWith(`/profile/user?tabActive`) ||
+        nextUrlPathnameWithSearch.startsWith(`/profile/user?tabActive`)
+      )
+        return NextResponse.redirect(
+          new URL(`/profile/${user?.name}?tabActive=${tabActive}`, req.url),
+        );
+    }
   }
 
   return NextResponse.next({
@@ -37,5 +75,11 @@ export async function middleware(req: NextRequest): Promise<NextResponse> {
 }
 
 export const config = {
-  matcher: ["/profile/:name*", "/auth/(.*)", "/developers/(.*)"],
+  matcher: [
+    "/profile/:name*",
+    "/auth/(.*)",
+    "/developers/(.*)",
+    "/accounts/payment/history",
+    "/accounts/settings/(.*)",
+  ],
 };
