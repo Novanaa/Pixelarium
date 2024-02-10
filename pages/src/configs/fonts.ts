4c1e5f9053061c083fd061c099f94fa07bff475a
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-02-09 23:11:18 -0500

    Added next font-weight configuration

--- pages/src/configs/fonts.ts
+++ pages/src/configs/fonts.ts
@@ -4,13 +4,13 @@ import { Fira_Code, Plus_Jakarta_Sans } from "next/font/google";
 export const jakartaSans: NextFontWithVariable = Plus_Jakarta_Sans({
   subsets: ["latin"],
   variable: "--font-jakarta",
-  weight: "400",
+  weight: ["400", "500", "600", "700", "800"],
   style: ["normal"],
 });
 
 export const firaCode: NextFontWithVariable = Fira_Code({
   subsets: ["latin"],
   variable: "--font-code",
-  weight: "400",
+  weight: ["400", "500", "600", "700"],
   style: ["normal"],
 });
