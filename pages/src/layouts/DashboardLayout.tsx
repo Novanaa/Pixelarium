ccbf2647135773e3a2aad7efe56817e9a95691fc
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-02-13 04:13:46 -0500

    Added Navbar and Footer Component to Dashboard Layout

--- pages/src/layouts/DashboardLayout.tsx
+++ pages/src/layouts/DashboardLayout.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import DashboardSidebar from "@/components/modules/DashboardSidebar";
 import DashboardSidebarMobile from "@/components/modules/DashboardSidebar/Mobile";
+import Footer from "@/components/modules/Footer";
+import Navbar from "@/components/modules/Navbar";
 
 interface DashboardLayoutProps extends React.ComponentProps<"main"> {}
 
@@ -8,12 +10,18 @@ export default function DashboardLayout({
   children,
 }: DashboardLayoutProps): React.ReactElement {
   return (
-    <main className="flex flex-col items-start sm:flex-row">
-      <DashboardSidebar />
-      <DashboardSidebarMobile />
-      <section className="container relative h-full sm:px-0">
-        {children}
+    <main>
+      <div className="block sm:hidden">
+        <Navbar />
+      </div>
+      <section className="flex flex-col items-start sm:flex-row">
+        <DashboardSidebar />
+        <DashboardSidebarMobile />
+        <div className="container relative h-full sm:px-0">{children}</div>
       </section>
+      <div className="hidden sm:block">
+        <Footer />
+      </div>
     </main>
   );
 }
