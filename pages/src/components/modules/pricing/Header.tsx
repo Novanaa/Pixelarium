c3501ba04c4ed3cf58f461572ed9dbc5a1a14288
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-02-13 22:54:16 -0500

    Added Pricing Header Component

---
+++ pages/src/components/modules/pricing/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { HeadingTwo } from "@/components/ui/Typographies/Heading";
+
+export default function PricingHeader(): React.ReactElement {
+  return (
+    <HeadingTwo className="text-center text-3xl font-bold @2xl:text-4xl">
+      Find the Perfect Fit for Your Needs.
+    </HeadingTwo>
+  );
+}
