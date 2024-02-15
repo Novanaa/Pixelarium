dabd4e079a45a620e78cef4f3217797d6e8370b9
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-02-15 10:16:22 -0500

    Added User Galleries Pages

---
+++ pages/src/app/galleries/page.tsx
@@ -0,0 +1,26 @@
+import DashboardLayout from "@/layouts/DashboardLayout";
+import { Metadata } from "next";
+import React from "react";
+import getServerSideUserData from "@/services/getServerSideUserData";
+import DecodedUser from "@/interfaces/types/DecodedUser";
+import capitalize from "capitalize";
+import UserGallery from "@/components/modules/galleries";
+
+export async function generateMetadata(): Promise<Metadata> {
+  const user: Awaited<DecodedUser | null> = await getServerSideUserData();
+  const username: string = capitalize(String(user?.name));
+  const title: string = `${username}'s Galleries`;
+
+  return {
+    title,
+    description: "Pixelarium Galleries Pages",
+  };
+}
+
+export default function Galleries(): React.ReactElement {
+  return (
+    <DashboardLayout>
+      <UserGallery />
+    </DashboardLayout>
+  );
+}
