41665bac55e7777bce14f3ec107c77c617e480b7
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-02-15 10:11:39 -0500

    Added padding-bottom to main section

--- pages/src/components/modules/PaymentHistory/index.tsx
+++ pages/src/components/modules/PaymentHistory/index.tsx
@@ -6,7 +6,7 @@ import PaymentHistoryDataTable from "./PaymentHistoryDataTable";
 
 export default function PaymentHistory(): React.ReactElement {
   return (
-    <main className="flex h-full w-full flex-col gap-4 pt-[6rem] @container sm:pl-8 sm:pt-12">
+    <main className="flex h-full w-full flex-col gap-4 pb-24 pt-[6rem] @container sm:pl-8 sm:pt-12">
       <PaymentHistoryHeader />
       <PaymentHistoryCards />
       <PaymentHistoryDataTable />
