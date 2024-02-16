e62c4ee699e7262c854bb197680ca811c840cf15
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-02-16 04:37:43 -0500

    Added Search Gallery Component

--- pages/src/components/modules/galleries/index.tsx
+++ pages/src/components/modules/galleries/index.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import GalleryHeader from "./Header";
 import GalleryPictures from "./PictureList";
+import Search from "../search";
 
 export default function UserGallery(): React.ReactElement {
   return (
-    <main className="flex h-full flex-col gap-4 pb-24 pt-[6rem] @container sm:px-10 sm:pt-12">
-      <GalleryHeader />
-      <GalleryPictures />
-    </main>
+    <>
+      <main className="flex h-full flex-col gap-4 pb-24 pt-[6rem] @container sm:px-10 sm:pt-12">
+        <GalleryHeader />
+        <GalleryPictures />
+      </main>
+      <Search />
+    </>
   );
 }
