4344ec7f720cf4f8a9e7e333e4c016f8a5382cf5
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-02-16 04:38:29 -0500

    Added ui search component (Uncompleted)

---
+++ pages/src/components/modules/search/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DesktopSearch from "./Desktop";
+
+export default function Search(): React.ReactElement {
+  return (
+    <main>
+      <section>
+        <DesktopSearch />
+      </section>
+    </main>
+  );
+}
