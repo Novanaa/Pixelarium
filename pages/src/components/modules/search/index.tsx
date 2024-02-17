39ce69354a522af2037aa4de48ca28837ddc08d6
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-02-17 10:49:19 -0500

    Added desktop search component

--- pages/src/components/modules/search/index.tsx
+++ pages/src/components/modules/search/index.tsx
@@ -1,11 +1,26 @@
-import React from "react";
-import DesktopSearch from "./Desktop";
+import React, { Suspense } from "react";
+import DesktopSearch, { DesktopSearchLoading } from "./Desktop";
+import SearchProps from "@/components/interfaces/types/SearchProps";
 
-export default function Search(): React.ReactElement {
+export default function Search({
+  datas,
+  heading,
+  emptyItemsMessege,
+  placeholder,
+}: SearchProps): React.ReactElement {
+  emptyItemsMessege = !datas.length ? emptyItemsMessege : "No results found.";
+  placeholder = placeholder || "Type a command or search...";
   return (
     <main>
-      <section>
-        <DesktopSearch />
+      <section className="hidden sm:block">
+        <Suspense fallback={<DesktopSearchLoading />}>
+          <DesktopSearch
+            datas={datas}
+            heading={heading}
+            emptyItemsMessege={emptyItemsMessege}
+            placeholder={placeholder}
+          />
+        </Suspense>
       </section>
     </main>
   );
