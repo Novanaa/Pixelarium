77eb213b504a78411419bbe3d12044ee0080c5d1
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-02-17 21:17:54 -0500

    Added useTogglePageScrollOverflow custom hooks

---
+++ pages/src/hooks/useTogglePageScrollOverflow.ts
@@ -0,0 +1,12 @@
+import { useEffect } from "react";
+
+export default function useTogglePageScrollOverflow(isOpen: boolean): void {
+  // @ts-expect-error miss types
+  useEffect(() => {
+    isOpen
+      ? (document.body.style.overflow = "hidden")
+      : (document.body.style.overflow = "auto");
+
+    return () => (document.body.style.overflow = "auto");
+  }, [isOpen]);
+}
