bde6def9f0a81845a91f072ff6eb11af0fb765c6
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-02-17 21:46:24 -0500

    Added useTogglePageScrollOverflow hooks docstring

--- pages/src/hooks/useTogglePageScrollOverflow.ts
+++ pages/src/hooks/useTogglePageScrollOverflow.ts
@@ -1,5 +1,13 @@
 import { useEffect } from "react";
 
+/**
+ * Toggles the page scroll overflow based on the value of isOpen.
+ * If isOpen is true, sets the body overflow to "hidden" to prevent scrolling.
+ * If isOpen is false, sets the body overflow to "auto" to allow scrolling.
+ *
+ * @param {boolean} isOpen - Indicates whether the page scroll overflow should be toggled.
+ * @returns {void}
+ */
 export default function useTogglePageScrollOverflow(isOpen: boolean): void {
   // @ts-expect-error miss types
   useEffect(() => {
