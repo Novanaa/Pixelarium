49ce2ec112a792983540ad41b9b94047488737e6
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-02-17 23:10:30 -0500

    Added new mobile search ui component

---
+++ pages/src/components/modules/search/mobile/index.tsx
@@ -0,0 +1,86 @@
+"use client";
+
+import React, { useRef, useState } from "react";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandList,
+  CommandSeparator,
+} from "@/components/ui/command";
+import AccountsSearchGroup from "../AccountsSearchGroup";
+import {
+  ReadonlyURLSearchParams,
+  usePathname,
+  useRouter,
+  useSearchParams,
+} from "next/navigation";
+import useActiveSearchParams from "../hooks/useActiveSearchParams";
+import useTogglePageScrollOverflow from "@/hooks/useTogglePageScrollOverflow";
+import { cn } from "@/lib/utils";
+import MobileSearchProps from "@/components/interfaces/types/MobileSearchProps";
+import MobileSearchGroup from "./SearchGroup";
+import useToggleOuterPageClick from "@/hooks/useToggleOuterPageClick";
+import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
+import { Cross2Icon } from "@radix-ui/react-icons";
+import { Button } from "@/components/ui/button";
+
+export default function MobileSearch({
+  emptyItemsMessege,
+  datas,
+  heading,
+  placeholder,
+}: MobileSearchProps): React.ReactElement {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const search: ReadonlyURLSearchParams = useSearchParams();
+  const router: AppRouterInstance = useRouter();
+  const pathname: string = usePathname();
+  // @ts-expect-error error types
+  const searchRef: React.MutableRefObject<HTMLDivElement> = useRef();
+  const searchOpenAndCloseAnimation: string = isOpen
+    ? "animate-in fade-in-0 zoom-in-95 slide-out-to-left-1/2 slide-out-to-top-[48%]"
+    : "animate-out fade-out-0 zoom-out-95 slide-out-to-left-1/2 slide-out-to-top-[48%]";
+  const searchStateValidation: string = isOpen ? `` : `hidden`;
+
+  useActiveSearchParams({ isOpen, setIsOpen, searchParams: search });
+
+  useTogglePageScrollOverflow(isOpen);
+
+  useToggleOuterPageClick({ ref: searchRef, pagePathname: pathname, router });
+
+  return (
+    <div
+      className={cn(
+        "fixed left-0 top-0 z-30 flex h-[100dvh] w-full items-center justify-center",
+        searchStateValidation,
+      )}
+    >
+      <Command
+        className={cn(
+          "h-[100dvh] w-[100vw] rounded-lg border shadow-md",
+          searchOpenAndCloseAnimation,
+        )}
+        ref={searchRef}
+      >
+        <CommandInput placeholder={placeholder} />
+        <CommandList>
+          <CommandEmpty>{emptyItemsMessege}</CommandEmpty>
+          <CommandGroup heading={heading}>
+            <MobileSearchGroup datas={datas} />
+          </CommandGroup>
+          <CommandSeparator />
+          <AccountsSearchGroup />
+        </CommandList>
+        <Button
+          variant="ghost"
+          size="icon"
+          onClick={() => router.push(pathname)}
+          className="absolute right-3 top-2 h-7 w-7 opacity-50"
+        >
+          <Cross2Icon className="h-4 w-4" />
+        </Button>
+      </Command>
+    </div>
+  );
+}
