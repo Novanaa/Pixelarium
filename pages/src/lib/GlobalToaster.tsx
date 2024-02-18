8bdfb73b13f5e4f48098ae825dd715fab12baf5d
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-02-18 09:45:51 -0500

    Added global toaster component

---
+++ pages/src/lib/GlobalToaster.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import React from "react";
+import { Toaster } from "@/components/ui/toaster";
+
+export default function GlobalToaster(): React.ReactElement {
+  return <Toaster />;
+}
