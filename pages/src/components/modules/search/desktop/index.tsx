ade82c1732ceb1d4cc534a8b10f1ee7050e5b185
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-02-22 05:18:22 -0500

    Fixed outer page component click bug

--- pages/src/components/modules/search/desktop/index.tsx
+++ pages/src/components/modules/search/desktop/index.tsx
@@ -10,16 +10,15 @@ import {
   CommandSeparator,
 } from "@/components/ui/command";
 import { cn } from "@/lib/utils";
-import { useRef } from "react";
 import { useRouter, usePathname } from "next/navigation";
 import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
 import DesktopSearchProps from "@/components/interfaces/types/DesktopSearchProps";
 import useTogglePageScrollOverflow from "@/hooks/useTogglePageScrollOverflow";
 import AccountsSearchGroup from "../AccountsSearchGroup";
-import useToggleOuterPageClick from "@/hooks/useToggleOuterPageClick";
 import useKeyboardToggle from "@/hooks/useKeyboardToggle";
 import useActiveSearchParams from "@/hooks/useActiveSearchParams";
 import DesktopSearchGroup from "./SearchGroup";
+import OutsideClickHandler from "react-outside-click-handler";
 
 export default function DesktopSearch({
   datas,
@@ -30,51 +29,58 @@ export default function DesktopSearch({
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const router: AppRouterInstance = useRouter();
   const pathname: string = usePathname();
-  // @ts-expect-error error types
-  const searchRef: React.MutableRefObject<HTMLDivElement> = useRef();
   const searchOpenAndCloseAnimation: string = isOpen
     ? "animate-in fade-in-0 zoom-in-95 slide-out-to-left-1/2 slide-out-to-top-[48%]"
     : "animate-out fade-out-0 zoom-out-95 slide-out-to-left-1/2 slide-out-to-top-[48%]";
-  const searchStateValidation: string = isOpen ? `` : `hidden`;
+  const searchStateValidation: string = isOpen
+    ? ``
+    : `hidden opacity-0 w-0 h-0 overflow-hidden`;
 
   useActiveSearchParams({ isOpen, setIsOpen, expectedValue: "search" });
 
   useKeyboardToggle({ pagePathname: pathname, router });
 
-  useToggleOuterPageClick({ ref: searchRef, router });
-
   useTogglePageScrollOverflow(isOpen);
 
   return (
-    <div
-      className={cn(
-        "fixed left-0 top-0 z-30 flex h-[100dvh] w-full items-center justify-center bg-black/50 @container",
-        searchStateValidation,
+    <>
+      {isOpen && (
+        <div
+          className={cn(
+            "fixed left-0 top-0 z-30 flex h-[100dvh] w-full items-center justify-center bg-black/50 @container",
+            searchStateValidation,
+          )}
+        >
+          <OutsideClickHandler
+            onOutsideClick={() => {
+              router.push(pathname);
+            }}
+          >
+            <Command
+              className={cn(
+                "h-[20rem] w-[32rem] rounded-lg border shadow-md",
+                searchOpenAndCloseAnimation,
+              )}
+            >
+              <CommandInput placeholder={placeholder} />
+              <CommandList>
+                <CommandEmpty>No results found.</CommandEmpty>
+                <CommandGroup heading={heading}>
+                  {!datas.length ? (
+                    <div className="mb-4 mt-2 text-center opacity-80">
+                      {emptyItemsMessege}
+                    </div>
+                  ) : (
+                    <DesktopSearchGroup datas={datas} />
+                  )}
+                </CommandGroup>
+                <CommandSeparator />
+                <AccountsSearchGroup />
+              </CommandList>
+            </Command>
+          </OutsideClickHandler>
+        </div>
       )}
-    >
-      <Command
-        className={cn(
-          "h-[20rem] w-[32rem] rounded-lg border shadow-md",
-          searchOpenAndCloseAnimation,
-        )}
-        ref={searchRef}
-      >
-        <CommandInput placeholder={placeholder} />
-        <CommandList>
-          <CommandEmpty>No results found.</CommandEmpty>
-          <CommandGroup heading={heading}>
-            {!datas.length ? (
-              <div className="mb-4 mt-2 text-center opacity-80">
-                {emptyItemsMessege}
-              </div>
-            ) : (
-              <DesktopSearchGroup datas={datas} />
-            )}
-          </CommandGroup>
-          <CommandSeparator />
-          <AccountsSearchGroup />
-        </CommandList>
-      </Command>
-    </div>
+    </>
   );
 }
