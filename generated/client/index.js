a5c2920915547d6a5bb487b4b1a99fe9a71aa2e7
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-02-21 22:00:18 -0500

    Generated Prisma Client

--- generated/client/index.js
+++ generated/client/index.js
@@ -312,7 +312,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../.env"
   },
   "relativePath": "../../prisma",
