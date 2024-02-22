922a43db401746e51d09a926567612abc09774fe
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-02-22 09:04:35 -0500

    Migrated dashboard sidebar components

---
+++ pages/src/components/modules/dashboard-sidebar/Mobile.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { cn } from "@/lib/utils";
+import DashboardSidebarMobileMenuLinksList from "./components/DashboardSidebarMobileMenuLinksList";
+
+export default function DashboardSidebarMobile(): React.ReactElement {
+  const bluryBackground: string =
+    "bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 bg-[var(--border)]";
+
+  return (
+    <section
+      className={cn(
+        "fixed bottom-0 z-30 w-full border py-[0.7rem] @container sm:hidden",
+        bluryBackground,
+      )}
+    >
+      <div className="flex flex-col items-center justify-center">
+        <DashboardSidebarMobileMenuLinksList />
+      </div>
+    </section>
+  );
+}
