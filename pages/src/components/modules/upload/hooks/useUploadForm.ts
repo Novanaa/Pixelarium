835c3646f2b5a63b33d3c4cef0bafa4fc823a44c
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-02-23 10:50:41 -0500

    Added use uplaod form hooks

---
+++ pages/src/components/modules/upload/hooks/useUploadForm.ts
@@ -0,0 +1,10 @@
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import PictureFieldFormSchema from "@/validations/pictureField";
+import { z } from "zod";
+
+export default function useUploadForm() {
+  return useForm<z.infer<typeof PictureFieldFormSchema>>({
+    resolver: zodResolver(PictureFieldFormSchema),
+  });
+}
