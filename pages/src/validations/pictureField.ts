b25a871a410321e0dd661dd670553de7fa92b1ab
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-02-23 10:49:41 -0500

    Added invalid type error msessege for expires_in and is_private field

--- pages/src/validations/pictureField.ts
+++ pages/src/validations/pictureField.ts
@@ -2,18 +2,20 @@ import invalidTypeErrorMessege from "@/constant/readonly/invalidTypeErrorMessege
 import { z } from "zod";
 
 const PictureFieldFormSchema = z.object({
-  title: z
+  link: z
     .string({
-      required_error: "Please a insert picture title.",
+      required_error: "Please a insert picture link.",
       invalid_type_error: invalidTypeErrorMessege,
     })
     .url({ message: "Link must be an URL!" }),
-  expires_in: z.number({
-    required_error: "test",
-    invalid_type_error: invalidTypeErrorMessege,
-  }),
+  expires_in: z
+    .number({
+      required_error: "Choose your expires picture",
+      invalid_type_error: invalidTypeErrorMessege,
+    })
+    .nullable(),
   is_private: z.boolean({
-    required_error: "test",
+    required_error: "Choose your picture status",
     invalid_type_error: invalidTypeErrorMessege,
   }),
 });
