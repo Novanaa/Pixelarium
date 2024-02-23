bc4d2a4c5984ff914026c66486df9326c03a700a
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-02-23 10:52:07 -0500

    Added constant readonly upload by link content form select picture status value

---
+++ pages/src/components/modules/upload/constant/readonly/linkContentSelectStatusValue.ts
@@ -0,0 +1,14 @@
+import FormSelectFieldItemValue from "@/components/molecules/interfaces/types/FormSelectFieldItemValue";
+
+const linkContentSelectStatusValue: Array<FormSelectFieldItemValue> = [
+    {
+      title: "Public",
+      value: false,
+    },
+    {
+      title: "Private",
+      value: true,
+    },
+  ];
+
+export default linkContentSelectStatusValue
