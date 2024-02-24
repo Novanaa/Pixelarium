dbeb67f34ce740b0122e37fdb71b76a0fa1eb4b0
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-02-24 07:54:46 -0500

    Added constant readonly unexpected error toast messege

---
+++ pages/src/constant/readonly/unexpectedErrorToasterMessege.ts
@@ -0,0 +1,8 @@
+import { Toast } from "@/components/ui/use-toast";
+
+const unexpectedErrorToasterMessege: Toast = {
+  title: "Uh oh! Something went wrong.",
+  description: "There was a problem with your request.",
+};
+
+export default unexpectedErrorToasterMessege;
