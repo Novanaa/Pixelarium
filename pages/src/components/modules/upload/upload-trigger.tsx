4335b53002a1e8802d82eef9aeef391cf5a2d869
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-02-23 23:21:13 -0500

    Added active tabs state on click event

--- pages/src/components/modules/upload/upload-trigger.tsx
+++ pages/src/components/modules/upload/upload-trigger.tsx
@@ -1,13 +1,29 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import { TabsList, TabsTrigger } from "@/components/ui/tabs";
+import activeUploadTabs from "./constant/readonly/activeUploadTabs";
+import { useQueryState, parseAsStringLiteral } from "nuqs";
 
 export default function UploadTrigger(): React.ReactElement {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [activeTabs, setActiveTabs] = useQueryState(
+    "upload_by",
+    parseAsStringLiteral(activeUploadTabs).withDefault("picture"),
+  );
+
+  useEffect(() => {
+    setActiveTabs(activeTabs);
+  });
+
   return (
     <TabsList className="grid w-full grid-cols-2">
-      <TabsTrigger value="picture">Picture</TabsTrigger>
-      <TabsTrigger value="link">Link</TabsTrigger>
+      <TabsTrigger value="picture" onClick={() => setActiveTabs("picture")}>
+        Picture
+      </TabsTrigger>
+      <TabsTrigger value="link" onClick={() => setActiveTabs("link")}>
+        Link
+      </TabsTrigger>
     </TabsList>
   );
 }
