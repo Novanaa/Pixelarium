610fbc6934cecb4e1dfd977d9dfdc3b8400b948f
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-02-26 08:24:42 -0500

    Changed mobile search group data map structure

--- pages/src/components/modules/search/mobile/SearchGroup.tsx
+++ pages/src/components/modules/search/mobile/SearchGroup.tsx
@@ -1,17 +1,21 @@
 import SearchGroupProps from "@/components/interfaces/types/SearchGroupProps";
-import { CommandItem } from "@/components/ui/command";
+import { CommandGroup, CommandItem } from "@/components/ui/command";
 import Link from "next/link";
 import React from "react";
 
 export default function MobileSearchGroup({
   datas,
 }: SearchGroupProps): Array<React.ReactElement> {
-  return datas?.map((d, i) => (
-    <Link href={d.link} key={i}>
-      <CommandItem>
-        {d.Icon}
-        <span>{d.title}</span>
-      </CommandItem>
-    </Link>
+  return datas?.map((data, i) => (
+    <CommandGroup heading={data.heading} key={i}>
+      {data.search.map((d, i) => (
+        <Link href={d.link} key={i}>
+          <CommandItem>
+            {d.Icon}
+            <span>{d.title}</span>
+          </CommandItem>
+        </Link>
+      ))}
+    </CommandGroup>
   ));
 }
