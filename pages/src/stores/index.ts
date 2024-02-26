256226e4fa7249e7294f440e2251c68a5e45654f
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-02-26 10:35:52 -0500

    Removed docs sidebar store reducer

--- pages/src/stores/index.ts
+++ pages/src/stores/index.ts
@@ -1,12 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./reducers/user";
-import docsSidebarReducer from "./reducers/docsSidebar";
 import pictureUpload from "./reducers/pictureUpload";
 
 const store = configureStore({
   reducer: {
     user: userReducer,
-    docsSidebar: docsSidebarReducer,
     pictureUpload: pictureUpload,
   },
 });
