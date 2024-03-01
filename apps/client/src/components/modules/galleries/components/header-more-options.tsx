82da3a2d0344cb257d26eb1dc2f0b18801031ef8
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-03-01 08:04:30 -0500

    Fixed Uncorrect Deletes All Pictures Gallery Dialog Messege

--- apps/client/src/components/modules/galleries/components/header-more-options.tsx
+++ apps/client/src/components/modules/galleries/components/header-more-options.tsx
@@ -105,8 +105,8 @@ function DeletePictures({
         <AlertDialogHeader>
           <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
           <AlertDialogDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
+            This action cannot be undone. This will permanently deletes your
+            pictures and remove your pictures from our servers.
           </AlertDialogDescription>
         </AlertDialogHeader>
         <AlertDialogFooter>
