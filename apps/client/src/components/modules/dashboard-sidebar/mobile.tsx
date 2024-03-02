d07c91cf639ca3867791b1093e6f4b06a11733e4
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-03-01 22:10:10 -0500

    Change Background Color for Dashboard Mobile Sidebar and Dashboard Navbar

--- apps/client/src/components/modules/dashboard-sidebar/mobile.tsx
+++ apps/client/src/components/modules/dashboard-sidebar/mobile.tsx
@@ -3,14 +3,10 @@ import { cn } from "@/lib/utils";
 import DashboardSidebarMobileMenuLinksList from "./components/mobile/menu-links-list";
 
 export default function DashboardSidebarMobile(): React.ReactElement {
-  const bluryBackground: string =
-    "bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 bg-[var(--border)]";
-
   return (
     <section
       className={cn(
-        "fixed bottom-0 z-30 w-full border py-[0.7rem] @container sm:hidden",
-        bluryBackground,
+        "fixed bottom-0 z-30 w-full border bg-background py-[0.7rem] @container sm:hidden",
       )}
     >
       <div className="flex flex-col items-center justify-center">
