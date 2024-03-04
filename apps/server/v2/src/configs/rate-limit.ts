1d91fef5827d5b176a2a27d7b8f1d10e860f2683
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-03-04 03:24:51 -0500

    Changed error response builder http status code

--- apps/server/v2/src/configs/rate-limit.ts
+++ apps/server/v2/src/configs/rate-limit.ts
@@ -1,5 +1,6 @@
 import { RateLimitPluginOptions } from "@fastify/rate-limit";
 import maxWindowMs from "../constant/max-window-ms";
+import http from "../constant/code";
 
 /* This code snippet is defining a rate limiting configuration object named `rateLimitConfig`. It is of
 type `RateLimitOptions` which is imported from `@fastify/rate-limit` module. */
@@ -7,7 +8,7 @@ const rateLimitConfig: RateLimitPluginOptions = {
   max: maxWindowMs,
   keyGenerator: (req) => req.ip,
   errorResponseBuilder: (_, ctx) => ({
-    statusCode: 429,
+    statusCode: http.StatusTooManyRequests,
     TypeError: "Too Many Requests",
     message: `I only allow ${ctx.max} requests per ${ctx.after} to this Website. Try again soon.`,
     date: Date.now(),
