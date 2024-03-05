5160e8183c7717e47ce73d881610af6220d38bed
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-03-04 23:56:46 -0500

    Added constant default error messege var

---
+++ apps/server/v2/src/constant/default-error-messege.ts
@@ -0,0 +1,11 @@
+export default {
+  notFound: "Oops! The data you are looking for could not be found.",
+  badRequest:
+    "Oops! Your request cannot be processed due to a bad request. Please check your input and try again.",
+  unprocessable:
+    "The content you provided could not be processed due to errors in the data. Please review your input and make sure it meets the required format and criteria.",
+  unauth:
+    "You are not authorized to access this resource. Please provide valid credentials or authentication.",
+  internalServerError:
+    "Oops! We're sorry, but something went wrong. Our server encountered an internal error while processing your request.",
+};
