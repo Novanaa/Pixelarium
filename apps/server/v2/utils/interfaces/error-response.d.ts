fbc59d30c8356721195155aab8bdfae101bd2c5b
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-03-04 23:57:50 -0500

    Added error response utils interface

---
+++ apps/server/v2/utils/interfaces/error-response.d.ts
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable semi */
+
+export interface ErrorResponse {
+  TypeError: string;
+  messege: string;
+  code: number;
+  date: string | Date;
+}
+
+export default interface ErrorResponses {
+  badRequest: (messege?: string) => ErrorResponse;
+  notFound: (messege?: string) => ErrorResponse;
+  unauthorized: (messege?: string) => ErrorResponse;
+  unprocessableContent: (messege?: string) => ErrorResponse;
+  internalServerError: (messege?: string) => ErrorResponse;
+}
