92666d4bdda510bf351593e9b3555edad7e1b493
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-03-04 23:59:38 -0500

    Added user management routes

---
+++ apps/server/v2/src/app/user/routes/index.ts
@@ -0,0 +1,11 @@
+import { FastifyInstance } from "fastify";
+import user from "@/app/user/controllers";
+
+/**
+ * This TypeScript function exports an asynchronous function that defines a route to retrieve a user by
+ * name in a Fastify application.
+ * @param {FastifyInstance} app - FastifyInstance
+ */
+export default async function userRoutes(app: FastifyInstance) {
+  app.get("/:name", user.retrieveUser);
+}
