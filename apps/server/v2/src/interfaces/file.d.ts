28f233890f75d7f71bec08490a7407d40f5e1095
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-03-06 11:54:18 -0500

    Added file data interface

---
+++ apps/server/v2/src/interfaces/file.d.ts
@@ -0,0 +1,14 @@
+/* eslint-disable semi */
+/* eslint-disable no-unused-vars */
+
+export default interface File {
+  name: string;
+  data: Buffer;
+  size: number;
+  encoding: string;
+  tempFilePath: string;
+  truncated: boolean;
+  mimetype: string;
+  md5: string;
+  mv: (path: string) => void;
+}
