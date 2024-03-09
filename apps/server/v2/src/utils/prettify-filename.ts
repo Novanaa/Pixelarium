2bac1077a9b8f5048024580e11f9c9c62e28b648
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-03-09 22:46:18 +0700

    Slugified file basename

--- apps/server/v2/src/utils/prettify-filename.ts
+++ apps/server/v2/src/utils/prettify-filename.ts
@@ -13,7 +13,7 @@ import slugify from "slugify";
  */
 export default function prettifyFilename(filename: string): string {
   const now: number = new Date().getTime();
-  const basename: string = filename.toLowerCase().split(".")[0];
+  const basename: string = slugify(filename.toLowerCase().split(".")[0]);
   const date: string = slugify(moment(now).format("LL"), { lower: true });
   const time: string = slugify(moment(now).format("LTS").replace(/:/g, "-"));
 
