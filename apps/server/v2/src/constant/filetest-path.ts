7bccb3f783ab55ddf44a625e70de733c514b86fa
Nvaa <130564362+ItsNvaa@users.noreply.github.com>
2024-03-09 13:38:17 +0700

    [PIX-128] Add Update User Avatar Validation Services Unit-Test  (#373)
    * Added mime npm package
    
    * Added public test picture file
    
    * Added dummy file for unit-test
    
    * Added update user avatar validation unit-test (Uncompleted)
    
    * Fixed eslint error
    
    * Added public test file
    
    * Added json file test
    
    * Added constant filetest path var
    
    * Added update user avatar validation unit-test

---
+++ apps/server/v2/src/constant/filetest-path.ts
@@ -0,0 +1,14 @@
+const base: string = "./public/test/";
+
+/* This block of code is exporting an object with multiple key-value pairs. Each key represents a
+specific image format or file type, and the corresponding value is the path to the file within the
+`base` directory. */
+export default {
+  "85mb": base + "85mb.png",
+  "20mb": base + "20mb.png",
+  "40mb": base + "40mb",
+  avif: base + "test.avif",
+  jpeg: base + "test.jpg",
+  sgi: base + "test.sgi",
+  json: base + "test.json",
+};
