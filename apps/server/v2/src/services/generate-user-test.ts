6121ce3eeca28aeaaf562ea49280d7aa56843bad
Nvaa <130564362+ItsNvaa@users.noreply.github.com>
2024-03-09 22:03:38 +0700

    [PIX-131] Fixed Foreign Key Constraint Failed Error While Generating Test User (#383)
    * Added falso npm package
    
    * Deleted user test resources
    
    * Fixed Foreign Key Constraint Failed Error While Generating Test User

--- apps/server/v2/src/services/generate-user-test.ts
+++ apps/server/v2/src/services/generate-user-test.ts
@@ -1,57 +1,46 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 import prisma from "@/libs/prisma";
-import users from "../../resources/user.json";
-import subscription from "../../resources/subscription.json";
 import logger from "@/libs/logger";
 import { User } from "prisma/generated/client";
 import env from "@/configs/env";
-import getUser from "@/app/user/services/get-user";
-import galleries from "../../resources/gallery.json";
-import clientKeys from "../../resources/client-key.json";
-import paymentsHistories from "../../resources/payment-history.json";
-import favorites from "../../resources/favorites.json";
-import albums from "../../resources/album.json";
-import pictures from "../../resources/picture.json";
-import embedLinks from "../../resources/embed-link.json";
+import slugify from "slugify";
+import {
+  randBoolean,
+  randEmail,
+  randFullName,
+  randPassword,
+  randSentence,
+  randUrl,
+} from "@ngneat/falso";
 
 /**
  * The function generates test user data using Prisma and logs the result.
  */
 export default async function generateTestUser(): Promise<void> {
   try {
-    const user: Awaited<User | null> = await getUser(users[0].name);
+    const user: Awaited<User | null> = await prisma.user.findUnique({
+      where: { origin_code: 1 },
+    });
 
     if (env.nodeEnv == "development" && !user) {
-      await Promise.all([
-        prisma.user.createMany({
-          data: users as Array<any>,
-        }),
-        prisma.subscription.createMany({
-          data: subscription as Array<any>,
-        }),
-        prisma.gallery.createMany({
-          data: galleries as Array<any>,
-        }),
-        prisma.clientKey.createMany({
-          data: clientKeys as Array<any>,
-        }),
-        prisma.paymentsHistory.createMany({
-          data: paymentsHistories as Array<any>,
-        }),
-        prisma.favorite.createMany({
-          data: favorites as Array<any>,
-        }),
-        prisma.album.createMany({
-          data: albums as Array<any>,
-        }),
-        prisma.picture.createMany({
-          data: pictures as Array<any>,
-        }),
-        prisma.embedLinks.createMany({
-          data: embedLinks as Array<any>,
-        }),
-      ]);
+      let i: number = 1;
+      while (i < 6) {
+        await prisma.user.create({
+          data: {
+            origin_code: i,
+            name: slugify(randFullName(), { lower: true }),
+            email: randEmail(),
+            avatar: randUrl(),
+            type: "Admin",
+            bio: randSentence(),
+            password: randPassword(),
+            is_member: randBoolean(),
+          },
+        });
+
+        i++;
+      }
 
       await prisma.$disconnect();
       return logger.info("Assessment data has been successfully generated.");
