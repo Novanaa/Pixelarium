d8af3246d4d07a4d266ac21dcd92800fbfddaf70
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-03-13 19:45:37 +0700

    Added decoded user session interface

--- apps/server/v2/src/interfaces/user.d.ts
+++ apps/server/v2/src/interfaces/user.d.ts
@@ -1,8 +1,19 @@
 /* eslint-disable semi */
 
-import { User } from "prisma/generated/client";
+import { $Enums, User } from "prisma/generated/client";
 
 export default interface UserWithOptionalChaining extends User {
   email?: string | null;
   password?: string | null;
+}
+
+export interface DecodedUser {
+  id: string;
+  name: string;
+  email: string | null;
+  origin_code: number;
+  avatar: string;
+  type: $Enums.UserType;
+  bio: string;
+  is_member: boolean;
 }
