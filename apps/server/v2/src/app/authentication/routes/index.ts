b921ca1d4179a3b8539fa858cebfa74f1b5db39c
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-03-16 10:15:53 -0400

    Registered user session tokenizer api endpoint

--- apps/server/v2/src/app/authentication/routes/index.ts
+++ apps/server/v2/src/app/authentication/routes/index.ts
@@ -3,4 +3,5 @@ import auth from "../controllers";
 
 export default async function authenticationRoutes(app: FastifyInstance) {
   app.post("/logout", auth.logout);
+  app.get("/tokenizer", auth.tokenizer);
 }
