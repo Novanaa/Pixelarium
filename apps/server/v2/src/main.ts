6564a875052ec5fcbfb3b08d30dc342423a70182
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-03-20 05:02:58 -0400

    Configured application static assest

--- apps/server/v2/src/main.ts
+++ apps/server/v2/src/main.ts
@@ -2,10 +2,13 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { NestExpressApplication } from "@nestjs/platform-express";
 import env from "./configs/env";
+import { join } from "path";
 
 async function bootstrap() {
   const app: NestExpressApplication =
     await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(join(__dirname, "..", "public"));
 
   await app.listen(env.port);
 }
