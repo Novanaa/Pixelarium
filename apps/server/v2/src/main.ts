aa44f3b0210faa575545f071c1fa8ed1df05984d
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-03-20 05:32:52 -0400

    Configured application middlewares

--- apps/server/v2/src/main.ts
+++ apps/server/v2/src/main.ts
@@ -1,14 +1,25 @@
+import { join } from "path";
+import env from "./configs/env";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import * as compression from "compression";
 import { NestExpressApplication } from "@nestjs/platform-express";
-import env from "./configs/env";
-import { join } from "path";
+import { default as corsConfig } from "@/configs/cors";
+import * as compressionConfig from "@/configs/compression";
+import * as cookieParser from "cookie-parser";
 
 async function bootstrap() {
   const app: NestExpressApplication =
     await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.useStaticAssets(join(__dirname, "..", "public"));
+  const staticAssestDirpath: string = join(__dirname, "..", "public");
+
+  app.useStaticAssets(staticAssestDirpath);
+  app.useBodyParser("json");
+  app.enableCors(corsConfig);
+  app.use(compression(compressionConfig));
+  app.use(cookieParser(env.jsonWebToken.accessToken));
+  app.set("trust proxy", 1);
 
   await app.listen(env.port);
 }
