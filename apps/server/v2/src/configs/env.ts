d83753b3e95407d16f4c1173c22e6dce19cf5888
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-03-20 04:11:54 -0400

    Updated environment variables structure

--- apps/server/v2/src/configs/env.ts
+++ apps/server/v2/src/configs/env.ts
@@ -5,17 +5,27 @@ hostnames, and port number. By exporting this object, other parts of the codebas
 these configuration settings as needed. */
 export default {
   nodeEnv: process.env.NODE_ENV,
-  databaseUrl: process.env.DATABASE_URL,
-  directUrl: process.env.DIRECT_URL,
-  jwtAccessToken: process.env.JWT_ACCESS_TOKEN,
-  jwtRefreshToken: process.env.JWT_REFRESH_TOKEN,
-  googleClientId: process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  midtransClientKey: process.env.MIDTRANS_CLIENT_KEY,
-  midtransSecretKey: process.env.MIDTRANS_SECRET_KEY,
-  midtransApiEndpoint: process.env.MIDTRANS_API_ENDPOINT,
-  githubClientId: process.env.GITHUB_CLIENT_ID,
-  githubClientSecret: process.env.GITHUB_CLIENT_SECRET,
+  database: {
+    url: process.env.DATABASE_URL,
+    directUrl: process.env.DIRECT_URL,
+  },
+  jsonWebToken: {
+    accessToken: process.env.JWT_ACCESS_TOKEN,
+    refreshToken: process.env.JWT_REFRESH_TOKEN,
+  },
+  google: {
+    clientId: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  },
+  midtrans: {
+    clientKey: process.env.MIDTRANS_CLIENT_KEY,
+    secretKey: process.env.MIDTRANS_SECRET_KEY,
+    endpoint: process.env.MIDTRANS_API_ENDPOINT,
+  },
+  github: {
+    clientId: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+  },
   hostname: process.env.HOSTNAME,
   clientHostname: process.env.CLIENT_HOSTNAME,
   port: 8000 || process.env.PORT,
