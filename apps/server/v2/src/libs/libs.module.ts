7f6f2b339ff6ee905156c162197c3ae00c7a0b09
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-03-21 11:49:48 -0400

    Registered and configured winston module

--- apps/server/v2/src/libs/libs.module.ts
+++ apps/server/v2/src/libs/libs.module.ts
@@ -1,10 +1,18 @@
 import { Global, Module } from "@nestjs/common";
 import { PrismaService } from "./prisma.service";
 import { ConfigModule } from "@nestjs/config";
+import { WinstonModule } from "nest-winston";
+import * as winston from "winston";
 
 @Global()
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    WinstonModule.forRoot({
+      format: winston.format.cli(),
+      transports: [new winston.transports.Console()],
+    }),
+  ],
   providers: [PrismaService],
   exports: [PrismaService],
 })
