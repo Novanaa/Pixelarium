d659fea6a5ea1b9e686855565ec1977ab1bbbc1c
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-03-21 12:06:03 -0400

    Added root application metadata api endpoint

--- apps/server/v2/src/app.controller.ts
+++ apps/server/v2/src/app.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get } from "@nestjs/common";
+import { AppService } from "./app.service";
+import { WelcomeResponse } from "./model/app.model";
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  welcome(): WelcomeResponse {
+    return this.appService.welcome();
   }
 }
