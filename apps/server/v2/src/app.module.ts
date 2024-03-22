a1ce600f2cbb5d69beba25965db54ca3f6c42b46
Nvaa <130564362+ItsNvaa@users.noreply.github.com>
2024-03-22 03:34:06 -0400

    [PIX-164] Setup User Application Module (#445)
    * Added user module
    
    * Registered user module
    
    * Added user controller
    
    * Added user controller spec

--- apps/server/v2/src/app.module.ts
+++ apps/server/v2/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { LibsModule } from "./libs/libs.module";
+import { UserModule } from "./cores/user/user.module";
 
 @Module({
-  imports: [LibsModule],
+  imports: [LibsModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
