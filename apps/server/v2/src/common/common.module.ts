040d3c27d7648b7a5a884033b6896f0651f5170c
Nvaa <130564362+ItsNvaa@users.noreply.github.com>
2024-03-23 08:09:56 -0400

    [PIX-167] Test Retrieve Single User API Endpoint (#451)
    * Fixed uncorrect export/import module
    
    * Added mock data common service
    
    * Registered mock data service
    
    * Added retrieve single user controller unit-test
    
    * Added supertest dependencies
    
    * Configured module name wrapper and module directory test
    
    * Fixed unmatch application end to end test
    
    * Added a several test scripts
    
    * Added test script
    
    * Added user module integration test

--- apps/server/v2/src/common/common.module.ts
+++ apps/server/v2/src/common/common.module.ts
@@ -1,9 +1,10 @@
 import { Global, Module } from "@nestjs/common";
 import { ErrorService } from "./error.service";
+import { MockData } from "./mock.service";
 
 @Global()
 @Module({
-  providers: [ErrorService],
-  exports: [ErrorService],
+  providers: [ErrorService, MockData],
+  exports: [ErrorService, MockData],
 })
 export class CommonModule {}
--- apps/server/v2/test/app.e2e-spec.ts
+++ apps/server/v2/test/app.e2e-spec.ts
@@ -1,10 +1,12 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { Test, TestingModule } from "@nestjs/testing";
+import { HttpStatus, INestApplication } from "@nestjs/common";
+import * as request from "supertest";
+import { AppModule } from "./../src/app.module";
+import { AppService } from "@/app.service";
 
-describe('AppController (e2e)', () => {
+describe("AppController (e2e)", () => {
   let app: INestApplication;
+  let appService: AppService;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -12,13 +14,14 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    appService = moduleFixture.get<AppService>(AppService);
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it("/ (GET)", () => {
     return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+      .get("/")
+      .expect(HttpStatus.OK)
+      .expect(appService.welcome());
   });
 });
