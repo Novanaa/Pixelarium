303454be83c3e19e71af69926214e2f5ad68ada2
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-03-23 23:01:01 -0400

    Added delete user service

--- apps/server/v2/src/cores/user/services/delete-user.service.ts
+++ apps/server/v2/src/cores/user/services/delete-user.service.ts
@@ -1,18 +1,19 @@
-import { ErrorService } from "@/common/error.service";
+import * as path from "path";
 import { PrismaService } from "@/libs/prisma.service";
 import { HttpStatus, Injectable } from "@nestjs/common";
 import { DeleteUserResponseDto } from "../dtos/delete-user.dto";
 import { FileSystemService } from "@/libs/file-system.service";
 import { RetrieveUserService } from "./retrieve-user.service";
 import { RetrieveUserResponseDto } from "../dtos/retrieve-user.dto";
+import { StaticDirectorySerive } from "@/common/static-directory.service";
 
 @Injectable()
 export class DeleteUserService {
   constructor(
     private readonly prisma: PrismaService,
-    private readonly error: ErrorService,
     private readonly fileSystem: FileSystemService,
-    private readonly retrieveUserService: RetrieveUserService
+    private readonly retrieveUserService: RetrieveUserService,
+    private readonly staticDirectory: StaticDirectorySerive
   ) {}
 
   public async deleteSingleUser(name: string): Promise<void> {
@@ -22,6 +23,14 @@ export class DeleteUserService {
   public async deleteUser(name: string): Promise<DeleteUserResponseDto> {
     const { user }: Awaited<RetrieveUserResponseDto> =
       await this.retrieveUserService.retrieveSingleUser(name);
+
+    const filename: string = path.basename(user.avatar);
+    const dirpath: string =
+      this.staticDirectory.getUserAvatarDirectory(filename);
+
+    this.fileSystem.deleteFile(dirpath);
+
+    await this.deleteSingleUser(name);
 
     return {
       deleted_user: user,
