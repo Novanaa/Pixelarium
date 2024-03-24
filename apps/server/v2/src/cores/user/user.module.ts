bb7fb7d0a353b1bc3d0d156ba3cdd33deb26ddf2
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-03-23 22:16:59 -0400

    Registered delete user service

--- apps/server/v2/src/cores/user/user.module.ts
+++ apps/server/v2/src/cores/user/user.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
 import { RetrieveUserService } from "./services/retrieve-user.service";
 import { UserController } from "./user.controller";
+import { DeleteUserService } from "./services/delete-user.service";
 
 @Module({
   controllers: [UserController],
-  providers: [RetrieveUserService],
+  providers: [RetrieveUserService, DeleteUserService],
 })
 export class UserModule {}
