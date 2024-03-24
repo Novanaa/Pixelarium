b4dbfc49745204f154db41843cc65ce85459293e
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-03-24 00:04:18 -0400

    Deleted libs services

--- apps/server/v2/src/libs/file-system.service.ts
+++
@@ -1,9 +0,0 @@
-import * as fs from "fs";
-import { Injectable } from "@nestjs/common";
-
-@Injectable()
-export class FileSystemService {
-  public deleteFile(path: string): void {
-    if (fs.existsSync(path)) fs.unlinkSync(path);
-  }
-}
