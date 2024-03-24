9d31bb570a1d3fad55e7fa70bb36590a02065085
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-03-23 22:58:58 -0400

    Registered static directory common service

--- apps/server/v2/src/common/common.module.ts
+++ apps/server/v2/src/common/common.module.ts
@@ -1,10 +1,11 @@
 import { Global, Module } from "@nestjs/common";
 import { ErrorService } from "./error.service";
 import { MockData } from "./mock.service";
+import { StaticDirectorySerive } from "./static-directory.service";
 
 @Global()
 @Module({
-  providers: [ErrorService, MockData],
-  exports: [ErrorService, MockData],
+  providers: [ErrorService, MockData, StaticDirectorySerive],
+  exports: [ErrorService, MockData, StaticDirectorySerive],
 })
 export class CommonModule {}
