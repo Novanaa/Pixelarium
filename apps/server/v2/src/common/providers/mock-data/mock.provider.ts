29ca43c7e6bc5427170724e7df0cf028a34f015a
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-03-24 03:41:05 -0400

    Added generate random user and create random user method

--- apps/server/v2/src/common/providers/mock-data/mock.provider.ts
+++ apps/server/v2/src/common/providers/mock-data/mock.provider.ts
@@ -1,12 +1,38 @@
-import { PrismaProvider } from "@/libs/providers/prisma/prisma.provider";
-import { Injectable } from "@nestjs/common";
 import { User } from "@prisma/client";
+import * as falso from "@ngneat/falso";
+import { Injectable } from "@nestjs/common";
+import { PrismaProvider } from "@/libs/providers/prisma/prisma.provider";
 
 @Injectable()
 export class MockDataProvider {
   constructor(private readonly prisma: PrismaProvider) {}
 
   public async getRandomser(): Promise<User> {
-    return await this.prisma.user.findFirst();
+    try {
+      return await this.prisma.user.findFirst();
+    } finally {
+      await this.prisma.$disconnect();
+    }
+  }
+
+  public generateRandomUser(): Omit<User, "created_at" | "updated_at" | "id"> {
+    return {
+      name: falso.randUserName(),
+      email: falso.randEmail(),
+      avatar: falso.randAvatar(),
+      bio: falso.randSentence(),
+      is_member: falso.randBoolean(),
+      origin_code: falso.randNumber(),
+      type: "Admin",
+      password: falso.randPassword(),
+    };
+  }
+
+  public async createRandomUser(): Promise<User> {
+    try {
+      return await this.prisma.user.create({ data: this.generateRandomUser() });
+    } finally {
+      await this.prisma.$disconnect();
+    }
   }
 }
