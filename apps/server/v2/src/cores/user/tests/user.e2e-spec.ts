01a22f0679dcac2fafa9afbf300dbe8ef7a2fa70
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-03-25 04:14:22 -0400

    Added delete user api endpoint end-to-end test

--- apps/server/v2/src/cores/user/tests/user.e2e-spec.ts
+++ apps/server/v2/src/cores/user/tests/user.e2e-spec.ts
@@ -10,6 +10,7 @@ import { User } from "@prisma/client";
 import { MockDataProvider } from "@/common/providers/mock-data/mock.provider";
 import { RetrieveUserResponseDto } from "../providers/retrieve-user/retrieve-user.dto";
 import { PrismaProvider } from "@/libs/providers";
+import { DeleteUserResponseDto } from "../providers/delete-user/delete-user.dto";
 
 describe("User Controller (e2e)", () => {
   let app: INestApplication;
@@ -124,6 +125,115 @@ describe("User Controller (e2e)", () => {
       delete user.password;
 
       expect(JSON.stringify(body.user)).toBe(JSON.stringify(user));
+    });
+  });
+
+  describe("DELETE /user/:name", () => {
+    it("should be return not found exeption when the user doens't exist", async () => {
+      const response: Awaited<supertest.Request | supertest.Response> =
+        await supertest(app.getHttpServer()).delete("/user/0");
+      const body: ResponseError = response.body as ResponseError;
+
+      expect(body).toEqual(errorService.notFound());
+    });
+    it("should be return 404 when the user doesn't exist", async () => {
+      const response: Awaited<supertest.Request | supertest.Response> =
+        await supertest(app.getHttpServer()).delete("/user/0");
+      const body: ResponseError = response.body as ResponseError;
+
+      expect(response.status).toBe(HttpStatus.NOT_FOUND);
+      expect(body.code).toBe(HttpStatus.NOT_FOUND);
+    });
+    it("status response should be 'KO' when the user doesn't exist", async () => {
+      const response: Awaited<supertest.Request | supertest.Response> =
+        await supertest(app.getHttpServer()).delete("/user/0");
+      const body: ResponseError = response.body as ResponseError;
+
+      expect(body.status).toBe("KO");
+    });
+    it("status response should be 'KO' when the user doesn't exist", async () => {
+      const response: Awaited<supertest.Request | supertest.Response> =
+        await supertest(app.getHttpServer()).delete("/user/0");
+      const body: ResponseError = response.body as ResponseError;
+
+      expect(body.status).toBe("KO");
+    });
+    it("should be accept application/json", async () => {
+      const user: Awaited<User> = await mockDataService.createRandomUser();
+      const response: Awaited<supertest.Request | supertest.Response> =
+        await supertest(app.getHttpServer())
+          .delete("/user/" + user.name)
+          .set("Content-Type", "application/json");
+      const body: DeleteUserResponseDto =
+        response.body as DeleteUserResponseDto;
+
+      expect(body.code).toBe(HttpStatus.OK);
+    });
+    it("should be return 200 status code", async () => {
+      const user: Awaited<User> = await mockDataService.createRandomUser();
+      const response: Awaited<supertest.Request | supertest.Response> =
+        await supertest(app.getHttpServer()).delete("/user/" + user.name);
+      const body: DeleteUserResponseDto =
+        response.body as DeleteUserResponseDto;
+
+      expect(body.code).toBe(HttpStatus.OK);
+    });
+    it("status response should be 'OK'", async () => {
+      const user: Awaited<User> = await mockDataService.createRandomUser();
+      const response: Awaited<supertest.Request | supertest.Response> =
+        await supertest(app.getHttpServer()).delete("/user/" + user.name);
+      const body: DeleteUserResponseDto =
+        response.body as DeleteUserResponseDto;
+
+      expect(body.status).toBe("OK");
+    });
+    it("deleted user response data should be defined", async () => {
+      const user: Awaited<User> = await mockDataService.createRandomUser();
+      const response: Awaited<supertest.Request | supertest.Response> =
+        await supertest(app.getHttpServer()).delete("/user/" + user.name);
+      const body: DeleteUserResponseDto =
+        response.body as DeleteUserResponseDto;
+
+      expect(body.deleted_user).toBeDefined();
+    });
+    it("deleted user response data should be not included email field data", async () => {
+      const user: Awaited<User> = await mockDataService.createRandomUser();
+      const response: Awaited<supertest.Request | supertest.Response> =
+        await supertest(app.getHttpServer()).delete("/user/" + user.name);
+      const body: DeleteUserResponseDto =
+        response.body as DeleteUserResponseDto;
+
+      expect(body.deleted_user.email).toBeUndefined();
+    });
+    it("deleted user response data should be not included password field data", async () => {
+      const user: Awaited<User> = await mockDataService.createRandomUser();
+      const response: Awaited<supertest.Request | supertest.Response> =
+        await supertest(app.getHttpServer()).delete("/user/" + user.name);
+      const body: DeleteUserResponseDto =
+        response.body as DeleteUserResponseDto;
+
+      expect(body.deleted_user.password).toBeUndefined();
+    });
+    it("deleted user response data should be match to requested user data", async () => {
+      const user: Awaited<User> = await mockDataService.createRandomUser();
+      const response: Awaited<supertest.Request | supertest.Response> =
+        await supertest(app.getHttpServer()).delete("/user/" + user.name);
+      const body: DeleteUserResponseDto =
+        response.body as DeleteUserResponseDto;
+
+      delete user.email;
+      delete user.password;
+
+      expect(JSON.stringify(body.deleted_user)).toMatch(JSON.stringify(user));
+    });
+    it("requested user should be deleted", async () => {
+      const user: Awaited<User> = await mockDataService.createRandomUser();
+      await supertest(app.getHttpServer()).delete("/user/" + user.name);
+      const searchUser: Awaited<User> = await prisma.user.findUnique({
+        where: { name: user.name },
+      });
+
+      expect(searchUser).toBeNull();
     });
   });
 });
