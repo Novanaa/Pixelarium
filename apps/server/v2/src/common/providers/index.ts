794058f1dbcea0301766fb97ac0fc29f8d9bad03
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-03-27 06:17:43 -0400

    Exported prettier common provider

--- apps/server/v2/src/common/providers/index.ts
+++ apps/server/v2/src/common/providers/index.ts
@@ -2,10 +2,12 @@ import { MockDataProvider } from "./mock-data/mock.provider";
 import { StaticDirectoryProvider } from "./static-directoty/static-directory.provider";
 import { ErrorProvider } from "./error/error.provider";
 import { ValidationProvider } from "./validation/validation.provider";
+import { PrettierProvider } from "./prettier/prettier.provider";
 
 export {
   MockDataProvider,
   StaticDirectoryProvider,
   ErrorProvider,
   ValidationProvider,
+  PrettierProvider,
 };
