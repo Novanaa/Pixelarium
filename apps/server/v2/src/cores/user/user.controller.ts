50cc67bca6ec658705f97192345a723d81fee85a
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-03-27 05:51:01 -0400

    Added update user api endpoint feature

--- apps/server/v2/src/cores/user/user.controller.ts
+++ apps/server/v2/src/cores/user/user.controller.ts
@@ -1,4 +1,5 @@
 import {
+  Body,
   Controller,
   Delete,
   Get,
@@ -6,19 +7,29 @@ import {
   HttpCode,
   HttpStatus,
   Param,
+  Patch,
+  UploadedFile,
+  UseInterceptors,
 } from "@nestjs/common";
-import { PrismaProvider } from "@/libs/providers/prisma/prisma.provider";
+import {
+  UpdateUserRequestDto,
+  UpdateUserResponsetDto,
+} from "./providers/update-user/update-user.dto";
+import { PrismaProvider } from "@/libs/providers/prisma-client/prisma.provider";
 import { RetrieveUserProvider } from "./providers/retrieve-user/retrieve-user.provider";
 import { RetrieveUserResponseDto } from "./providers/retrieve-user/retrieve-user.dto";
 import { DeleteUserResponseDto } from "./providers/delete-user/delete-user.dto";
 import { DeleteUserProvider } from "./providers/delete-user/delete-user.provider";
+import { UpdateUserProvider } from "./providers/update-user/update-user.provider";
+import { FileInterceptor } from "@nestjs/platform-express";
 
 @Controller("user")
 export class UserController {
   constructor(
     private readonly prisma: PrismaProvider,
     private readonly retrieveUserService: RetrieveUserProvider,
-    private readonly deleteUserService: DeleteUserProvider
+    private readonly deleteUserService: DeleteUserProvider,
+    private readonly updateUserService: UpdateUserProvider
   ) {}
 
   @Get("/:name")
@@ -42,6 +53,22 @@ export class UserController {
   ): Promise<DeleteUserResponseDto> {
     try {
       return await this.deleteUserService.deleteUser(name);
+    } finally {
+      await this.prisma.$disconnect();
+    }
+  }
+
+  @Patch("/:name")
+  @Header("Content-Type", "application/json")
+  @UseInterceptors(FileInterceptor("avatar"))
+  @HttpCode(HttpStatus.OK)
+  public async updateUser(
+    @Param("name") name: string,
+    @Body() payload: UpdateUserRequestDto,
+    @UploadedFile() avatar: Express.Multer.File
+  ): Promise<UpdateUserResponsetDto> {
+    try {
+      return await this.updateUserService.updateUser({ name, payload, avatar });
     } finally {
       await this.prisma.$disconnect();
     }
