e3aca9290968940b17de60f894e0c5e759520285
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-03-27 21:26:37 -0400

    Fixed absolute export/import module

--- apps/server/v2/src/app.module.ts
+++ apps/server/v2/src/app.module.ts
@@ -4,7 +4,7 @@ import { AppService } from "./app.service";
 import { LibsModule } from "./libs/libs.module";
 import { UserModule } from "./cores/user/user.module";
 import { CommonModule } from "./common/common.module";
-import { TestModule } from "test/test.module";
+import { TestModule } from "../test/test.module";
 
 @Module({
   imports: [LibsModule, UserModule, CommonModule, TestModule],
