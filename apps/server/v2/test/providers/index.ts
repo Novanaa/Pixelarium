a1e4deed0aeb28b490df60ec455c5fc3ae633860
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-03-28 03:08:39 -0400

    Removed test generator provider

--- apps/server/v2/test/providers/index.ts
+++ apps/server/v2/test/providers/index.ts
@@ -1,4 +1,3 @@
-import { GeneratorProvider } from "./generator/generator.provider";
 import { LifecycleProvider } from "./lifecycle/lifecycle.provider";
 
-export { GeneratorProvider, LifecycleProvider };
+export { LifecycleProvider };
