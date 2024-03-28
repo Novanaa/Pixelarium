83f751e380cd7e9edf992cf1774cd7f179569b0b
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-03-27 23:00:15 -0400

    Exported lifecycle test provider

--- apps/server/v2/test/providers/index.ts
+++ apps/server/v2/test/providers/index.ts
@@ -1,3 +1,4 @@
 import { GeneratorProvider } from "./generator/generator.provider";
+import { LifecycleProvider } from "./lifecycle/lifecycle.provider";
 
-export { GeneratorProvider };
+export { GeneratorProvider, LifecycleProvider };
