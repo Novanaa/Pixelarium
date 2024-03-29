a9f6e1364c1c761d7d2c989d4618f9dc9ac9622f
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-03-29 07:19:25 -0400

    Added validation common provider invalid file size unit-test

--- apps/server/v2/src/common/providers/validation/validation.spec.ts
+++ apps/server/v2/src/common/providers/validation/validation.spec.ts
@@ -1,15 +1,19 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { ValidationProvider } from "./validation.provider";
+import { MockDataProvider } from "../mock-data/mock.provider";
+import { PrismaProvider } from "@/libs/providers";
 
 describe("Validation Common Provider", () => {
   let validation: ValidationProvider;
+  let mockData: MockDataProvider;
 
   beforeEach(async () => {
     const app: Awaited<TestingModule> = await Test.createTestingModule({
-      providers: [ValidationProvider],
+      providers: [ValidationProvider, MockDataProvider, PrismaProvider],
     }).compile();
 
     validation = app.get<ValidationProvider>(ValidationProvider);
+    mockData = app.get<MockDataProvider>(MockDataProvider);
   });
 
   describe("Blank Payload", () => {
@@ -18,6 +22,33 @@ describe("Validation Common Provider", () => {
     });
     it("should be return true when the sended object was empty", () => {
       expect(validation.blankPayload(new Object())).toBe(true);
+    });
+  });
+
+  describe("Invalid File Size", () => {
+    it("should be return true when the image file size is more than 1mb", () => {
+      expect(
+        validation.invalidFileSize({
+          file: mockData.dummyLargeFilePicture() as Express.Multer.File,
+          size: 1,
+        })
+      ).toBe(true);
+    });
+    it("should be return false when the image file size is less than 1mb", () => {
+      expect(
+        validation.invalidFileSize({
+          file: mockData.dummyPicture() as Express.Multer.File,
+          size: 1,
+        })
+      ).toBe(false);
+    });
+    it("should be return false when the image file is broken", () => {
+      expect(
+        validation.invalidFileSize({
+          file: mockData.dummyBrokenPicture() as Express.Multer.File,
+          size: 1,
+        })
+      ).toBe(false);
     });
   });
 });
