2abb86ad69fa08df953285d3de8e64df7f128a77
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-03-30 02:11:08 -0400

    Added login with google authentication api endpoint

--- apps/server/v2/src/cores/auth/auth.controller.ts
+++ apps/server/v2/src/cores/auth/auth.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from "@nestjs/common";
+import { GoogleProvider } from "@/libs/providers";
+import {
+  Controller,
+  Get,
+  HttpCode,
+  HttpRedirectResponse,
+  HttpStatus,
+  Redirect,
+} from "@nestjs/common";
 
 @Controller("auth")
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly google: GoogleProvider) {}
+
+  @Redirect()
+  @Get("/google")
+  @HttpCode(HttpStatus.MOVED_PERMANENTLY)
+  public loginWithGoogle(): HttpRedirectResponse {
+    return {
+      statusCode: HttpStatus.MOVED_PERMANENTLY,
+      url: this.google.getAuthenticationUrl(),
+    };
+  }
+}
