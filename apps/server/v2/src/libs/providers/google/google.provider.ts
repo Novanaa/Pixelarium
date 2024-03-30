b4ca76df7dbdc583ad25afef62f5d1c8cbd619fe
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-03-30 06:03:34 -0400

    Added get credentials token method

--- apps/server/v2/src/libs/providers/google/google.provider.ts
+++ apps/server/v2/src/libs/providers/google/google.provider.ts
@@ -1,24 +1,29 @@
 import { google, Auth } from "googleapis";
+import { GetTokenResponse } from "google-auth-library/build/src/auth/oauth2client";
 import { AuthConstant } from "@/constant/auth.constant";
 import { Injectable } from "@nestjs/common";
 import { Environment } from "@/configs/readonly";
 
 @Injectable()
 export class GoogleProvider {
-  public getclient(): Auth.OAuth2Client {
+  public getClient(): Auth.OAuth2Client {
     return new google.auth.OAuth2(
       Environment.GOOGLE.CLIENT_ID,
       Environment.GOOGLE.CLIENT_SECRET,
-      Environment.HOSTNAME + "/auth/google"
+      Environment.HOSTNAME + "/auth/google/callback"
     );
   }
 
   public getAuthenticationUrl(): string {
-    return this.getclient().generateAuthUrl({
+    return this.getClient().generateAuthUrl({
       access_type: "offline",
       scope: AuthConstant.GOOGLE_AUTH_SCOPES,
       include_granted_scopes: true,
       prompt: "consent",
     });
+  }
+
+  public async getToken(code: string): Promise<GetTokenResponse> {
+    return await this.getClient().getToken(code);
   }
 }
