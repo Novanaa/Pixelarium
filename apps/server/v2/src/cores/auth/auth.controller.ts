9a7ac5fa3dc07ddfafbba5211c2868725bcc6716
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-03-30 06:13:57 -0400

    Added new handler for google authentication callback

--- apps/server/v2/src/cores/auth/auth.controller.ts
+++ apps/server/v2/src/cores/auth/auth.controller.ts
@@ -1,24 +1,39 @@
-import { GoogleProvider } from "@/libs/providers";
 import {
   Controller,
   Get,
   HttpCode,
   HttpRedirectResponse,
   HttpStatus,
+  Query,
   Redirect,
 } from "@nestjs/common";
+import { PrismaProvider } from "@/libs/providers";
+import { GoogleAuthProvider } from "./providers/google/google.provider";
 
 @Controller("auth")
 export class AuthController {
-  constructor(private readonly google: GoogleProvider) {}
+  constructor(
+    private readonly prisma: PrismaProvider,
+    private readonly googleService: GoogleAuthProvider
+  ) {}
 
   @Redirect()
   @Get("/google")
   @HttpCode(HttpStatus.MOVED_PERMANENTLY)
   public loginWithGoogle(): HttpRedirectResponse {
-    return {
-      statusCode: HttpStatus.MOVED_PERMANENTLY,
-      url: this.google.getAuthenticationUrl(),
-    };
+    return this.googleService.loginWithGoogle();
+  }
+
+  @Redirect()
+  @Get("/google/callback")
+  @HttpCode(HttpStatus.MOVED_PERMANENTLY)
+  public async googleAuthenticationCallback(
+    @Query("code") code: string
+  ): Promise<HttpRedirectResponse> {
+    try {
+      return await this.googleService.authenticationCallback(code);
+    } finally {
+      await this.prisma.$disconnect();
+    }
   }
 }
