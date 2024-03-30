a5efe031d97b85fb81c159218adfd500ba9284bc
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-03-30 06:13:31 -0400

    Registered google auth provider

--- apps/server/v2/src/cores/auth/auth.module.ts
+++ apps/server/v2/src/cores/auth/auth.module.ts
@@ -1,7 +1,9 @@
 import { Module } from "@nestjs/common";
 import { AuthController } from "./auth.controller";
+import { GoogleAuthProvider } from "./providers/google/google.provider";
 
 @Module({
   controllers: [AuthController],
+  providers: [GoogleAuthProvider],
 })
 export class AuthModule {}
