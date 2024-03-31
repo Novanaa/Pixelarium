7083b3a57f4fe7b1f1d3dba834daf84a28c783f0
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-03-30 22:34:41 -0400

    Registered userinfo auth provider

--- apps/server/v2/src/cores/auth/auth.module.ts
+++ apps/server/v2/src/cores/auth/auth.module.ts
@@ -2,10 +2,11 @@ import { Module } from "@nestjs/common";
 import { AuthController } from "./auth.controller";
 import { GoogleAuthProvider } from "./providers/google/google.provider";
 import { UserModule } from "../user/user.module";
+import { UserInfoProvider } from "./providers/userinfo/userinfo.provider";
 
 @Module({
   imports: [UserModule],
   controllers: [AuthController],
-  providers: [GoogleAuthProvider],
+  providers: [GoogleAuthProvider, UserInfoProvider],
 })
 export class AuthModule {}
