30f3f8440cef3725d3280fe0293194414412504c
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-03-30 23:09:22 -0400

    Added user info credentials payload dto

--- apps/server/v2/src/cores/auth/providers/userinfo/userinfo.dto.ts
+++ apps/server/v2/src/cores/auth/providers/userinfo/userinfo.dto.ts
@@ -1,6 +1,15 @@
+import { UserType } from "@prisma/client";
+
 export class UserInfoCreateUserDto {
   avatar: string | null;
   name: string;
   email: string | null;
   originCode: number;
+}
+
+export class UserInfoCredentialsPayloadDto extends UserInfoCreateUserDto {
+  id: string;
+  type: UserType;
+  bio: string;
+  is_member: boolean;
 }
