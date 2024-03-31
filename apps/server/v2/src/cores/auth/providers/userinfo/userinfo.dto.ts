88b7c3aa39dc895e49266e4a1f568d1f7f066f1f
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-03-30 23:45:32 -0400

    Deleted user info credentials payload dto

--- apps/server/v2/src/cores/auth/providers/userinfo/userinfo.dto.ts
+++ apps/server/v2/src/cores/auth/providers/userinfo/userinfo.dto.ts
@@ -1,15 +1,6 @@
-import { UserType } from "@prisma/client";
-
 export class UserInfoCreateUserDto {
   avatar: string | null;
   name: string;
   email: string | null;
   originCode: number;
-}
-
-export class UserInfoCredentialsPayloadDto extends UserInfoCreateUserDto {
-  id: string;
-  type: UserType;
-  bio: string;
-  is_member: boolean;
 }
