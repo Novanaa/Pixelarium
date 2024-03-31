038261796dcab1cdc62cfc2c6321b90cd694eba7
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-03-31 06:22:59 -0400

    Fixed unresolved dependency authentication unit-test error

--- apps/server/v2/src/cores/auth/tests/auth.controller.spec.ts
+++ apps/server/v2/src/cores/auth/tests/auth.controller.spec.ts
@@ -1,12 +1,22 @@
+import {
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  UserInfoProvider,
+} from "../providers";
 import { Test, TestingModule } from "@nestjs/testing";
 import { AuthController } from "../auth.controller";
+import { LibsModule } from "@/libs/libs.module";
+import { UserModule } from "@/cores/user/user.module";
+import { CommonModule } from "@/common/common.module";
 
 describe("AuthController", () => {
   let controller: AuthController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [LibsModule, UserModule, CommonModule],
       controllers: [AuthController],
+      providers: [GithubAuthProvider, GoogleAuthProvider, UserInfoProvider],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
