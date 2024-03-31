467648714d4e5ec020a08f58c113ca89e4c76912
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-03-30 21:37:33 -0400

    Added google auth provider dto

---
+++ apps/server/v2/src/cores/auth/providers/google/google.dto.ts
@@ -0,0 +1,15 @@
+export class GoogleUserInfo {
+  iss: string;
+  azp: string;
+  aud: string;
+  sub: string;
+  email: string;
+  email_verified: boolean;
+  at_hash: string;
+  name: string;
+  picture: string;
+  given_name: string;
+  family_name: string;
+  iat: number;
+  exp: number;
+}
