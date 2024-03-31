a9292ad1e0b3b6879a98a3bfc69f4ba76089201d
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-03-31 02:04:39 -0400

    Added login with github api core logic

--- apps/server/v2/src/cores/auth/providers/github/github.provider.ts
+++ apps/server/v2/src/cores/auth/providers/github/github.provider.ts
@@ -1,4 +1,18 @@
-import { Injectable } from "@nestjs/common";
+import { HttpRedirectResponse, HttpStatus, Injectable } from "@nestjs/common";
+import { UserInfoProvider } from "../userinfo/userinfo.provider";
+import { GithubProvider } from "@/libs/providers";
 
 @Injectable()
-export class GithubAuthProvider {}
+export class GithubAuthProvider {
+  constructor(
+    private readonly userInfo: UserInfoProvider,
+    private readonly github: GithubProvider
+  ) {}
+
+  public loginWithGithub(): HttpRedirectResponse {
+    return {
+      url: this.github.getAuthenticationURL(),
+      statusCode: HttpStatus.MOVED_PERMANENTLY,
+    };
+  }
+}
