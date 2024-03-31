2501c4774a84cafff25327c62487292a030d0bf5
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-03-30 22:08:44 -0400

    Shared user module with authentication module

--- apps/server/v2/src/cores/auth/auth.module.ts
+++ apps/server/v2/src/cores/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
 import { AuthController } from "./auth.controller";
 import { GoogleAuthProvider } from "./providers/google/google.provider";
+import { UserModule } from "../user/user.module";
 
 @Module({
+  imports: [UserModule],
   controllers: [AuthController],
   providers: [GoogleAuthProvider],
 })
