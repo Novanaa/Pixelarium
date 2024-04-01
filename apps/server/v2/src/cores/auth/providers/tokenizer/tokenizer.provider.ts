3f3ad3e52f790f6de72bb4a3d89d98456ad12c05
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-04-01 11:44:23 -0400

    Added user tokenizer authentication api core logic

--- apps/server/v2/src/cores/auth/providers/tokenizer/tokenizer.provider.ts
+++ apps/server/v2/src/cores/auth/providers/tokenizer/tokenizer.provider.ts
@@ -1,6 +1,55 @@
-import { Injectable } from "@nestjs/common";
+import { verify, Jwt } from "jsonwebtoken";
+import {
+  ForbiddenException,
+  HttpStatus,
+  Injectable,
+  UnauthorizedException,
+} from "@nestjs/common";
+import { TokenizerResponseDto } from "./tokenizer.dto";
+import { ErrorProvider } from "@/common/providers";
+import { UserInfoProvider } from "../userinfo/userinfo.provider";
+import { Environment } from "@/configs/readonly";
+import { UserPayload } from "./tokenizer";
+import { UserInfoGenerateCredentials } from "../userinfo/userinfo";
 
 @Injectable()
 export class TokenizerProvider {
-  constructor() {}
+  constructor(
+    private readonly error: ErrorProvider,
+    private readonly userinfo: UserInfoProvider
+  ) {}
+
+  private verifyCredentials(session: string): Jwt {
+    try {
+      return verify(session, Environment.JSONWEBTOKEN.REFRESH_TOKEN, {
+        complete: true,
+      });
+    } catch (err) {
+      throw new ForbiddenException(this.error.forbidden());
+    }
+  }
+
+  public generate(session: string): TokenizerResponseDto {
+    if (!session) throw new UnauthorizedException(this.error.unauthorized());
+
+    const verifyCredentials: Jwt = this.verifyCredentials(session);
+    const user: UserPayload = verifyCredentials.payload as UserPayload;
+
+    delete user.iat;
+    delete user.exp;
+
+    const userCredentials: UserInfoGenerateCredentials =
+      this.userinfo.generateCredentials(user);
+    const now: number = new Date().getTime();
+    const expiresTime: number = now + 30 * 60 * 1000;
+
+    return {
+      credentials: {
+        access_token: userCredentials.accessToken,
+        expires_in: expiresTime,
+      },
+      code: HttpStatus.OK,
+      status: "OK",
+    };
+  }
 }
