c08faa9991aa254a3c2684291766a7b54281301d
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-04-01 10:55:22 -0400

    Exported tokenizer auth provider

--- apps/server/v2/src/cores/auth/providers/index.ts
+++ apps/server/v2/src/cores/auth/providers/index.ts
@@ -2,10 +2,12 @@ import { GoogleAuthProvider } from "./google/google.provider";
 import { UserInfoProvider } from "./userinfo/userinfo.provider";
 import { GithubAuthProvider } from "./github/github.provider";
 import { LogoutProvider } from "./logout/logout.provider";
+import { TokenizerProvider } from "./tokenizer/tokenizer.provider";
 
 export {
   UserInfoProvider,
   GoogleAuthProvider,
   GithubAuthProvider,
   LogoutProvider,
+  TokenizerProvider,
 };
