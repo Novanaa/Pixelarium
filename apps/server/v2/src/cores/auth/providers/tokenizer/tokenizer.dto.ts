9b66c918b0011b90c77a39fdc511a49baf5cc671
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-04-01 10:59:41 -0400

    Added tokenizer auth dto

--- apps/server/v2/src/cores/auth/providers/tokenizer/tokenizer.dto.ts
+++ apps/server/v2/src/cores/auth/providers/tokenizer/tokenizer.dto.ts
@@ -1,1 +1,10 @@
-export class TokenizerResponseDto {}
+import { WebResponse } from "@/model/app.model";
+
+export class TokenizerUserCredentials {
+  access_token: string;
+  expires_in: number;
+}
+
+export class TokenizerResponseDto extends WebResponse {
+  credentials: string;
+}
