a42269224784c7b2a60b3bec952a664829961736
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-04-04 04:59:18 -0400

    Added user authentication end-to-end test (Uncompleted)

--- apps/server/v2/src/cores/auth/tests/auth.e2e-spec.ts
+++ apps/server/v2/src/cores/auth/tests/auth.e2e-spec.ts
@@ -3,23 +3,31 @@ import { AuthModule } from "../auth.module";
 import { LibsModule } from "@/libs/libs.module";
 import { CommonModule } from "@/common/common.module";
 import { TestModule } from "../../../../test/test.module";
-import { LifecycleProvider } from "test/providers";
+import { LifecycleProvider } from "../../../../test/providers";
 import { PrismaProvider } from "@/libs/providers";
+import * as supertest from "supertest";
+import { INestApplication } from "@nestjs/common";
+import { ResponseError } from "@/model/error.model";
+import { ErrorProvider } from "@/common/providers";
 
 describe("Auth Controller (e2e)", () => {
+  let app: INestApplication;
   let testLifecycle: LifecycleProvider;
   let prisma: PrismaProvider;
+  let errorService: ErrorProvider;
 
   beforeAll(async () => {
     const module: Awaited<TestingModule> = await Test.createTestingModule({
       imports: [AuthModule, LibsModule, CommonModule, TestModule],
     }).compile();
 
+    app = module.createNestApplication();
     testLifecycle = module.get<LifecycleProvider>(LifecycleProvider);
     prisma = module.get<PrismaProvider>(PrismaProvider);
+    errorService = module.get<ErrorProvider>(ErrorProvider);
 
-    await testLifecycle.ModuleTestInit();
     await module.init();
+    await testLifecycle.ModuleTestInit();
   });
 
   afterAll(async () => {
@@ -28,4 +36,14 @@ describe("Auth Controller (e2e)", () => {
   });
 
   afterEach(async () => await prisma.$disconnect());
+
+  describe("GET /auth/tokenizer", () => {
+    it("should be throw unauthorized exception error response", async () => {
+      const response: Awaited<supertest.Request | supertest.Response> =
+        await supertest(app.getHttpServer()).get("/auth/tokenizer");
+      const body: ResponseError = response.body as ResponseError;
+
+      expect(body).toEqual(errorService.unauthorized());
+    });
+  });
 });
