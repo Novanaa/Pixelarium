0fb7dadb4531a2228c1219e351f6d02060ed3b05
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-04-06 06:27:30 -0400

    Added a few authentication refresh token rotation end-to-end test

--- apps/server/v2/src/cores/auth/tests/auth.e2e-spec.ts
+++ apps/server/v2/src/cores/auth/tests/auth.e2e-spec.ts
@@ -8,14 +8,21 @@ import { PrismaProvider } from "@/libs/providers";
 import * as supertest from "supertest";
 import { HttpStatus, INestApplication } from "@nestjs/common";
 import { ResponseError } from "@/model/error.model";
-import { ErrorProvider } from "@/common/providers";
+import { ErrorProvider, MockDataProvider } from "@/common/providers";
 import * as cookieParser from "cookie-parser";
+import { UserInfoProvider } from "../providers";
+import { User } from "@prisma/client";
+import { UserInfoGenerateCredentials } from "../providers/userinfo/userinfo";
+import { TokenizerResponseDto } from "../providers/tokenizer/tokenizer.dto";
+import { AuthConstant } from "@/constant/auth.constant";
 
 describe("Auth Controller (e2e)", () => {
   let app: INestApplication;
   let testLifecycle: LifecycleProvider;
   let prisma: PrismaProvider;
   let errorService: ErrorProvider;
+  let userinfoService: UserInfoProvider;
+  let mockDataService: MockDataProvider;
 
   beforeAll(async () => {
     const module: Awaited<TestingModule> = await Test.createTestingModule({
@@ -26,6 +33,8 @@ describe("Auth Controller (e2e)", () => {
     testLifecycle = module.get<LifecycleProvider>(LifecycleProvider);
     prisma = module.get<PrismaProvider>(PrismaProvider);
     errorService = module.get<ErrorProvider>(ErrorProvider);
+    userinfoService = module.get<UserInfoProvider>(UserInfoProvider);
+    mockDataService = module.get<MockDataProvider>(MockDataProvider);
 
     app.use(cookieParser());
 
@@ -71,6 +80,143 @@ describe("Auth Controller (e2e)", () => {
       const body: ResponseError = response.body as ResponseError;
 
       expect(body).toEqual(errorService.forbidden());
+    });
+    it("should be return 403 status code if the session cookies is invalid", async () => {
+      const response: Awaited<supertest.Request | supertest.Response> =
+        await supertest(app.getHttpServer())
+          .get("/auth/tokenizer")
+          .set("Cookie", "session=test");
+      const body: ResponseError = response.body as ResponseError;
+
+      expect(body.code).toBe(HttpStatus.FORBIDDEN);
+      expect(response.status).toBe(HttpStatus.FORBIDDEN);
+    });
+    it("status response should be 'KO' if the session cookies is invalid", async () => {
+      const response: Awaited<supertest.Request | supertest.Response> =
+        await supertest(app.getHttpServer())
+          .get("/auth/tokenizer")
+          .set("Cookie", "session=test");
+      const body: ResponseError = response.body as ResponseError;
+
+      expect(body.status).toBe("KO");
+    });
+    it("should be return 200 status code", async () => {
+      const user: Awaited<User> = await mockDataService.getRandomser();
+      const credentials: UserInfoGenerateCredentials =
+        userinfoService.generateCredentials(user);
+      const response: Awaited<supertest.Request | supertest.Response> =
+        await supertest(app.getHttpServer())
+          .get("/auth/tokenizer")
+          .set("Cookie", "session=" + credentials.refreshToken);
+      const body: TokenizerResponseDto = response.body as TokenizerResponseDto;
+
+      expect(body.code).toBe(HttpStatus.OK);
+      expect(response.status).toBe(HttpStatus.OK);
+    });
+    it("status response should be 'OK'", async () => {
+      const user: Awaited<User> = await mockDataService.getRandomser();
+      const credentials: UserInfoGenerateCredentials =
+        userinfoService.generateCredentials(user);
+      const response: Awaited<supertest.Request | supertest.Response> =
+        await supertest(app.getHttpServer())
+          .get("/auth/tokenizer")
+          .set("Cookie", "session=" + credentials.refreshToken);
+      const body: TokenizerResponseDto = response.body as TokenizerResponseDto;
+
+      expect(body.status).toBe("OK");
+    });
+    it("make sure it can accept application/json", async () => {
+      const user: Awaited<User> = await mockDataService.getRandomser();
+      const credentials: UserInfoGenerateCredentials =
+        userinfoService.generateCredentials(user);
+      const response: Awaited<supertest.Request | supertest.Response> =
+        await supertest(app.getHttpServer())
+          .get("/auth/tokenizer")
+          .set("Cookie", "session=" + credentials.refreshToken)
+          .set("Content-Type", "application/json");
+      const body: TokenizerResponseDto = response.body as TokenizerResponseDto;
+
+      expect(body.status).toBe("OK");
+    });
+    it("user credentials response data should be defined", async () => {
+      const user: Awaited<User> = await mockDataService.getRandomser();
+      const credentials: UserInfoGenerateCredentials =
+        userinfoService.generateCredentials(user);
+      const response: Awaited<supertest.Request | supertest.Response> =
+        await supertest(app.getHttpServer())
+          .get("/auth/tokenizer")
+          .set("Cookie", "session=" + credentials.refreshToken)
+          .set("Content-Type", "application/json");
+      const body: TokenizerResponseDto = response.body as TokenizerResponseDto;
+
+      expect(body.credentials).toBeDefined();
+    });
+    it("user credentials access_token response data should be defined", async () => {
+      const user: Awaited<User> = await mockDataService.getRandomser();
+      const credentials: UserInfoGenerateCredentials =
+        userinfoService.generateCredentials(user);
+      const response: Awaited<supertest.Request | supertest.Response> =
+        await supertest(app.getHttpServer())
+          .get("/auth/tokenizer")
+          .set("Cookie", "session=" + credentials.refreshToken)
+          .set("Content-Type", "application/json");
+      const body: TokenizerResponseDto = response.body as TokenizerResponseDto;
+
+      expect(body.credentials.access_token).toBeDefined();
+    });
+    it("user credentials expires_in response data should be defined", async () => {
+      const user: Awaited<User> = await mockDataService.getRandomser();
+      const credentials: UserInfoGenerateCredentials =
+        userinfoService.generateCredentials(user);
+      const response: Awaited<supertest.Request | supertest.Response> =
+        await supertest(app.getHttpServer())
+          .get("/auth/tokenizer")
+          .set("Cookie", "session=" + credentials.refreshToken)
+          .set("Content-Type", "application/json");
+      const body: TokenizerResponseDto = response.body as TokenizerResponseDto;
+
+      expect(body.credentials.expires_in).toBeDefined();
+    });
+    it("user credentials expires_in response data should be a number", async () => {
+      const user: Awaited<User> = await mockDataService.getRandomser();
+      const credentials: UserInfoGenerateCredentials =
+        userinfoService.generateCredentials(user);
+      const response: Awaited<supertest.Request | supertest.Response> =
+        await supertest(app.getHttpServer())
+          .get("/auth/tokenizer")
+          .set("Cookie", "session=" + credentials.refreshToken)
+          .set("Content-Type", "application/json");
+      const body: TokenizerResponseDto = response.body as TokenizerResponseDto;
+
+      expect(typeof body.credentials.expires_in).toBe("number");
+    });
+    it("user credentials access_token response data should be a string", async () => {
+      const user: Awaited<User> = await mockDataService.getRandomser();
+      const credentials: UserInfoGenerateCredentials =
+        userinfoService.generateCredentials(user);
+      const response: Awaited<supertest.Request | supertest.Response> =
+        await supertest(app.getHttpServer())
+          .get("/auth/tokenizer")
+          .set("Cookie", "session=" + credentials.refreshToken)
+          .set("Content-Type", "application/json");
+      const body: TokenizerResponseDto = response.body as TokenizerResponseDto;
+
+      expect(typeof body.credentials.access_token).toBe("string");
+    });
+    it("user credentials access_token response data should be a jwt", async () => {
+      const user: Awaited<User> = await mockDataService.getRandomser();
+      const credentials: UserInfoGenerateCredentials =
+        userinfoService.generateCredentials(user);
+      const response: Awaited<supertest.Request | supertest.Response> =
+        await supertest(app.getHttpServer())
+          .get("/auth/tokenizer")
+          .set("Cookie", "session=" + credentials.refreshToken)
+          .set("Content-Type", "application/json");
+      const body: TokenizerResponseDto = response.body as TokenizerResponseDto;
+
+      expect(
+        AuthConstant.JWT_PARTTERN.test(body.credentials.access_token)
+      ).toBe(true);
     });
   });
 });
