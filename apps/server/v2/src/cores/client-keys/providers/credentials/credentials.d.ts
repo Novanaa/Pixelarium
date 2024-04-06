b5fa877d6bd9c9c2fd2cb02819de0600984d61bc
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-04-06 09:05:16 -0400

    Added generate client keys credentials param interface

--- apps/server/v2/src/cores/client-keys/providers/credentials/credentials.d.ts
+++ apps/server/v2/src/cores/client-keys/providers/credentials/credentials.d.ts
@@ -2,3 +2,8 @@ export interface GeneratedClientKeysCredentials {
   clientId: string;
   clientSecret: string;
 }
+
+export interface GenerateClientKeysCredentialsParams {
+  originCode: number;
+  name: string;
+}
