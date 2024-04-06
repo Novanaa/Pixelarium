91134826d574c34942a747958772335359683cb9
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-04-06 10:15:27 -0400

    Added retrieve user client credentials keys api endpoint handler

--- apps/server/v2/src/cores/client-keys/client-keys.controller.ts
+++ apps/server/v2/src/cores/client-keys/client-keys.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from "@nestjs/common";
+import { PrismaProvider } from "@/libs/providers";
+import {
+  Controller,
+  Get,
+  Header,
+  HttpCode,
+  HttpStatus,
+  Param,
+} from "@nestjs/common";
+import { RetrieveUserClientCredentialsKeysProvider } from "./providers";
+import { RetrieveUserClientCredentialsKeysResponseDto } from "./providers/retrieve-credentials/retrieve-credentials.dto";
 
 @Controller("client-keys")
-export class ClientKeysController {}
+export class ClientKeysController {
+  constructor(
+    private readonly prisma: PrismaProvider,
+    private readonly retrieveCredentials: RetrieveUserClientCredentialsKeysProvider
+  ) {}
+
+  @Get("/:name")
+  @Header("Content-Type", "application/json")
+  @Header("Accept", "application/json")
+  @HttpCode(HttpStatus.OK)
+  public async retrieveUserClientCredentialsKeys(
+    @Param("name") name: string
+  ): Promise<RetrieveUserClientCredentialsKeysResponseDto> {
+    try {
+      return await this.retrieveCredentials.retrieveCredentials(name);
+    } finally {
+      await this.prisma.$disconnect();
+    }
+  }
+}
