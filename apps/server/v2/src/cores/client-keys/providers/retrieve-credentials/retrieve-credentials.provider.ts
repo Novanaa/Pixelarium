58fbf55ab4e130d5e81de3e3653f6d4fe755bd6f
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-04-06 10:15:56 -0400

    Added retrieve user client credentials keys api core logic

--- apps/server/v2/src/cores/client-keys/providers/retrieve-credentials/retrieve-credentials.provider.ts
+++ apps/server/v2/src/cores/client-keys/providers/retrieve-credentials/retrieve-credentials.provider.ts
@@ -1,6 +1,36 @@
-import { Injectable } from "@nestjs/common";
+import { HttpStatus, Injectable } from "@nestjs/common";
+import { RetrieveUserProvider } from "@/cores/user/providers";
+import { RetrieveUserClientCredentialsKeysResponseDto } from "./retrieve-credentials.dto";
+import { RetrieveUserResponseDto } from "@/cores/user/providers/retrieve-user/retrieve-user.dto";
+import { ClientKey } from "@prisma/client";
+import { PrismaProvider } from "@/libs/providers";
 
 @Injectable()
 export class RetrieveUserClientCredentialsKeysProvider {
-  constructor() {}
+  constructor(
+    private readonly retrieveUser: RetrieveUserProvider,
+    private readonly prisma: PrismaProvider
+  ) {}
+
+  public async retrieveUserClientKeys(userId: string): Promise<ClientKey> {
+    return await this.prisma.clientKey.findUnique({
+      where: { user_id: userId },
+    });
+  }
+
+  public async retrieveCredentials(
+    name: string
+  ): Promise<RetrieveUserClientCredentialsKeysResponseDto> {
+    const { user }: Awaited<RetrieveUserResponseDto> =
+      await this.retrieveUser.retrieveSingleUser(name);
+
+    const userCredentialsKeys: Awaited<ClientKey> =
+      await this.retrieveUserClientKeys(user.id);
+
+    return {
+      credentials_keys: userCredentialsKeys,
+      code: HttpStatus.OK,
+      status: "OK",
+    };
+  }
 }
