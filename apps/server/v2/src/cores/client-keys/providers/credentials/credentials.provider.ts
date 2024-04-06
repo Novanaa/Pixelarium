509edb27d06ac5a57b0a0a3d35db2f630a296fa5
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-04-06 09:06:08 -0400

    Added generate client keys property method

--- apps/server/v2/src/cores/client-keys/providers/credentials/credentials.provider.ts
+++ apps/server/v2/src/cores/client-keys/providers/credentials/credentials.provider.ts
@@ -1,9 +1,24 @@
+import * as crypto from "crypto";
 import { Injectable } from "@nestjs/common";
-import { GeneratedClientKeysCredentials } from "./credentials";
+import {
+  GenerateClientKeysCredentialsParams,
+  GeneratedClientKeysCredentials,
+} from "./credentials";
 
 @Injectable()
 export class CredentialsProvider {
-  public generateClientKeys(): GeneratedClientKeysCredentials {
-    return { clientId: "", clientSecret: "" };
+  public generateClientKeys(
+    param: GenerateClientKeysCredentialsParams
+  ): GeneratedClientKeysCredentials {
+    const clientId: string = crypto
+      .createHash("sha256")
+      .update(param.name)
+      .digest("hex");
+    const clientSecret: string = crypto
+      .createHash("sha256")
+      .update(String(param.originCode))
+      .digest("hex");
+
+    return { clientId, clientSecret };
   }
 }
