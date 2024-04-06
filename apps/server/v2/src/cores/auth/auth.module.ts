1801634115a8153d1b3be9005abedb13d4d7e85b
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-04-06 09:05:41 -0400

    Imported client keys module

--- apps/server/v2/src/cores/auth/auth.module.ts
+++ apps/server/v2/src/cores/auth/auth.module.ts
@@ -2,9 +2,10 @@ import * as provider from "./providers";
 import { Module } from "@nestjs/common";
 import { AuthController } from "./auth.controller";
 import { UserModule } from "../user/user.module";
+import { ClientKeysModule } from "../client-keys/client-keys.module";
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, ClientKeysModule],
   controllers: [AuthController],
   providers: [
     provider.GoogleAuthProvider,
