53132f925982a2dc0a9b0dfbc8082275617e08c5
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-04-06 08:17:55 -0400

    Registered client keys credentials provider

--- apps/server/v2/src/cores/client-keys/client-keys.module.ts
+++ apps/server/v2/src/cores/client-keys/client-keys.module.ts
@@ -1,7 +1,9 @@
 import { Module } from "@nestjs/common";
 import { ClientKeysController } from "./client-keys.controller";
+import * as provider from "./providers";
 
 @Module({
   controllers: [ClientKeysController],
+  providers: [provider.CredentialsProvider],
 })
 export class ClientKeysModule {}
