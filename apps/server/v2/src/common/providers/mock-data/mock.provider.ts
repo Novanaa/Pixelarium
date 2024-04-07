294bc0456733a4361b4c9e8f991f7c3166947684
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-04-06 21:18:50 -0400

    Added user client keys to test user

--- apps/server/v2/src/common/providers/mock-data/mock.provider.ts
+++ apps/server/v2/src/common/providers/mock-data/mock.provider.ts
@@ -5,10 +5,18 @@ import { Injectable } from "@nestjs/common";
 import { PrismaProvider } from "@/libs/providers/prisma-client/prisma.provider";
 import slugify from "slugify";
 import { PictureConstant } from "@/constant/picture.constant";
+import {
+  GenerateClientKeysCredentialsParams,
+  GeneratedClientKeysCredentials,
+} from "@/cores/client-keys/providers/credentials/credentials";
+import { CredentialsProvider } from "@/cores/client-keys/providers";
 
 @Injectable()
 export class MockDataProvider {
-  constructor(private readonly prisma: PrismaProvider) {}
+  constructor(
+    private readonly prisma: PrismaProvider,
+    private readonly clientKeysCredentials: CredentialsProvider
+  ) {}
 
   public async getRandomser(): Promise<User> {
     try {
@@ -19,21 +27,57 @@ export class MockDataProvider {
   }
 
   public generateRandomUser(): Omit<User, "created_at" | "updated_at" | "id"> {
+    const originCode: number = falso.randNumber();
+    const name: string = slugify(falso.randUserName(), {
+      lower: true,
+      remove: /\d+/,
+    });
+
     return {
-      name: slugify(falso.randUserName(), { lower: true, remove: /\d+/ }),
+      name,
       email: falso.randEmail(),
       avatar: falso.randAvatar(),
       bio: falso.randSentence(),
       is_member: falso.randBoolean(),
-      origin_code: falso.randNumber(),
+      origin_code: originCode,
       type: "Admin",
       password: falso.randPassword(),
     };
   }
 
+  public generateRandomClientCredentialsKeys(
+    param: GenerateClientKeysCredentialsParams
+  ) {
+    const credentials: GeneratedClientKeysCredentials =
+      this.clientKeysCredentials.generateClientKeys({
+        name: param.name,
+        originCode: param.originCode,
+      });
+
+    return {
+      client_id: credentials.clientId,
+      client_secret: credentials.clientSecret,
+    };
+  }
+
   public async createRandomUser(): Promise<User> {
     try {
-      return await this.prisma.user.create({ data: this.generateRandomUser() });
+      const generatedUser = this.generateRandomUser();
+      const generatedClientKeys = this.generateRandomClientCredentialsKeys({
+        name: generatedUser.name,
+        originCode: generatedUser.origin_code,
+      });
+
+      return await this.prisma.user.create({
+        data: {
+          ...generatedUser,
+          client_key: {
+            create: {
+              ...generatedClientKeys,
+            },
+          },
+        },
+      });
     } finally {
       await this.prisma.$disconnect();
     }
