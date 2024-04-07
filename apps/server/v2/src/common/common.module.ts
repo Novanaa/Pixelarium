31b99bcf49da57744c78ea159cf67b30224d4876
Nvaa <130564362+ItsNvaa@users.noreply.github.com>
2024-04-06 21:19:48 -0400

    [PIX-206] Add User Client Keys Data to Test User (#525)
    * Registered user client credentials keys module
    
    * Added user client keys to test user

--- apps/server/v2/src/common/common.module.ts
+++ apps/server/v2/src/common/common.module.ts
@@ -1,8 +1,10 @@
 import { Global, Module } from "@nestjs/common";
 import * as commonProviders from "./providers";
+import { ClientKeysModule } from "@/cores/client-keys/client-keys.module";
 
 @Global()
 @Module({
+  imports: [ClientKeysModule],
   providers: [
     commonProviders.ErrorProvider,
     commonProviders.MockDataProvider,
