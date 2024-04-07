537e2ff87a4dac994b4c1d979734cb9a7bfd9571
Nvaa <130564362+ItsNvaa@users.noreply.github.com>
2024-04-07 03:41:43 -0400

    [PIX-210] Remove Unnecessary Module (#533)
    * Removed @nestjs/config package
    
    * Removed config module from libs module

--- apps/server/v2/src/libs/libs.module.ts
+++ apps/server/v2/src/libs/libs.module.ts
@@ -1,10 +1,8 @@
 import { Global, Module } from "@nestjs/common";
-import { ConfigModule } from "@nestjs/config";
 import providers from "./providers";
 
 @Global()
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
   providers,
   exports: providers,
 })
