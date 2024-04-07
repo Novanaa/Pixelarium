60d65a2ad3b61411e8a0193d168f1b90d0d2a2c4
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-04-07 10:18:41 -0400

    Added retrieve user gallery end-to-end test

--- apps/server/v2/src/cores/gallery/test/gallery.e2e-spec.ts
+++ apps/server/v2/src/cores/gallery/test/gallery.e2e-spec.ts
@@ -6,12 +6,19 @@ import { GalleryModule } from "../gallery.module";
 import { TestModule } from "../../../../test/test.module";
 import { LifecycleProvider } from "../../../../test/providers";
 import { PrismaProvider } from "@/libs/providers";
-import { INestApplication } from "@nestjs/common";
+import { HttpStatus, INestApplication } from "@nestjs/common";
+import * as supertest from "supertest";
+import { ResponseError } from "@/model/error.model";
+import { ErrorProvider, MockDataProvider } from "@/common/providers";
+import { User } from "@prisma/client";
+import { RetrieveUserGalleryResponseDto } from "../providers/retrieve-gallery/retrieve-gallery.dto";
 
 describe("GalleryController", () => {
   let app: INestApplication;
   let lifecycle: LifecycleProvider;
   let prisma: PrismaProvider;
+  let error: ErrorProvider;
+  let mockData: MockDataProvider;
 
   beforeAll(async () => {
     const module: Awaited<TestingModule> = await Test.createTestingModule({
@@ -27,6 +34,8 @@ describe("GalleryController", () => {
     app = module.createNestApplication();
     lifecycle = module.get<LifecycleProvider>(LifecycleProvider);
     prisma = module.get<PrismaProvider>(PrismaProvider);
+    error = module.get<ErrorProvider>(ErrorProvider);
+    mockData = module.get<MockDataProvider>(MockDataProvider);
 
     await app.init();
     await lifecycle.ModuleTestInit();
@@ -39,7 +48,91 @@ describe("GalleryController", () => {
 
   afterEach(async () => await prisma.$disconnect());
 
-  it("should be defined", () => {
-    expect(app).toBeDefined();
+  describe("GET /gallery/:name", () => {
+    it("should be throw not found exception error response if the user doesn't exist", async () => {
+      const response: Awaited<supertest.Response | supertest.Request> =
+        await supertest(app.getHttpServer()).get("/gallery/0");
+      const body: ResponseError = response.body as ResponseError;
+
+      expect(body).toEqual(error.notFound());
+    });
+    it("should be return 404 status code if the user doesn't exist", async () => {
+      const response: Awaited<supertest.Response | supertest.Request> =
+        await supertest(app.getHttpServer()).get("/gallery/0");
+      const body: ResponseError = response.body as ResponseError;
+
+      expect(body.code).toBe(HttpStatus.NOT_FOUND);
+      expect(response.status).toBe(HttpStatus.NOT_FOUND);
+    });
+    it("status response should be 'KO' if the user doesn't exist", async () => {
+      const response: Awaited<supertest.Response | supertest.Request> =
+        await supertest(app.getHttpServer()).get("/gallery/0");
+      const body: ResponseError = response.body as ResponseError;
+
+      expect(body.status).toBe("KO");
+    });
+    it("should be return 200 status code", async () => {
+      const user: Awaited<User> = await mockData.getRandomser();
+      const response: Awaited<supertest.Response | supertest.Request> =
+        await supertest(app.getHttpServer()).get("/gallery/" + user.name);
+      const body: RetrieveUserGalleryResponseDto =
+        response.body as RetrieveUserGalleryResponseDto;
+
+      expect(body.code).toBe(HttpStatus.OK);
+      expect(response.status).toBe(HttpStatus.OK);
+    });
+    it("status response should be 'OK'", async () => {
+      const user: Awaited<User> = await mockData.getRandomser();
+      const response: Awaited<supertest.Response | supertest.Request> =
+        await supertest(app.getHttpServer()).get("/gallery/" + user.name);
+      const body: RetrieveUserGalleryResponseDto =
+        response.body as RetrieveUserGalleryResponseDto;
+
+      expect(body.status).toBe("OK");
+    });
+    it("make sure it can accept application/json", async () => {
+      const user: Awaited<User> = await mockData.getRandomser();
+      const response: Awaited<supertest.Response | supertest.Request> =
+        await supertest(app.getHttpServer())
+          .get("/gallery/" + user.name)
+          .set("Content-Type", "application/json");
+      const body: RetrieveUserGalleryResponseDto =
+        response.body as RetrieveUserGalleryResponseDto;
+
+      expect(body.status).toBe("OK");
+    });
+    it("user_gallery response data field should be defined", async () => {
+      const user: Awaited<User> = await mockData.getRandomser();
+      const response: Awaited<supertest.Response | supertest.Request> =
+        await supertest(app.getHttpServer())
+          .get("/gallery/" + user.name)
+          .set("Content-Type", "application/json");
+      const body: RetrieveUserGalleryResponseDto =
+        response.body as RetrieveUserGalleryResponseDto;
+
+      expect(body.user_gallery).toBeDefined();
+    });
+    it("user_gallery pictures response data field should be defined", async () => {
+      const user: Awaited<User> = await mockData.getRandomser();
+      const response: Awaited<supertest.Response | supertest.Request> =
+        await supertest(app.getHttpServer())
+          .get("/gallery/" + user.name)
+          .set("Content-Type", "application/json");
+      const body: RetrieveUserGalleryResponseDto =
+        response.body as RetrieveUserGalleryResponseDto;
+
+      expect(body.user_gallery.pictures).toBeDefined();
+    });
+    it("user_gallery pictures response data field should be an array", async () => {
+      const user: Awaited<User> = await mockData.getRandomser();
+      const response: Awaited<supertest.Response | supertest.Request> =
+        await supertest(app.getHttpServer())
+          .get("/gallery/" + user.name)
+          .set("Content-Type", "application/json");
+      const body: RetrieveUserGalleryResponseDto =
+        response.body as RetrieveUserGalleryResponseDto;
+
+      expect(Array.isArray(body.user_gallery.pictures)).toBe(true);
+    });
   });
 });
