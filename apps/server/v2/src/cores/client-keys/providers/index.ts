f37df596874568e43b71ae452c5dd37fb7199f1e
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-04-07 03:18:36 -0400

    Simplefied user client keys provider

--- apps/server/v2/src/cores/client-keys/providers/index.ts
+++ apps/server/v2/src/cores/client-keys/providers/index.ts
@@ -2,3 +2,5 @@ import { CredentialsProvider } from "./credentials/credentials.provider";
 import { RetrieveUserClientCredentialsKeysProvider } from "./retrieve-credentials/retrieve-credentials.provider";
 
 export { CredentialsProvider, RetrieveUserClientCredentialsKeysProvider };
+
+export default [CredentialsProvider, RetrieveUserClientCredentialsKeysProvider];
