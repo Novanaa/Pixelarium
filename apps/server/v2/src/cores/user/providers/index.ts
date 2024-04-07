e5215a516a1b3bd484b9e8e66780cd61b685cd11
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-04-07 03:20:23 -0400

    Simplefied user provider

--- apps/server/v2/src/cores/user/providers/index.ts
+++ apps/server/v2/src/cores/user/providers/index.ts
@@ -3,3 +3,5 @@ import { RetrieveUserProvider } from "./retrieve-user/retrieve-user.provider";
 import { UpdateUserProvider } from "./update-user/update-user.provider";
 
 export { RetrieveUserProvider, DeleteUserProvider, UpdateUserProvider };
+
+export default [RetrieveUserProvider, DeleteUserProvider, UpdateUserProvider];
