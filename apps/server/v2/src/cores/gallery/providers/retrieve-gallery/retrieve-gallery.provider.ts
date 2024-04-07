6eb5c144a18ac51c414c19fb6db829f992234df6
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-04-07 07:37:37 -0400

    Added retrieve user gallery api core logic

--- apps/server/v2/src/cores/gallery/providers/retrieve-gallery/retrieve-gallery.provider.ts
+++ apps/server/v2/src/cores/gallery/providers/retrieve-gallery/retrieve-gallery.provider.ts
@@ -1,4 +1,47 @@
-import { Injectable } from "@nestjs/common";
+import { HttpStatus, Injectable } from "@nestjs/common";
+import { RetrieveUserGalleryResponseDto } from "./retrieve-gallery.dto";
+import { PrismaProvider } from "@/libs/providers";
+import { RetrieveUserProvider } from "@/cores/user/providers";
+import { RetrieveUserResponseDto } from "@/cores/user/providers/retrieve-user/retrieve-user.dto";
+import { Gallery, Picture } from "@prisma/client";
 
 @Injectable()
-export class RetrieveUserGalleryProvider {}
+export class RetrieveUserGalleryProvider {
+  constructor(
+    private readonly prisma: PrismaProvider,
+    private readonly retrieveUserService: RetrieveUserProvider
+  ) {}
+
+  public async retrieveUserGalleryByUserId(userId: string) {
+    return await this.prisma.gallery.findUnique({
+      where: { user_id: userId },
+    });
+  }
+
+  public async retrieveUserGalleryPictures(
+    galleryId: string
+  ): Promise<Array<Picture>> {
+    return await this.prisma.picture.findMany({
+      where: { gallery_id: galleryId },
+    });
+  }
+
+  public async retrieveUserGallery(
+    name: string
+  ): Promise<RetrieveUserGalleryResponseDto> {
+    const { user }: Awaited<RetrieveUserResponseDto> =
+      await this.retrieveUserService.retrieveSingleUser(name);
+
+    const userGallery: Awaited<Gallery> =
+      await this.retrieveUserGalleryByUserId(user.id);
+
+    const userGalleryPictures: Awaited<Array<Picture>> =
+      await this.retrieveUserGalleryPictures(userGallery.id);
+
+    return {
+      user_gallery: { ...userGallery, pictures: userGalleryPictures },
+      code: HttpStatus.OK,
+      status: "OK",
+    };
+  }
+}
