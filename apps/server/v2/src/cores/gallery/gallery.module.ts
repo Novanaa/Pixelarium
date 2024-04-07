77ae9f007e245209bb0c94ed7ff3951a0afb923f
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-04-07 07:15:02 -0400

    Registered user module to gallery module

--- apps/server/v2/src/cores/gallery/gallery.module.ts
+++ apps/server/v2/src/cores/gallery/gallery.module.ts
@@ -1,8 +1,10 @@
 import providers from "./providers";
 import { Module } from "@nestjs/common";
 import { GalleryController } from "./gallery.controller";
+import { UserModule } from "../user/user.module";
 
 @Module({
+  imports: [UserModule],
   controllers: [GalleryController],
   providers,
 })
