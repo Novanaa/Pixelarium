2c4782580b26ce05ddbe2a8680f795b2e5b72496
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-04-08 06:48:50 -0400

    Added application exception filter

---
+++ apps/server/v2/src/filter/error.filter.ts
@@ -0,0 +1,40 @@
+/* eslint-disable indent */
+
+import { ErrorProvider } from "@/common/providers";
+import { ResponseError } from "@/model/error.model";
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+} from "@nestjs/common";
+import { HttpArgumentsHost } from "@nestjs/common/interfaces";
+import { Response } from "express";
+
+export class ResponseException extends ResponseError {
+  error: string;
+}
+
+@Catch(HttpException)
+export class ApplicationExceptionFilter
+  implements ExceptionFilter<HttpException>
+{
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const http: HttpArgumentsHost = host.switchToHttp();
+    const response: Response = http.getResponse<Response>();
+    const status: number = exception.getStatus();
+    const exceptionResponse: ResponseException =
+      exception.getResponse() as ResponseException;
+
+    return response.status(status).json(
+      new ErrorProvider().custom({
+        code: status,
+        messege: exception.message,
+        TypeError:
+          exceptionResponse.TypeError ||
+          exceptionResponse.error ||
+          exception.name,
+      })
+    );
+  }
+}
