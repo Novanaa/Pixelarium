3e057a9dae77ae4d02be0421a07fbeaf0f5c7db9
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-04-08 08:28:04 -0400

    Added unfavorite user picture api endpoint handler

--- apps/server/v2/src/cores/favorite-picture/favorite-picture.controller.ts
+++ apps/server/v2/src/cores/favorite-picture/favorite-picture.controller.ts
@@ -1,20 +1,29 @@
 import {
   Controller,
+  Delete,
   Get,
   Header,
   HttpCode,
   HttpStatus,
   Param,
+  ParseUUIDPipe,
+  UseFilters,
 } from "@nestjs/common";
 import { RetrieveUserFavoritesPicturesResponseDto } from "./providers/retrieve-favorites/retrieve-favotires.dto";
 import { PrismaProvider } from "@/libs/providers";
-import { RetrieveUserFavoritesPicturesProvider } from "./providers";
+import {
+  RetrieveUserFavoritesPicturesProvider,
+  UnfavoritePictureProvider,
+} from "./providers";
+import { ApplicationExceptionFilter } from "@/filter/error.filter";
+import { UnfavoritePictureResponseDTO } from "./providers/unfavorite/unfavorite.dto";
 
 @Controller("favorite")
 export class FavoritePictureController {
   constructor(
     private readonly prisma: PrismaProvider,
-    private readonly retrieveFavoritesPicturesService: RetrieveUserFavoritesPicturesProvider
+    private readonly retrieveFavoritesPicturesService: RetrieveUserFavoritesPicturesProvider,
+    private readonly unfavPictureService: UnfavoritePictureProvider
   ) {}
 
   @Get("/:name")
@@ -30,6 +39,22 @@ export class FavoritePictureController {
       )) satisfies Awaited<RetrieveUserFavoritesPicturesResponseDto>;
     } finally {
       await this.prisma.$disconnect();
+    }
+  }
+
+  @Delete("/:name/:pictureId")
+  @Header("Content-Type", "application/json")
+  @Header("Accept", "application/json")
+  @HttpCode(HttpStatus.OK)
+  @UseFilters(ApplicationExceptionFilter)
+  public async unfavPicture(
+    @Param("name") name: string,
+    @Param("pictureId", ParseUUIDPipe) pictureId: string
+  ): Promise<UnfavoritePictureResponseDTO> {
+    try {
+      return await this.unfavPictureService.unfavPicture(name, pictureId);
+    } finally {
+      this.prisma.$disconnect();
     }
   }
 }
