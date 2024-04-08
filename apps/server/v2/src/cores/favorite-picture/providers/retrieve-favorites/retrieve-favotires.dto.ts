88fce2994d8fadc2866be0f6931b94c9e9799f98
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-04-08 08:56:35 -0400

    Added owner field to retrieve user favorites pictures response dto

--- apps/server/v2/src/cores/favorite-picture/providers/retrieve-favorites/retrieve-favotires.dto.ts
+++ apps/server/v2/src/cores/favorite-picture/providers/retrieve-favorites/retrieve-favotires.dto.ts
@@ -1,6 +1,8 @@
 import { UserFavoritesPictures } from "@/model/app";
 import { WebResponse } from "@/model/app.model";
+import { User } from "@prisma/client";
 
 export class RetrieveUserFavoritesPicturesResponseDto extends WebResponse {
   favorites_pictures: UserFavoritesPictures;
+  owner: User;
 }
