a94b2c500ec3d3b3a3790c8b4df6e464cedf1906
Nvaa <130564362+ItsNvaa@users.noreply.github.com>
2024-04-07 22:51:05 -0400

    [PIX-224] Test Retrieve User Favorites Pictures API Endpoint (#561)
    * Added retrieve user favorites pictures unit-test
    
    * Added retrieve user favorite pictures end-to-end test

--- apps/server/v2/src/cores/favorite-picture/test/favorite-picture.controller.spec.ts
+++ apps/server/v2/src/cores/favorite-picture/test/favorite-picture.controller.spec.ts
@@ -8,11 +8,18 @@ import { CommonModule } from "@/common/common.module";
 import { TestModule } from "../../../../test/test.module";
 import { LifecycleProvider } from "../../../../test/providers";
 import { PrismaProvider } from "@/libs/providers";
+import { HttpException, HttpStatus } from "@nestjs/common";
+import { ResponseError } from "@/model/error.model";
+import { ErrorProvider, MockDataProvider } from "@/common/providers";
+import { RetrieveUserFavoritesPicturesResponseDto } from "../providers/retrieve-favorites/retrieve-favotires.dto";
+import { User } from "@prisma/client";
 
 describe("FavoritePictureController", () => {
   let controller: FavoritePictureController;
   let lifecycle: LifecycleProvider;
   let prisma: PrismaProvider;
+  let errorService: ErrorProvider;
+  let mockData: MockDataProvider;
 
   beforeAll(async () => {
     const module: Awaited<TestingModule> = await Test.createTestingModule({
@@ -32,6 +39,8 @@ describe("FavoritePictureController", () => {
     );
     lifecycle = module.get<LifecycleProvider>(LifecycleProvider);
     prisma = module.get<PrismaProvider>(PrismaProvider);
+    errorService = module.get<ErrorProvider>(ErrorProvider);
+    mockData = module.get<MockDataProvider>(MockDataProvider);
 
     await lifecycle.ModuleTestInit();
   });
@@ -43,7 +52,86 @@ describe("FavoritePictureController", () => {
 
   afterEach(async () => await prisma.$disconnect());
 
-  it("should be defined", () => {
-    expect(controller).toBeDefined();
+  describe("GET /favorite/:name", () => {
+    it("should be throw not found exception if the user doesn't exist", async () => {
+      try {
+        await controller.retrieveUserFavoritesPictures("0");
+      } catch (error) {
+        const err: HttpException = error as HttpException;
+        const response: ResponseError = err.getResponse() as ResponseError;
+
+        expect(response).toEqual(errorService.notFound());
+      }
+    });
+    it("should be return 404 status code if the user doesn't exist", async () => {
+      try {
+        await controller.retrieveUserFavoritesPictures("0");
+      } catch (error) {
+        const err: HttpException = error as HttpException;
+        const response: ResponseError = err.getResponse() as ResponseError;
+
+        expect(response.code).toBe(HttpStatus.NOT_FOUND);
+        expect(err.getStatus()).toBe(HttpStatus.NOT_FOUND);
+      }
+    });
+    it("status response should be 'KO' if the user doesn't exist", async () => {
+      try {
+        await controller.retrieveUserFavoritesPictures("0");
+      } catch (error) {
+        const err: HttpException = error as HttpException;
+        const response: ResponseError = err.getResponse() as ResponseError;
+
+        expect(response.status).toBe("KO");
+      }
+    });
+    it("should be return 200 status code", async () => {
+      const user: Awaited<User> = await mockData.getRandomser();
+      const response: Awaited<RetrieveUserFavoritesPicturesResponseDto> =
+        await controller.retrieveUserFavoritesPictures(user.name);
+
+      expect(response.code).toBe(HttpStatus.OK);
+    });
+    it("status response should be 'OK'", async () => {
+      const user: Awaited<User> = await mockData.getRandomser();
+      const response: Awaited<RetrieveUserFavoritesPicturesResponseDto> =
+        await controller.retrieveUserFavoritesPictures(user.name);
+
+      expect(response.status).toBe("OK");
+    });
+    it("favorites_pictures response data should be defined", async () => {
+      const user: Awaited<User> = await mockData.getRandomser();
+      const response: Awaited<RetrieveUserFavoritesPicturesResponseDto> =
+        await controller.retrieveUserFavoritesPictures(user.name);
+
+      expect(response.favorites_pictures).toBeDefined();
+    });
+    it("favorites_pictures pictures response data should be defined", async () => {
+      const user: Awaited<User> = await mockData.getRandomser();
+      const response: Awaited<RetrieveUserFavoritesPicturesResponseDto> =
+        await controller.retrieveUserFavoritesPictures(user.name);
+
+      expect(response.favorites_pictures.pictures).toBeDefined();
+    });
+    it("favorites_pictures total response data should be defined", async () => {
+      const user: Awaited<User> = await mockData.getRandomser();
+      const response: Awaited<RetrieveUserFavoritesPicturesResponseDto> =
+        await controller.retrieveUserFavoritesPictures(user.name);
+
+      expect(response.favorites_pictures.total).toBeDefined();
+    });
+    it("favorites_pictures total response data should be a number", async () => {
+      const user: Awaited<User> = await mockData.getRandomser();
+      const response: Awaited<RetrieveUserFavoritesPicturesResponseDto> =
+        await controller.retrieveUserFavoritesPictures(user.name);
+
+      expect(typeof response.favorites_pictures.total).toBe("number");
+    });
+    it("favorites_pictures picture response data should be an array", async () => {
+      const user: Awaited<User> = await mockData.getRandomser();
+      const response: Awaited<RetrieveUserFavoritesPicturesResponseDto> =
+        await controller.retrieveUserFavoritesPictures(user.name);
+
+      expect(Array.isArray(response.favorites_pictures.pictures)).toBe(true);
+    });
   });
 });
