cbf4165a8edd1300709cd466dbdef24ef0e67581
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-04-08 10:05:50 -0400

    Added unfavorite user picture api core logic

--- apps/server/v2/src/cores/favorite-picture/providers/unfavorite/unfavorite.provider.ts
+++ apps/server/v2/src/cores/favorite-picture/providers/unfavorite/unfavorite.provider.ts
@@ -1,26 +1,70 @@
-import { Injectable } from "@nestjs/common";
+import { HttpStatus, Injectable, NotFoundException } from "@nestjs/common";
 import { UnfavoritePictureResponseDTO } from "./unfavorite.dto";
 import { PrismaProvider } from "@/libs/providers";
-import { RetrieveUserProvider } from "@/cores/user/providers";
-import { RetrieveUserResponseDto } from "@/cores/user/providers/retrieve-user/retrieve-user.dto";
+import { RetrieveUserFavoritesPicturesProvider } from "../retrieve-favorites/retrieve-favotires.provider";
+import { RetrieveUserFavoritesPicturesResponseDto } from "../retrieve-favorites/retrieve-favotires.dto";
+import { Favorite, Picture } from "@prisma/client";
+import { ErrorProvider } from "@/common/providers";
+import { UnlinkFavoritedPictureParams } from "./unfavorite";
 
 @Injectable()
 export class UnfavoritePictureProvider {
   constructor(
     private readonly prisma: PrismaProvider,
-    private readonly retrieveUserService: RetrieveUserProvider
+    private readonly retrieveFavoritePictureService: RetrieveUserFavoritesPicturesProvider,
+    private readonly errorService: ErrorProvider
   ) {}
+
+  public async unlinkFavoritedPicture(
+    param: UnlinkFavoritedPictureParams
+  ): Promise<Favorite> {
+    return await this.prisma.favorite.update({
+      where: { id: param.favoriteId },
+      data: {
+        pictures: {
+          disconnect: {
+            id: param.pictureId,
+          },
+        },
+      },
+    });
+  }
 
   public async unfavPicture(
     name: string,
     pictureId: string
   ): Promise<UnfavoritePictureResponseDTO> {
-    const { user }: Awaited<RetrieveUserResponseDto> =
-      await this.retrieveUserService.retrieveSingleUser(name);
+    const {
+      owner: user,
+      favorites_pictures: userFavoritePicture,
+    }: Awaited<RetrieveUserFavoritesPicturesResponseDto> =
+      (await this.retrieveFavoritePictureService.retrieveUserFavoritesPictures(
+        name
+      )) satisfies Awaited<RetrieveUserFavoritesPicturesResponseDto>;
 
-    console.log(user);
-    console.log(pictureId);
+    const favoritedPicture: Awaited<Picture> =
+      await this.retrieveFavoritePictureService.retrieveFavroitedPicture({
+        favoriteId: userFavoritePicture.id,
+        pictureId,
+      });
 
-    return null;
+    if (!favoritedPicture)
+      throw new NotFoundException(
+        this.errorService.notFound(
+          "Trying to find the picture you want to unfavorite..."
+        )
+      );
+
+    await this.unlinkFavoritedPicture({
+      favoriteId: userFavoritePicture.id,
+      pictureId: favoritedPicture.id,
+    });
+
+    return {
+      unfav_picture: favoritedPicture,
+      owner: user,
+      code: HttpStatus.OK,
+      status: "OK",
+    };
   }
 }
