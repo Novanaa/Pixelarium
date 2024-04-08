ec48c3079023062e0570d0af064762628de06fd1
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-04-07 21:51:34 -0400

    Added user gallery interface

--- apps/server/v2/src/model/app.d.ts
+++ apps/server/v2/src/model/app.d.ts
@@ -3,3 +3,7 @@ import { Favorite, Picture } from "@prisma/client";
 export interface UserFavoritesPictures extends Favorite {
   pictures: Array<Picture>;
 }
+
+export interface UserGallery extends Gallery {
+  pictures: Array<Picture>;
+}
