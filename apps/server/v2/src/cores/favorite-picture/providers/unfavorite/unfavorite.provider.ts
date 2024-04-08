4bdf4ccaf7d0f229977397528fd3e8618b38a45c
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-04-08 08:36:39 -0400

    Added unfav picture method

--- apps/server/v2/src/cores/favorite-picture/providers/unfavorite/unfavorite.provider.ts
+++ apps/server/v2/src/cores/favorite-picture/providers/unfavorite/unfavorite.provider.ts
@@ -1,4 +1,26 @@
 import { Injectable } from "@nestjs/common";
+import { UnfavoritePictureResponseDTO } from "./unfavorite.dto";
+import { PrismaProvider } from "@/libs/providers";
+import { RetrieveUserProvider } from "@/cores/user/providers";
+import { RetrieveUserResponseDto } from "@/cores/user/providers/retrieve-user/retrieve-user.dto";
 
 @Injectable()
-export class UnfavoritePictureProvider {}
+export class UnfavoritePictureProvider {
+  constructor(
+    private readonly prisma: PrismaProvider,
+    private readonly retrieveUserService: RetrieveUserProvider
+  ) {}
+
+  public async unfavPicture(
+    name: string,
+    pictureId: string
+  ): Promise<UnfavoritePictureResponseDTO> {
+    const { user }: Awaited<RetrieveUserResponseDto> =
+      await this.retrieveUserService.retrieveSingleUser(name);
+
+    console.log(user);
+    console.log(pictureId);
+
+    return null;
+  }
+}
