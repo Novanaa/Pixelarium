c8edcb72ff06ca709355d6183c7bb71e0a356ea7
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-04-12 10:15:25 +0700

    Registered user picture core providers

--- apps/server/v2/src/cores/picture/picture.module.ts
+++ apps/server/v2/src/cores/picture/picture.module.ts
@@ -1,7 +1,10 @@
+import providers from "./providers";
 import { Module } from "@nestjs/common";
 import { PictureController } from "./picture.controller";
 
 @Module({
   controllers: [PictureController],
+  exports: [],
+  providers,
 })
 export class PictureModule {}
