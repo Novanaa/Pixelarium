708c3c255932c932ccf97dd872c0dc5d65a2485c
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-04-12 10:16:02 +0700

    Imported user core module

--- apps/server/v2/src/cores/picture/picture.module.ts
+++ apps/server/v2/src/cores/picture/picture.module.ts
@@ -1,8 +1,10 @@
 import providers from "./providers";
 import { Module } from "@nestjs/common";
 import { PictureController } from "./picture.controller";
+import { UserModule } from "../user/user.module";
 
 @Module({
+  imports: [UserModule],
   controllers: [PictureController],
   exports: [],
   providers,
