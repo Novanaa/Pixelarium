5720cea1111d75da32d07eff57ac3f24f09b0fbb
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-04-12 19:31:25 +0700

    Added retrieve user picture api core logic

--- apps/server/v2/src/cores/picture/providers/retrieve-picture/retrieve-picture.provider.ts
+++ apps/server/v2/src/cores/picture/providers/retrieve-picture/retrieve-picture.provider.ts
@@ -1,4 +1,39 @@
-import { Injectable } from "@nestjs/common";
+import { PrismaProvider } from "@/libs/providers";
+import { HttpStatus, Injectable, NotFoundException } from "@nestjs/common";
+import { Picture } from "@prisma/client";
+import { RetrieveUserPictureResponseDTO } from "./retrieve-picture.dto";
+import { ErrorProvider } from "@/common/providers";
 
 @Injectable()
-export class RetrieveUserPictureProvider {}
+export class RetrieveUserPictureProvider {
+  constructor(
+    private readonly prisma: PrismaProvider,
+    private readonly error: ErrorProvider
+  ) {}
+
+  public async retrieveUserPictureById(pictureId: string): Promise<Picture> {
+    try {
+      return await this.prisma.picture.findUnique({ where: { id: pictureId } });
+    } finally {
+      await this.prisma.$disconnect();
+    }
+  }
+
+  public async retrievePicture(
+    pictureId: string
+  ): Promise<RetrieveUserPictureResponseDTO> {
+    const picture: Awaited<Picture> =
+      await this.retrieveUserPictureById(pictureId);
+
+    if (!picture)
+      throw new NotFoundException(
+        this.error.notFound("Sorry, the requested picture could not be found.")
+      );
+
+    return {
+      picture,
+      code: HttpStatus.OK,
+      status: "OK",
+    };
+  }
+}
