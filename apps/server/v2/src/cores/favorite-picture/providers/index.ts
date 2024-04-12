84094f6663e0e5e8624c80131d17eb3540444a51
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-04-12 09:31:13 +0700

    Exported add user picture favorite provider

--- apps/server/v2/src/cores/favorite-picture/providers/index.ts
+++ apps/server/v2/src/cores/favorite-picture/providers/index.ts
@@ -1,9 +1,15 @@
 import { RetrieveUserFavoritesPicturesProvider } from "./retrieve-favorites/retrieve-favotires.provider";
 import { UnfavoritePictureProvider } from "./unfavorite/unfavorite.provider";
+import { AddUserFavoritePicture } from "./add-picture/add-favorite-picture.provider";
 
-export { RetrieveUserFavoritesPicturesProvider, UnfavoritePictureProvider };
+export {
+  RetrieveUserFavoritesPicturesProvider,
+  UnfavoritePictureProvider,
+  AddUserFavoritePicture,
+};
 
 export default [
   RetrieveUserFavoritesPicturesProvider,
   UnfavoritePictureProvider,
+  AddUserFavoritePicture,
 ];
