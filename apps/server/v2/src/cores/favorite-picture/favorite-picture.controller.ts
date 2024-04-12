849d87835469cbec34afc0042d627a7e0bc4f868
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-04-12 09:55:48 +0700

    Added mark/add user favorite picture api endpoint handler

--- apps/server/v2/src/cores/favorite-picture/favorite-picture.controller.ts
+++ apps/server/v2/src/cores/favorite-picture/favorite-picture.controller.ts
@@ -7,23 +7,27 @@ import {
   HttpStatus,
   Param,
   ParseUUIDPipe,
+  Post,
   UseFilters,
 } from "@nestjs/common";
 import { RetrieveUserFavoritesPicturesResponseDto } from "./providers/retrieve-favorites/retrieve-favotires.dto";
 import { PrismaProvider } from "@/libs/providers";
 import {
+  AddUserFavoritePictureProvider,
   RetrieveUserFavoritesPicturesProvider,
   UnfavoritePictureProvider,
 } from "./providers";
 import { ApplicationExceptionFilter } from "@/filter/error.filter";
 import { UnfavoritePictureResponseDTO } from "./providers/unfavorite/unfavorite.dto";
+import { AddUserFavoritePictureResponseDTO } from "./providers/add-picture/add-favorite-picture.dto";
 
 @Controller("favorite")
 export class FavoritePictureController {
   constructor(
     private readonly prisma: PrismaProvider,
     private readonly retrieveFavoritesPicturesService: RetrieveUserFavoritesPicturesProvider,
-    private readonly unfavPictureService: UnfavoritePictureProvider
+    private readonly unfavPictureService: UnfavoritePictureProvider,
+    private readonly addUserFavoritePictureService: AddUserFavoritePictureProvider
   ) {}
 
   @Get("/:name")
@@ -55,6 +59,25 @@ export class FavoritePictureController {
       return await this.unfavPictureService.unfavPicture(name, pictureId);
     } finally {
       this.prisma.$disconnect();
+    }
+  }
+
+  @Post("/:name/:pictureId")
+  @Header("Content-Type", "application/json")
+  @Header("Accept", "application/json")
+  @HttpCode(HttpStatus.CREATED)
+  @UseFilters(ApplicationExceptionFilter)
+  public async markFavoritePicture(
+    @Param("name") name: string,
+    @Param("pictureId", ParseUUIDPipe) pictureId: string
+  ): Promise<AddUserFavoritePictureResponseDTO> {
+    try {
+      return await this.addUserFavoritePictureService.AddFavoritePictureList(
+        name,
+        pictureId
+      );
+    } finally {
+      await this.prisma.$disconnect();
     }
   }
 }
