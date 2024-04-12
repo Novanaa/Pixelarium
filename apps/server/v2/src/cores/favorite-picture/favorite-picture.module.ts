2da84bfc865ef42a50e67e619ddb81f911e62a6c
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-04-12 21:08:48 +0700

    Imported user picture module

--- apps/server/v2/src/cores/favorite-picture/favorite-picture.module.ts
+++ apps/server/v2/src/cores/favorite-picture/favorite-picture.module.ts
@@ -2,9 +2,10 @@ import providers from "./providers";
 import { Module } from "@nestjs/common";
 import { FavoritePictureController } from "./favorite-picture.controller";
 import { UserModule } from "../user/user.module";
+import { PictureModule } from "../picture/picture.module";
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, PictureModule],
   controllers: [FavoritePictureController],
   providers,
 })
