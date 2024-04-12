0e90e93a94bc110d77205153b4047b6a9a04b591
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-04-12 21:18:37 +0700

    Added mark/add favorite user picture list api core logic

--- apps/server/v2/src/cores/favorite-picture/providers/add-picture/add-favorite-picture.provider.ts
+++ apps/server/v2/src/cores/favorite-picture/providers/add-picture/add-favorite-picture.provider.ts
@@ -1,15 +1,28 @@
-import { Injectable } from "@nestjs/common";
+import { HttpStatus, Injectable } from "@nestjs/common";
 import { AddUserFavoritePictureResponseDTO } from "./add-favorite-picture.dto";
 import { PrismaProvider } from "@/libs/providers";
 import { RetrieveUserProvider } from "@/cores/user/providers";
 import { RetrieveUserResponseDto } from "@/cores/user/providers/retrieve-user/retrieve-user.dto";
+import { RetrieveUserPictureProvider } from "@/cores/picture/providers";
+import { RetrieveUserPictureResponseDTO } from "@/cores/picture/providers/retrieve-picture/retrieve-picture.dto";
 
 @Injectable()
 export class AddUserFavoritePictureProvider {
   constructor(
     private readonly prisma: PrismaProvider,
-    private readonly retrieveUserService: RetrieveUserProvider
+    private readonly retrieveUserService: RetrieveUserProvider,
+    private readonly retrieveUserPictureService: RetrieveUserPictureProvider
   ) {}
+
+  public async insertPictureToUserFavoriteList(
+    userId: string,
+    pictureId: string
+  ): Promise<void> {
+    await this.prisma.favorite.update({
+      where: { user_id: userId },
+      data: { pictures: { connect: { id: pictureId } } },
+    });
+  }
 
   public async AddFavoritePictureList(
     name: string,
@@ -18,9 +31,16 @@ export class AddUserFavoritePictureProvider {
     const { user }: Awaited<RetrieveUserResponseDto> =
       await this.retrieveUserService.retrieveSingleUser(name);
 
-    console.log(user);
-    console.log(pictureId);
+    const { picture }: Awaited<RetrieveUserPictureResponseDTO> =
+      await this.retrieveUserPictureService.retrievePicture(pictureId);
 
-    return null;
+    await this.insertPictureToUserFavoriteList(user.id, picture.id);
+
+    return {
+      added_picture: picture,
+      owner: user,
+      code: HttpStatus.CREATED,
+      status: "OK",
+    };
   }
 }
