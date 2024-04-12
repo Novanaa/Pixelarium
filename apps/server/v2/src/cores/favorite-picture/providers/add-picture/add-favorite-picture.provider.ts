cb85e38e79f8f0f0e92cef2dbf476db0a052d223
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-04-12 10:04:20 +0700

    Added favorite picture list method

--- apps/server/v2/src/cores/favorite-picture/providers/add-picture/add-favorite-picture.provider.ts
+++ apps/server/v2/src/cores/favorite-picture/providers/add-picture/add-favorite-picture.provider.ts
@@ -1,4 +1,26 @@
 import { Injectable } from "@nestjs/common";
+import { AddUserFavoritePictureResponseDTO } from "./add-favorite-picture.dto";
+import { PrismaProvider } from "@/libs/providers";
+import { RetrieveUserProvider } from "@/cores/user/providers";
+import { RetrieveUserResponseDto } from "@/cores/user/providers/retrieve-user/retrieve-user.dto";
 
 @Injectable()
-export class AddUserFavoritePicture {}
+export class AddUserFavoritePictureProvider {
+  constructor(
+    private readonly prisma: PrismaProvider,
+    private readonly retrieveUserService: RetrieveUserProvider
+  ) {}
+
+  public async AddFavoritePictureList(
+    name: string,
+    pictureId: string
+  ): Promise<AddUserFavoritePictureResponseDTO> {
+    const { user }: Awaited<RetrieveUserResponseDto> =
+      await this.retrieveUserService.retrieveSingleUser(name);
+
+    console.log(user);
+    console.log(pictureId);
+
+    return null;
+  }
+}
