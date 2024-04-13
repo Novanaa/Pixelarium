c98cdc06d58b5e775fbefd5f3ba386b68b54309b
Nvaa <130564362+ItsNvaa@users.noreply.github.com>
2024-04-13 10:33:04 +0700

    [PIX-246] Add Picture Embed Links Data to Returned Picture Response (#611)
    * Imported picture embed links module
    
    * Added embed_link field data to retrieve user picture response dto
    
    * Added picture embed link field data

--- apps/server/v2/src/cores/picture/providers/retrieve-picture/retrieve-picture.dto.ts
+++ apps/server/v2/src/cores/picture/providers/retrieve-picture/retrieve-picture.dto.ts
@@ -1,6 +1,7 @@
 import { WebResponse } from "@/model/app.model";
-import { Picture } from "@prisma/client";
+import { EmbedLinks, Picture } from "@prisma/client";
 
 export class RetrieveUserPictureResponseDTO extends WebResponse {
   picture: Picture;
+  embed_link: EmbedLinks;
 }
