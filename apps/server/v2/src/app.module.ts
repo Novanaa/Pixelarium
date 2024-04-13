0d5411c7c06c848eea72603587d71a5f1d0d91ae
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-04-13 08:32:34 +0700

    Registered picture embed links core module

--- apps/server/v2/src/app.module.ts
+++ apps/server/v2/src/app.module.ts
@@ -10,6 +10,7 @@ import { ClientKeysModule } from "./cores/client-keys/client-keys.module";
 import { GalleryModule } from "./cores/gallery/gallery.module";
 import { FavoritePictureModule } from "./cores/favorite-picture/favorite-picture.module";
 import { PictureModule } from "./cores/picture/picture.module";
+import { EmbedLinkModule } from "./cores/embed-link/embed-link.module";
 
 @Module({
   imports: [
@@ -22,6 +23,7 @@ import { PictureModule } from "./cores/picture/picture.module";
     GalleryModule,
     FavoritePictureModule,
     PictureModule,
+    EmbedLinkModule,
   ],
   controllers: [AppController],
   providers: [AppService],
