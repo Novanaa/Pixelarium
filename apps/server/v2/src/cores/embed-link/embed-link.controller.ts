26e62e43ddb68842ffb990dcb6c3ee7077634bbb
Nvaa <130564362+ItsNvaa@users.noreply.github.com>
2024-04-13 09:16:40 +0700

    [PIX-249] Implement 'Retrieve Picture Embed Link' Feature (#608)
    * Registered retrieve picture embed links provider
    
    * Exported retrieve picture embed links provider
    
    * Added retrieve picture embed links provider
    
    * Added retrieve picture embed link response dto
    
    * Added retrieve picture embed link api endpoint handler
    
    * Added retrieve picture embed link api core logic

--- apps/server/v2/src/cores/embed-link/embed-link.controller.ts
+++ apps/server/v2/src/cores/embed-link/embed-link.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from "@nestjs/common";
+import {
+  Controller,
+  Get,
+  Header,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseUUIDPipe,
+  UseFilters,
+} from "@nestjs/common";
+import { ApplicationExceptionFilter } from "@/filter/error.filter";
+import { RetrievePictureEmbedLinkResponseDTO } from "./providers/retrieve-link/retrieve-link.dto";
+import { PrismaProvider } from "@/libs/providers";
+import { RetrievePictureEmbedLinkProvider } from "./providers";
 
 @Controller("embed-link")
-export class EmbedLinkController {}
+export class EmbedLinkController {
+  constructor(
+    private readonly prisma: PrismaProvider,
+    private readonly retrievePicturEmbedLinkService: RetrievePictureEmbedLinkProvider
+  ) {}
+
+  @Get("/:pictureId")
+  @UseFilters(ApplicationExceptionFilter)
+  @Header("Content-Type", "application/json")
+  @Header("Accept", "application/json")
+  @HttpCode(HttpStatus.OK)
+  public async retrievePicturEmbedLink(
+    @Param("pictureId", ParseUUIDPipe) pictureId: string
+  ): Promise<RetrievePictureEmbedLinkResponseDTO> {
+    try {
+      return await this.retrievePicturEmbedLinkService.retrieveEmbedLink(
+        pictureId
+      );
+    } finally {
+      await this.prisma.$disconnect();
+    }
+  }
+}
