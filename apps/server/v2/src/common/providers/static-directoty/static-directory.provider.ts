9acb289a2a6c296d0d9522f096d2a98b569e9600
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-04-14 09:12:53 +0700

    Removed user gallery static directory method

--- apps/server/v2/src/common/providers/static-directoty/static-directory.provider.ts
+++ apps/server/v2/src/common/providers/static-directoty/static-directory.provider.ts
@@ -10,17 +10,7 @@ export class StaticDirectoryProvider {
     return filename ? path.join(dirpath, filename) : dirpath;
   }
 
-  public getUserGalleryDirectory(name: string, filename?: string): string {
-    const dirpath: string = "./public/galleries/" + name;
-
-    return filename ? path.join(dirpath, filename) : dirpath;
-  }
-
   public getUserAvatarUrlpath(filename: string) {
     return `${Environment.HOSTNAME}/avatar/${filename}`;
-  }
-
-  public getUserGalleryUrlpath(name: string, filename: string) {
-    return `${Environment.HOSTNAME}/galleries/${name}/${filename}`;
   }
 }
