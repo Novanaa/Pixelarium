72b2ff99b940107af298b5eec3224136fac0fab4
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-04-15 08:38:54 +0700

    Added download user picture api core logic

--- apps/server/v2/src/cores/picture/providers/download-picture/download-picture.provider.ts
+++ apps/server/v2/src/cores/picture/providers/download-picture/download-picture.provider.ts
@@ -1,6 +1,49 @@
-import { Injectable } from "@nestjs/common";
+import * as axios from "axios";
+import { ErrorProvider, ValidationProvider } from "@/common/providers";
+import { DownloadUserPicture } from "./download-picture";
+import { BadRequestException, Injectable } from "@nestjs/common";
+import { RetrieveUserPictureResponseDTO } from "../retrieve-picture/retrieve-picture.dto";
+import { RetrieveUserPictureProvider } from "../retrieve-picture/retrieve-picture.provider";
 
 @Injectable()
 export class DownloadUserPictureProvider {
-  public download(): void {}
+  constructor(
+    private readonly errorService: ErrorProvider,
+    private readonly validationService: ValidationProvider,
+    private readonly retrievePictureService: RetrieveUserPictureProvider
+  ) {}
+
+  public async downloadExternalPicture(link: string): Promise<Buffer> {
+    try {
+      const picture: Awaited<Buffer> = (await axios.default.get(link)).data;
+      const binary: Buffer = Buffer.from(picture);
+
+      if (this.validationService.brokenPicture(binary))
+        throw new BadRequestException(
+          this.errorService.badRequest("Cannot downloaded broken picture!")
+        );
+
+      return binary;
+    } catch (err) {
+      throw new BadRequestException(
+        this.errorService.badRequest("Something wrong when retrieve a picture!")
+      );
+    }
+  }
+
+  public async download(pictureId: string): Promise<DownloadUserPicture> {
+    const { picture }: Awaited<RetrieveUserPictureResponseDTO> =
+      await this.retrievePictureService.retrievePicture(pictureId);
+
+    if (picture.type == "External")
+      return {
+        binary: await this.downloadExternalPicture(picture.data),
+        filename: picture.filename,
+      };
+
+    return {
+      binary: picture.binary,
+      filename: picture.filename,
+    };
+  }
 }
