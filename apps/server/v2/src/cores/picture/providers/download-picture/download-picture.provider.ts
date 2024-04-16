07c50311930fbe0c8c2cc43c9f7d9b2f18b60e46
Nvaa <130564362+ItsNvaa@users.noreply.github.com>
2024-04-16 14:16:37 +0700

    [PIX-259] Improve Download User Picture Feature Download Speed (#629)
    * Moved download external picture from server to client based
    
    * Added validation to picture type mock data

--- apps/server/v2/src/cores/picture/providers/download-picture/download-picture.provider.ts
+++ apps/server/v2/src/cores/picture/providers/download-picture/download-picture.provider.ts
@@ -1,7 +1,6 @@
-import * as axios from "axios";
 import { DownloadUserPicture } from "./download-picture";
 import { BadRequestException, Injectable } from "@nestjs/common";
-import { ErrorProvider, ValidationProvider } from "@/common/providers";
+import { ErrorProvider } from "@/common/providers";
 import { RetrieveUserPictureResponseDTO } from "../retrieve-picture/retrieve-picture.dto";
 import { RetrieveUserPictureProvider } from "../retrieve-picture/retrieve-picture.provider";
 
@@ -9,45 +8,19 @@ import { RetrieveUserPictureProvider } from "../retrieve-picture/retrieve-pictur
 export class DownloadUserPictureProvider {
   constructor(
     private readonly errorService: ErrorProvider,
-    private readonly validationService: ValidationProvider,
     private readonly retrievePictureService: RetrieveUserPictureProvider
   ) {}
-
-  public async downloadExternalPicture(link: string): Promise<Buffer> {
-    try {
-      const picture: Awaited<axios.AxiosResponse<Buffer>> =
-        await axios.default.get(link, {
-          responseType: "arraybuffer",
-        });
-      const binary: Buffer = picture.data;
-
-      if (!Buffer.isBuffer(binary))
-        throw new BadRequestException(
-          this.errorService.badRequest("Please provide a valid picture link!")
-        );
-
-      if (this.validationService.brokenPicture(binary))
-        throw new BadRequestException(
-          this.errorService.badRequest("Cannot downloaded broken picture!")
-        );
-
-      return binary;
-    } catch (err) {
-      throw new BadRequestException(
-        this.errorService.badRequest("Something wrong when retrieve a picture!")
-      );
-    }
-  }
 
   public async download(pictureId: string): Promise<DownloadUserPicture> {
     const { picture }: Awaited<RetrieveUserPictureResponseDTO> =
       await this.retrievePictureService.retrievePicture(pictureId);
 
     if (picture.type == "External")
-      return {
-        binary: await this.downloadExternalPicture(picture.data),
-        filename: picture.filename,
-      };
+      throw new BadRequestException(
+        this.errorService.badRequest(
+          "Cannot download a picture from external link, instead use pixelarium integrated download feature."
+        )
+      );
 
     return {
       binary: picture.binary,
