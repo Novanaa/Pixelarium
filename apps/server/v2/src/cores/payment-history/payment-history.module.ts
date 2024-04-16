785f939ef5b0e72f6efdaa407f108642704383ca
Nvaa <130564362+ItsNvaa@users.noreply.github.com>
2024-04-16 19:18:56 +0700

    [PIX-261] Initialize User Payment History Testing Module (#634)
    * Added user payment history unit-test module
    
    * Added user payment history end-to-end test module
    
    * Imported user core module to payment history core module

--- apps/server/v2/src/cores/payment-history/payment-history.module.ts
+++ apps/server/v2/src/cores/payment-history/payment-history.module.ts
@@ -1,8 +1,10 @@
 import providers from "./providers";
 import { Module } from "@nestjs/common";
+import { UserModule } from "../user/user.module";
 import { PaymentHistoryController } from "./payment-history.controller";
 
 @Module({
+  imports: [UserModule],
   controllers: [PaymentHistoryController],
   providers,
 })
