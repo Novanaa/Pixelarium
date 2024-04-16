bcb1fb93f14d10346b34d084120cf2cd869bb1fb
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-04-16 19:32:37 +0700

    Added retrieve user payment history api endpoint handler

--- apps/server/v2/src/cores/payment-history/payment-history.controller.ts
+++ apps/server/v2/src/cores/payment-history/payment-history.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from "@nestjs/common";
+import {
+  Controller,
+  Get,
+  Header,
+  HttpCode,
+  HttpStatus,
+  Param,
+} from "@nestjs/common";
+import { PrismaProvider } from "@/libs/providers";
+import { RetrieveUserPaymentHistoryProvider } from "./providers";
+import { RetrieveUserPaymentHistoryResponseDTO } from "./providers/retrieve-history/retrieve-history.dto";
 
 @Controller("payment-history")
-export class PaymentHistoryController {}
+export class PaymentHistoryController {
+  constructor(
+    private readonly prisma: PrismaProvider,
+    private readonly retrieveHistoryService: RetrieveUserPaymentHistoryProvider
+  ) {}
+
+  @Get("/:name")
+  @Header("Content-Type", "application/json")
+  @Header("Accept", "application/json")
+  @HttpCode(HttpStatus.OK)
+  public async retrieveHistories(
+    @Param("name") name: string
+  ): Promise<RetrieveUserPaymentHistoryResponseDTO> {
+    try {
+      return await this.retrieveHistoryService.retrievePaymentsHistories(name);
+    } finally {
+      await this.prisma.$disconnect();
+    }
+  }
+}
