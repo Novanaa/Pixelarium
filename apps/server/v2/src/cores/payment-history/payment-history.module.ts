beeae1484fd6fa98bdbc56d22782e9406c23f195
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-04-16 18:57:37 +0700

    Registered user payment history core providers

--- apps/server/v2/src/cores/payment-history/payment-history.module.ts
+++ apps/server/v2/src/cores/payment-history/payment-history.module.ts
@@ -1,7 +1,9 @@
+import providers from "./providers";
 import { Module } from "@nestjs/common";
 import { PaymentHistoryController } from "./payment-history.controller";
 
 @Module({
   controllers: [PaymentHistoryController],
+  providers,
 })
 export class PaymentHistoryModule {}
