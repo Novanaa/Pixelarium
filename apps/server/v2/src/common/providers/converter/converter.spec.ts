7d8d94bfaf8067838bcaff84217954033a93be83
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-04-17 20:49:14 +0700

    Added converted common provider unit-test

---
+++ apps/server/v2/src/common/providers/converter/converter.spec.ts
@@ -0,0 +1,25 @@
+import { Test, TestingModule } from "@nestjs/testing";
+import { ConverterProvider } from "./converter.provider";
+import { ErrorProvider } from "../error/error.provider";
+
+describe("Converter", () => {
+  let converter: ConverterProvider;
+
+  beforeAll(async () => {
+    const module: Awaited<TestingModule> = await Test.createTestingModule({
+      providers: [ConverterProvider, ErrorProvider],
+    }).compile();
+
+    converter = module.get<ConverterProvider>(ConverterProvider);
+  });
+
+  it("should be return a number data type", async () => {
+    expect(typeof (await converter.convertUSDToIDR(1))).toBe("number");
+  });
+  it("should be defined", async () => {
+    expect(typeof (await converter.convertUSDToIDR(1))).toBeDefined();
+  });
+  it("should be not NaN", async () => {
+    expect(typeof (await converter.convertUSDToIDR(1))).not.toBeNaN();
+  });
+});
