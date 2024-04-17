7c06e44b7511189f1f6cb6fa81263ae81a26194e
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-04-17 21:20:14 +0700

    Added create payment history param interface

--- apps/server/v2/src/cores/payment-history/providers/add-history/add-history.d.ts
+++ apps/server/v2/src/cores/payment-history/providers/add-history/add-history.d.ts
@@ -1,4 +1,13 @@
+import { Prisma } from "@prisma/client";
+import { AddUserPaymentHistoryRequestDTO } from "./add-history.dto";
+
 export interface OrderAmount {
   IDR: number;
   USD: number;
+}
+
+export interface createPaymentHistoryParam {
+  userId: string;
+  amount: OrderAmount | Prisma.InputJsonValue;
+  payload: AddUserPaymentHistoryRequestDTO;
 }
