7db5881f874ebc0201f4ca94e91b8d7b2fe4dbc1
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-04-17 20:37:43 +0700

    Added currency converted from usd to idr

--- apps/server/v2/src/common/providers/converter/converter.provider.ts
+++ apps/server/v2/src/common/providers/converter/converter.provider.ts
@@ -1,4 +1,24 @@
-import { Injectable } from "@nestjs/common";
+import * as axios from "axios";
+import { Injectable, InternalServerErrorException } from "@nestjs/common";
+import { CurrencyExchange } from "./converter";
+import { ErrorProvider } from "../error/error.provider";
 
 @Injectable()
-export class ConverterProvider {}
+export class ConverterProvider {
+  constructor(private readonly errorService: ErrorProvider) {}
+
+  public async convertUSDToIDR(amount: number): Promise<number> {
+    try {
+      const endpoint: string =
+        "https://api.frankfurter.app/latest?from=USD&to=IDR&amount=" + amount;
+      const currency: Awaited<axios.AxiosResponse<CurrencyExchange>> =
+        await axios.default.get(endpoint);
+
+      return currency.data.rates.IDR;
+    } catch (err) {
+      throw new InternalServerErrorException(
+        this.errorService.internalServerError()
+      );
+    }
+  }
+}
