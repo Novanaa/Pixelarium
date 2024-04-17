62e00f70728127f7566e78ed4e71bff9ab1a83a7
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-04-17 12:37:47 +0700

    Added add new user payment history api endpoint handler

--- apps/server/v2/src/cores/payment-history/payment-history.controller.ts
+++ apps/server/v2/src/cores/payment-history/payment-history.controller.ts
@@ -1,20 +1,27 @@
 import {
+  Body,
   Controller,
   Get,
   Header,
   HttpCode,
   HttpStatus,
   Param,
+  Post,
 } from "@nestjs/common";
 import { PrismaProvider } from "@/libs/providers";
-import { RetrieveUserPaymentHistoryProvider } from "./providers";
+import {
+  AddUserPaymentHistoryProvider,
+  RetrieveUserPaymentHistoryProvider,
+} from "./providers";
 import { RetrieveUserPaymentHistoryResponseDTO } from "./providers/retrieve-history/retrieve-history.dto";
+import { AddUserPaymentHistoryRequestDTO } from "./providers/add-history/add-history.dto";
 
 @Controller("payment-history")
 export class PaymentHistoryController {
   constructor(
     private readonly prisma: PrismaProvider,
-    private readonly retrieveHistoryService: RetrieveUserPaymentHistoryProvider
+    private readonly retrieveHistoryService: RetrieveUserPaymentHistoryProvider,
+    private readonly addUserPaymentHistoryService: AddUserPaymentHistoryProvider
   ) {}
 
   @Get("/:name")
@@ -26,6 +33,21 @@ export class PaymentHistoryController {
   ): Promise<RetrieveUserPaymentHistoryResponseDTO> {
     try {
       return await this.retrieveHistoryService.retrievePaymentsHistories(name);
+    } finally {
+      await this.prisma.$disconnect();
+    }
+  }
+
+  @Post("/:name")
+  @Header("Content-Type", "application/json")
+  @Header("Accept", "application/json")
+  @HttpCode(HttpStatus.CREATED)
+  public async addUserPaymentHistory(
+    @Param("name") name: string,
+    @Body() body: AddUserPaymentHistoryRequestDTO
+  ) {
+    try {
+      return await this.addUserPaymentHistoryService.addHistory(name, body);
     } finally {
       await this.prisma.$disconnect();
     }
