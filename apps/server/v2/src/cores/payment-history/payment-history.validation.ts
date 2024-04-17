04bdce215d0dd120103834ea58b55e30e2060d53
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-04-17 13:09:00 +0700

    Fixed invalid joi.valid() arguments

--- apps/server/v2/src/cores/payment-history/payment-history.validation.ts
+++ apps/server/v2/src/cores/payment-history/payment-history.validation.ts
@@ -7,11 +7,11 @@ import { SubscriptionConstant } from "@/constant/subscription.constant";
 export class PaymentHistoryValidation {
   public static ADD_PAYMENT_HISTORY: joi.ObjectSchema<AddUserPaymentHistoryRequestDTO> =
     joi.object({
-      plan: joi.string().valid(SubscriptionConstant.USERPLAN),
+      plan: joi.string().valid(...SubscriptionConstant.USERPLAN),
       interval_count: joi.number(),
       order_date: joi.date(),
       order_id: joi.string(),
-      status: joi.string().valid(SubscriptionConstant.ORDER_STATUS),
+      status: joi.string().valid(...SubscriptionConstant.ORDER_STATUS),
       amount: joi.object({
         USD: joi.number(),
         IDR: joi.number(),
