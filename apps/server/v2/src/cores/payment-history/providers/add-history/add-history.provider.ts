a80620561f6880904e94760981618cdfa2863bd8
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-04-17 13:15:04 +0700

    Added add history method

--- apps/server/v2/src/cores/payment-history/providers/add-history/add-history.provider.ts
+++ apps/server/v2/src/cores/payment-history/providers/add-history/add-history.provider.ts
@@ -1,4 +1,20 @@
+import { PrismaProvider } from "@/libs/providers";
 import { Injectable } from "@nestjs/common";
+import {
+  AddUserPaymentHistoryRequestDTO,
+  AddUserPaymentHistoryResponseDTO,
+} from "./add-history.dto";
 
 @Injectable()
-export class AddUserPaymentHistoryProvider {}
+export class AddUserPaymentHistoryProvider {
+  constructor(private readonly prisma: PrismaProvider) {}
+
+  public async addHistory(
+    name: string,
+    body: AddUserPaymentHistoryRequestDTO
+  ): Promise<AddUserPaymentHistoryResponseDTO> {
+    console.log(body);
+    console.log(name);
+    return null;
+  }
+}
