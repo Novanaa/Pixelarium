220eaa453a15379bd427678a189635f3c155d674
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-04-17 17:54:24 +0700

    Added payment history order id pattern

--- apps/server/v2/src/constant/subscription.constant.ts
+++ apps/server/v2/src/constant/subscription.constant.ts
@@ -15,4 +15,7 @@ export class SubscriptionConstant {
     "Pending",
     "Success",
   ];
+
+  public static ORDERID_PATTERN: RegExp =
+    /plxmoid-[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}/;
 }
