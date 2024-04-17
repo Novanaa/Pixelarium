5a016699d60c0f121e02513b129adb997a1fbf68
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-04-17 21:26:10 +0700

    Added add user payment history api core logic

--- apps/server/v2/src/cores/payment-history/providers/add-history/add-history.provider.ts
+++ apps/server/v2/src/cores/payment-history/providers/add-history/add-history.provider.ts
@@ -1,20 +1,79 @@
-import { PrismaProvider } from "@/libs/providers";
-import { Injectable } from "@nestjs/common";
 import {
   AddUserPaymentHistoryRequestDTO,
   AddUserPaymentHistoryResponseDTO,
 } from "./add-history.dto";
+import { PrismaProvider } from "@/libs/providers";
+import { ForbiddenException, HttpStatus, Injectable } from "@nestjs/common";
+import { RetrieveUserProvider } from "@/cores/user/providers";
+import { ConverterProvider, ErrorProvider } from "@/common/providers";
+import { RetrieveUserResponseDto } from "@/cores/user/providers/retrieve-user/retrieve-user.dto";
+import { SubscriptionConstant } from "@/constant/subscription.constant";
+import { PaymentsHistory, Prisma } from "@prisma/client";
+import { OrderAmount, createPaymentHistoryParam } from "./add-history";
 
 @Injectable()
 export class AddUserPaymentHistoryProvider {
-  constructor(private readonly prisma: PrismaProvider) {}
+  constructor(
+    private readonly prisma: PrismaProvider,
+    private readonly retrieveUserService: RetrieveUserProvider,
+    private readonly converterService: ConverterProvider,
+    private readonly errorService: ErrorProvider
+  ) {}
+
+  public async createPaymentHistory(
+    param: createPaymentHistoryParam
+  ): Promise<PaymentsHistory> {
+    return await this.prisma.paymentsHistory.create({
+      data: {
+        ...param.payload,
+        user: { connect: { id: param.userId } },
+        amount: param.amount as Prisma.InputJsonValue,
+      },
+    });
+  }
+
+  public async isHistoryAlreadyCreated(
+    userId: string,
+    orderId: string
+  ): Promise<boolean> {
+    return !(await this.prisma.paymentsHistory.count({
+      where: { user_id: userId, order_id: orderId },
+    }));
+  }
 
   public async addHistory(
     name: string,
     body: AddUserPaymentHistoryRequestDTO
   ): Promise<AddUserPaymentHistoryResponseDTO> {
-    console.log(body);
-    console.log(name);
-    return null;
+    const { user }: Awaited<RetrieveUserResponseDto> =
+      await this.retrieveUserService.retrieveSingleUser(name);
+
+    if (this.isHistoryAlreadyCreated(user.id, body.order_id))
+      throw new ForbiddenException(
+        this.errorService.forbidden(
+          "The payment history order id cannot used twice"
+        )
+      );
+
+    const usd: number =
+      SubscriptionConstant.PLAN_PRICES[body.plan.toLowerCase()] *
+      body.interval_count;
+    const convertedCurrency: Awaited<number> =
+      await this.converterService.convertUSDToIDR(usd);
+
+    const amount: OrderAmount = { IDR: convertedCurrency, USD: usd };
+    const paymentHistory: Awaited<PaymentsHistory> =
+      await this.createPaymentHistory({
+        amount,
+        payload: body,
+        userId: user.id,
+      });
+
+    return {
+      history: paymentHistory,
+      owner: user,
+      code: HttpStatus.CREATED,
+      status: "OK",
+    };
   }
 }
