57886d72ea82907f96a15d12be3ab45ca8bc6bf4
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-04-17 13:14:43 +0700

    Added required options to add payment history validation

--- apps/server/v2/src/cores/payment-history/payment-history.validation.ts
+++ apps/server/v2/src/cores/payment-history/payment-history.validation.ts
@@ -6,15 +6,17 @@ import { SubscriptionConstant } from "@/constant/subscription.constant";
 @Injectable()
 export class PaymentHistoryValidation {
   public static ADD_PAYMENT_HISTORY: joi.ObjectSchema<AddUserPaymentHistoryRequestDTO> =
-    joi.object({
-      plan: joi.string().valid(...SubscriptionConstant.USERPLAN),
-      interval_count: joi.number(),
-      order_date: joi.date(),
-      order_id: joi.string(),
-      status: joi.string().valid(...SubscriptionConstant.ORDER_STATUS),
-      amount: joi.object({
-        USD: joi.number(),
-        IDR: joi.number(),
-      }),
-    });
+    joi
+      .object({
+        plan: joi.string().valid(...SubscriptionConstant.USERPLAN),
+        interval_count: joi.number(),
+        order_date: joi.date(),
+        order_id: joi.string(),
+        status: joi.string().valid(...SubscriptionConstant.ORDER_STATUS),
+        amount: joi.object({
+          USD: joi.number(),
+          IDR: joi.number(),
+        }),
+      })
+      .required();
 }
