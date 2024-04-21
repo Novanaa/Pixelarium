ac9843391a87fda62a9c014428bc203abde4d42f
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-04-21 18:42:59 +0700

    Added find unique by user id method

--- apps/server/v2/src/cores/gallery/gallery.repository.ts
+++ apps/server/v2/src/cores/gallery/gallery.repository.ts
@@ -1,4 +1,14 @@
+import { PrismaProvider } from "@/libs/providers";
 import { Injectable } from "@nestjs/common";
+import { Gallery } from "@prisma/client";
 
 @Injectable()
-export class GalleryRepository {}
+export class GalleryRepository {
+  constructor(private readonly prisma: PrismaProvider) {}
+
+  public async findUniqueByUserId(userId: string): Promise<Gallery> {
+    return await this.prisma.gallery.findUnique({
+      where: { user_id: userId },
+    });
+  }
+}
