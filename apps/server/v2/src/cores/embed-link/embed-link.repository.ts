7d597c543f3a1336d67db981197299f795990797
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-04-21 22:15:02 +0700

    Added new find embed link by picture id method

--- apps/server/v2/src/cores/embed-link/embed-link.repository.ts
+++ apps/server/v2/src/cores/embed-link/embed-link.repository.ts
@@ -1,7 +1,16 @@
 import { PrismaProvider } from "@/libs/providers";
 import { Injectable } from "@nestjs/common";
+import { EmbedLinks } from "@prisma/client";
 
 @Injectable()
 export class EmbedLinkRepository {
   constructor(private readonly prisma: PrismaProvider) {}
+
+  public async findEmbedLinkByPictureId(
+    pictureId: string
+  ): Promise<EmbedLinks> {
+    return await this.prisma.embedLinks.findUnique({
+      where: { picture_id: pictureId },
+    });
+  }
 }
