ad4dd798beb0c3efae411f7c340d99a1735e538e
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-04-21 21:37:38 +0700

    Registered favorite picture repository

--- apps/server/v2/src/cores/favorite-picture/favorite-picture.module.ts
+++ apps/server/v2/src/cores/favorite-picture/favorite-picture.module.ts
@@ -3,10 +3,11 @@ import { Module } from "@nestjs/common";
 import { FavoritePictureController } from "./favorite-picture.controller";
 import { UserModule } from "../user/user.module";
 import { PictureModule } from "../picture/picture.module";
+import { FavoritePictureRepository } from "./favorite-picture.repository";
 
 @Module({
   imports: [UserModule, PictureModule],
   controllers: [FavoritePictureController],
-  providers,
+  providers: [...providers, FavoritePictureRepository],
 })
 export class FavoritePictureModule {}
