eb689644b1035c7d09c70068f6baa415445a0373
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-04-21 17:03:48 +0700

    Added payment history repository

--- apps/server/v2/src/cores/payment-history/payment-history.repository.ts
+++ apps/server/v2/src/cores/payment-history/payment-history.repository.ts
@@ -1,4 +1,25 @@
+import { PrismaProvider } from "@/libs/providers";
 import { Injectable } from "@nestjs/common";
+import { PaymentsHistory } from "@prisma/client";
 
 @Injectable()
-export class PaymentHistoryRepository {}
+export class PaymentHistoryRepository {
+  constructor(private readonly prisma: PrismaProvider) {}
+
+  public async findByOrderIdAndUserId(
+    userId: string,
+    orderId: string
+  ): Promise<PaymentsHistory> {
+    return await this.prisma.paymentsHistory.findUnique({
+      where: { order_id: orderId, user_id: userId },
+    });
+  }
+
+  public async findManyByUserId(
+    userId: string
+  ): Promise<Array<PaymentsHistory>> {
+    return await this.prisma.paymentsHistory.findMany({
+      where: { user_id: userId },
+    });
+  }
+}
