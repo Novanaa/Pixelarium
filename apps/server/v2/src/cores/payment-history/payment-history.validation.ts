5f6d7c0392311a34a8e1feaaaf07c03e5506105b
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-04-21 16:35:59 +0700

    Added update payment history validation schema

--- apps/server/v2/src/cores/payment-history/payment-history.validation.ts
+++ apps/server/v2/src/cores/payment-history/payment-history.validation.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@nestjs/common";
 import * as joi from "joi";
 import { AddUserPaymentHistoryRequestDTO } from "./providers/add-history/add-history.dto";
 import { SubscriptionConstant } from "@/constant/subscription.constant";
+import { UpdatePaymentHistoryRequestDTO } from "./providers/update-history/update-history.dto";
 
 @Injectable()
 export class PaymentHistoryValidation {
@@ -21,5 +22,13 @@ export class PaymentHistoryValidation {
         .string()
         .valid(...SubscriptionConstant.ORDER_STATUS)
         .default("Pending"),
+    });
+
+  public static UPDATE_HISTORY: joi.ObjectSchema<UpdatePaymentHistoryRequestDTO> =
+    joi.object({
+      order_status: joi
+        .string()
+        .required()
+        .valid(...SubscriptionConstant.ORDER_STATUS),
     });
 }
