245c30e3497c2ee22b8dc8a6d520b9142896b4c5
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-04-22 13:16:39 +0700

    Added user repository getter method

--- apps/server/v2/src/cores/user/user.repository.ts
+++ apps/server/v2/src/cores/user/user.repository.ts
@@ -1,7 +1,16 @@
 import { PrismaProvider } from "@/libs/providers";
 import { Injectable } from "@nestjs/common";
+import { User } from "@prisma/client";
 
 @Injectable()
 export class UserRepository {
   constructor(private readonly prisma: PrismaProvider) {}
+
+  public async findByName(name: string): Promise<User> {
+    return await this.prisma.user.findUnique({ where: { name } });
+  }
+
+  public async findByOriginCode(origin_code: number): Promise<User> {
+    return await this.prisma.user.findUnique({ where: { origin_code } });
+  }
 }
