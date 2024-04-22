f989432e595a40db6f7d218df9414b904ae1299d
Nvaa <130564362+ItsNvaa@users.noreply.github.com>
2024-04-22 16:03:16 +0700

    [PIX-282] Implement Repository Pattern on User Authentication Feature (#675)
    * Added authentication repository
    
    * Registered authentication repository

--- apps/server/v2/src/cores/auth/auth.module.ts
+++ apps/server/v2/src/cores/auth/auth.module.ts
@@ -3,10 +3,11 @@ import { Module } from "@nestjs/common";
 import { AuthController } from "./auth.controller";
 import { UserModule } from "../user/user.module";
 import { ClientKeysModule } from "../client-keys/client-keys.module";
+import { AuthRepository } from "./auth.repository";
 
 @Module({
   imports: [UserModule, ClientKeysModule],
   controllers: [AuthController],
-  providers,
+  providers: [...providers, AuthRepository],
 })
 export class AuthModule {}
