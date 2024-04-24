e0e51d4a9429c27dc9296dd6597021b0957381e6
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-04-24 18:52:00 +0700

    Added a few scenarion to add user payment history end-to-end test

--- apps/server/v2/src/cores/payment-history/test/payment-history.e2e-spec.ts
+++ apps/server/v2/src/cores/payment-history/test/payment-history.e2e-spec.ts
@@ -10,13 +10,15 @@ import { HttpStatus, INestApplication } from "@nestjs/common";
 import * as supertest from "supertest";
 import { ResponseError } from "@/model/error.model";
 import { ErrorProvider } from "@/common/providers";
-import { PaymentsHistory, User } from "@prisma/client";
+import { PaymentsHistory, User, UserPlan } from "@prisma/client";
 import { RetrieveUserPaymentHistoryResponseDTO } from "../providers/retrieve-history/retrieve-history.dto";
 import {
   UpdatePaymentHistoryRequestDTO,
   UpdatePaymentHistoryResponseDTO,
 } from "../providers/update-history/update-history.dto";
 import * as falso from "@ngneat/falso";
+import { AddUserPaymentHistoryResponseDTO } from "../providers/add-history/add-history.dto";
+import { SubscriptionConstant } from "@/constant/subscription.constant";
 
 describe("PaymentHistoryController (e2e)", () => {
   let app: INestApplication;
@@ -332,6 +334,430 @@ describe("PaymentHistoryController (e2e)", () => {
       const body: ResponseError = response.body as ResponseError;
 
       expect(body.status).toBe("KO");
+    });
+    it("status response should be 'KO' if the request payload user plan is invalid data type", async () => {
+      const response: Awaited<supertest.Request | supertest.Response> =
+        await supertest(app.getHttpServer())
+          .post("/payment-history/0")
+          .send({
+            plan: 1,
+            order_date: new Date(),
+            order_id: "plxmoid-" + falso.randUuid(),
+            test: "test",
+          });
+      const body: ResponseError = response.body as ResponseError;
+
+      expect(body.status).toBe("KO");
+    });
+    it("should be return 400 status code if the request payload user plan is invalid data type", async () => {
+      const response: Awaited<supertest.Request | supertest.Response> =
+        await supertest(app.getHttpServer())
+          .post("/payment-history/0")
+          .send({
+            plan: 1,
+            order_date: new Date(),
+            order_id: "plxmoid-" + falso.randUuid(),
+            test: "test",
+          });
+      const body: ResponseError = response.body as ResponseError;
+
+      expect(body.code).toBe(HttpStatus.BAD_REQUEST);
+      expect(response.status).toBe(HttpStatus.BAD_REQUEST);
+    });
+    it("should be throw bad request exception if the request payload user plan is invalid data type", async () => {
+      const response: Awaited<supertest.Request | supertest.Response> =
+        await supertest(app.getHttpServer())
+          .post("/payment-history/0")
+          .send({
+            plan: 1,
+            order_date: new Date(),
+            order_id: "plxmoid-" + falso.randUuid(),
+            test: "test",
+          });
+      const body: ResponseError = response.body as ResponseError;
+
+      expect(body).toEqual(
+        error.badRequest("plan must be one of [Diamond, Gold, Netherite]")
+      );
+    });
+    it("should be throw bad request exception if the request payload order id is invalid data type", async () => {
+      const response: Awaited<supertest.Request | supertest.Response> =
+        await supertest(app.getHttpServer()).post("/payment-history/0").send({
+          plan: "Gold",
+          order_date: new Date(),
+          order_id: 1,
+          test: "test",
+        });
+      const body: ResponseError = response.body as ResponseError;
+
+      expect(body).toEqual(error.badRequest("order_id must be a string"));
+    });
+    it("should be return 400 status code if the request payload order id is invalid data type", async () => {
+      const response: Awaited<supertest.Request | supertest.Response> =
+        await supertest(app.getHttpServer()).post("/payment-history/0").send({
+          plan: "Gold",
+          order_date: new Date(),
+          order_id: 1,
+          test: "test",
+        });
+      const body: ResponseError = response.body as ResponseError;
+
+      expect(body.code).toBe(HttpStatus.BAD_REQUEST);
+      expect(response.status).toBe(HttpStatus.BAD_REQUEST);
+    });
+    it("status response should be 'KO' if the request payload order id is invalid data type", async () => {
+      const response: Awaited<supertest.Request | supertest.Response> =
+        await supertest(app.getHttpServer()).post("/payment-history/0").send({
+          plan: "Gold",
+          order_date: new Date(),
+          order_id: 1,
+          test: "test",
+        });
+      const body: ResponseError = response.body as ResponseError;
+
+      expect(body.code).toBe(HttpStatus.BAD_REQUEST);
+      expect(response.status).toBe(HttpStatus.BAD_REQUEST);
+    });
+    it("should be throw bad request exception if the request payload order id is invalid", async () => {
+      const response: Awaited<supertest.Request | supertest.Response> =
+        await supertest(app.getHttpServer()).post("/payment-history/0").send({
+          plan: "Gold",
+          order_date: new Date(),
+          order_id: "test",
+        });
+      const body: ResponseError = response.body as ResponseError;
+
+      expect(body).toEqual(
+        error.badRequest(
+          "order_id with value test fails to match the required pattern: /plxmoid-[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}/"
+        )
+      );
+    });
+    it("should be return 400 status code if the request payload order id is invalid", async () => {
+      const response: Awaited<supertest.Request | supertest.Response> =
+        await supertest(app.getHttpServer()).post("/payment-history/0").send({
+          plan: "Gold",
+          order_date: new Date(),
+          order_id: "test",
+        });
+      const body: ResponseError = response.body as ResponseError;
+
+      expect(body.code).toBe(HttpStatus.BAD_REQUEST);
+      expect(response.status).toBe(HttpStatus.BAD_REQUEST);
+    });
+    it("status response should be 'KO' if the request payload order id is invalid", async () => {
+      const response: Awaited<supertest.Request | supertest.Response> =
+        await supertest(app.getHttpServer()).post("/payment-history/0").send({
+          plan: "Gold",
+          order_date: new Date(),
+          order_id: "test",
+        });
+      const body: ResponseError = response.body as ResponseError;
+
+      expect(body.status).toBe("KO");
+    });
+    it("status response should be 'KO' if the request payload order date is invalid", async () => {
+      const response: Awaited<supertest.Request | supertest.Response> =
+        await supertest(app.getHttpServer()).post("/payment-history/0").send({
+          plan: "Gold",
+          order_date: "test",
+          order_id: "test",
+        });
+      const body: ResponseError = response.body as ResponseError;
+
+      expect(body.status).toBe("KO");
+    });
+    it("should be return 400 status code if the request payload order date is invalid", async () => {
+      const response: Awaited<supertest.Request | supertest.Response> =
+        await supertest(app.getHttpServer()).post("/payment-history/0").send({
+          plan: "Gold",
+          order_date: "test",
+          order_id: "test",
+        });
+      const body: ResponseError = response.body as ResponseError;
+
+      expect(body.code).toBe(HttpStatus.BAD_REQUEST);
+      expect(response.status).toBe(HttpStatus.BAD_REQUEST);
+    });
+    it("should be throw bad request exception if the request payload order date is invalid", async () => {
+      const response: Awaited<supertest.Request | supertest.Response> =
+        await supertest(app.getHttpServer()).post("/payment-history/0").send({
+          plan: "Gold",
+          order_date: "test",
+          order_id: "test",
+        });
+      const body: ResponseError = response.body as ResponseError;
+
+      expect(body).toEqual(error.badRequest("order_date must be a valid date"));
+    });
+    it("should be throw not found exception if the user doesn't exist", async () => {
+      const response: Awaited<supertest.Request | supertest.Response> =
+        await supertest(app.getHttpServer())
+          .post("/payment-history/0")
+          .send({
+            plan: "Gold",
+            order_date: new Date(),
+            order_id: "plxmoid-" + falso.randUuid(),
+          });
+      const body: ResponseError = response.body as ResponseError;
+
+      expect(body).toEqual(error.notFound());
+    });
+    it("should be return 404 status code if the user doesn't exist", async () => {
+      const response: Awaited<supertest.Request | supertest.Response> =
+        await supertest(app.getHttpServer())
+          .post("/payment-history/0")
+          .send({
+            plan: "Gold",
+            order_date: new Date(),
+            order_id: "plxmoid-" + falso.randUuid(),
+          });
+      const body: ResponseError = response.body as ResponseError;
+
+      expect(body.code).toBe(HttpStatus.NOT_FOUND);
+      expect(response.status).toBe(HttpStatus.NOT_FOUND);
+    });
+    it("status response should be 'KO' if the user doesn't exist", async () => {
+      const response: Awaited<supertest.Request | supertest.Response> =
+        await supertest(app.getHttpServer())
+          .post("/payment-history/0")
+          .send({
+            plan: "Gold",
+            order_date: new Date(),
+            order_id: "plxmoid-" + falso.randUuid(),
+          });
+      const body: ResponseError = response.body as ResponseError;
+
+      expect(body.status).toBe("KO");
+    });
+    it("status response should be 'KO' if the payment history is created twice", async () => {
+      const user: Awaited<User> = await prisma.user.findFirst();
+      const paymentHistory: Awaited<PaymentsHistory> =
+        await prisma.paymentsHistory.findFirst();
+      const response: Awaited<supertest.Request | supertest.Response> =
+        await supertest(app.getHttpServer())
+          .post("/payment-history/" + user.name)
+          .send({
+            plan: "Gold",
+            order_date: new Date(),
+            order_id: paymentHistory.order_id,
+          });
+      const body: ResponseError = response.body as ResponseError;
+
+      expect(body.status).toBe("KO");
+    });
+    it("should be return 403 status code if the payment history is created twice", async () => {
+      const user: Awaited<User> = await prisma.user.findFirst();
+      const paymentHistory: Awaited<PaymentsHistory> =
+        await prisma.paymentsHistory.findFirst();
+      const response: Awaited<supertest.Request | supertest.Response> =
+        await supertest(app.getHttpServer())
+          .post("/payment-history/" + user.name)
+          .send({
+            plan: "Gold",
+            order_date: new Date(),
+            order_id: paymentHistory.order_id,
+          });
+      const body: ResponseError = response.body as ResponseError;
+
+      expect(body.code).toBe(HttpStatus.FORBIDDEN);
+      expect(response.status).toBe(HttpStatus.FORBIDDEN);
+    });
+    it("should be throw forbidden exception if the payment history is created twice", async () => {
+      const user: Awaited<User> = await prisma.user.findFirst();
+      const paymentHistory: Awaited<PaymentsHistory> =
+        await prisma.paymentsHistory.findFirst();
+      const response: Awaited<supertest.Request | supertest.Response> =
+        await supertest(app.getHttpServer())
+          .post("/payment-history/" + user.name)
+          .send({
+            plan: "Gold",
+            order_date: new Date(),
+            order_id: paymentHistory.order_id,
+          });
+      const body: ResponseError = response.body as ResponseError;
+
+      expect(body).toEqual(
+        error.forbidden("The payment history order id cannot used twice")
+      );
+    });
+    it("should be return 201 status code", async () => {
+      const user: Awaited<User> = await prisma.user.findFirst();
+      const response: Awaited<supertest.Request | supertest.Response> =
+        await supertest(app.getHttpServer())
+          .post("/payment-history/" + user.name)
+          .send({
+            plan: "Gold",
+            order_date: new Date(),
+            order_id: "plxmoid-" + falso.randUuid(),
+          });
+      const body: AddUserPaymentHistoryResponseDTO =
+        response.body as AddUserPaymentHistoryResponseDTO;
+
+      expect(body.code).toBe(HttpStatus.CREATED);
+      expect(response.status).toBe(HttpStatus.CREATED);
+    });
+    it("make sure it can accept application/json", async () => {
+      const user: Awaited<User> = await prisma.user.findFirst();
+      const response: Awaited<supertest.Request | supertest.Response> =
+        await supertest(app.getHttpServer())
+          .post("/payment-history/" + user.name)
+          .send({
+            plan: "Gold",
+            order_date: new Date(),
+            order_id: "plxmoid-" + falso.randUuid(),
+          })
+          .set("Content-Type", "application/json")
+          .set("Accept", "application/json");
+      const body: AddUserPaymentHistoryResponseDTO =
+        response.body as AddUserPaymentHistoryResponseDTO;
+
+      expect(body.code).toBe(HttpStatus.CREATED);
+      expect(response.status).toBe(HttpStatus.CREATED);
+    });
+    it("status response should be 'OK'", async () => {
+      const user: Awaited<User> = await prisma.user.findFirst();
+      const response: Awaited<supertest.Request | supertest.Response> =
+        await supertest(app.getHttpServer())
+          .post("/payment-history/" + user.name)
+          .send({
+            plan: "Gold",
+            order_date: new Date(),
+            order_id: "plxmoid-" + falso.randUuid(),
+          })
+          .set("Content-Type", "application/json")
+          .set("Accept", "application/json");
+      const body: AddUserPaymentHistoryResponseDTO =
+        response.body as AddUserPaymentHistoryResponseDTO;
+
+      expect(body.status).toBe("OK");
+    });
+    it("owner response data should be defined", async () => {
+      const user: Awaited<User> = await prisma.user.findFirst();
+      const response: Awaited<supertest.Request | supertest.Response> =
+        await supertest(app.getHttpServer())
+          .post("/payment-history/" + user.name)
+          .send({
+            plan: "Gold",
+            order_date: new Date(),
+            order_id: "plxmoid-" + falso.randUuid(),
+          })
+          .set("Content-Type", "application/json")
+          .set("Accept", "application/json");
+      const body: AddUserPaymentHistoryResponseDTO =
+        response.body as AddUserPaymentHistoryResponseDTO;
+
+      expect(body.owner).toBeDefined();
+    });
+    it("owner response data should be match to requested user", async () => {
+      const user: Awaited<User> = await prisma.user.findFirst();
+      const response: Awaited<supertest.Request | supertest.Response> =
+        await supertest(app.getHttpServer())
+          .post("/payment-history/" + user.name)
+          .send({
+            plan: "Gold",
+            order_date: new Date(),
+            order_id: "plxmoid-" + falso.randUuid(),
+          })
+          .set("Content-Type", "application/json")
+          .set("Accept", "application/json");
+      const body: AddUserPaymentHistoryResponseDTO =
+        response.body as AddUserPaymentHistoryResponseDTO;
+
+      delete user.email;
+      delete user.password;
+
+      expect(JSON.stringify(body.owner)).toMatch(JSON.stringify(user));
+    });
+    it("history response data should be defined", async () => {
+      const user: Awaited<User> = await prisma.user.findFirst();
+      const response: Awaited<supertest.Request | supertest.Response> =
+        await supertest(app.getHttpServer())
+          .post("/payment-history/" + user.name)
+          .send({
+            plan: "Gold",
+            order_date: new Date(),
+            order_id: "plxmoid-" + falso.randUuid(),
+          })
+          .set("Content-Type", "application/json")
+          .set("Accept", "application/json");
+      const body: AddUserPaymentHistoryResponseDTO =
+        response.body as AddUserPaymentHistoryResponseDTO;
+
+      expect(body.history).toBeDefined();
+    });
+    it("history.amount response data should be defined", async () => {
+      const user: Awaited<User> = await prisma.user.findFirst();
+      const response: Awaited<supertest.Request | supertest.Response> =
+        await supertest(app.getHttpServer())
+          .post("/payment-history/" + user.name)
+          .send({
+            plan: "Gold",
+            order_date: new Date(),
+            order_id: "plxmoid-" + falso.randUuid(),
+          })
+          .set("Content-Type", "application/json")
+          .set("Accept", "application/json");
+      const body: AddUserPaymentHistoryResponseDTO =
+        response.body as AddUserPaymentHistoryResponseDTO;
+
+      expect(body.history.amount).toBeDefined();
+    });
+    it("history.amount.USD response data should be defined", async () => {
+      const user: Awaited<User> = await prisma.user.findFirst();
+      const response: Awaited<supertest.Request | supertest.Response> =
+        await supertest(app.getHttpServer())
+          .post("/payment-history/" + user.name)
+          .send({
+            plan: "Gold",
+            order_date: new Date(),
+            order_id: "plxmoid-" + falso.randUuid(),
+          })
+          .set("Content-Type", "application/json")
+          .set("Accept", "application/json");
+      const body: AddUserPaymentHistoryResponseDTO =
+        response.body as AddUserPaymentHistoryResponseDTO;
+
+      expect(body.history.amount.USD).toBeDefined();
+    });
+    it("history.amount.IDR response data should be defined", async () => {
+      const user: Awaited<User> = await prisma.user.findFirst();
+      const response: Awaited<supertest.Request | supertest.Response> =
+        await supertest(app.getHttpServer())
+          .post("/payment-history/" + user.name)
+          .send({
+            plan: "Gold",
+            order_date: new Date(),
+            order_id: "plxmoid-" + falso.randUuid(),
+          })
+          .set("Content-Type", "application/json")
+          .set("Accept", "application/json");
+      const body: AddUserPaymentHistoryResponseDTO =
+        response.body as AddUserPaymentHistoryResponseDTO;
+
+      expect(body.history.amount.IDR).toBeDefined();
+    });
+    it("history.amount.USD response data should be correctly calculated", async () => {
+      const plan: UserPlan = "Gold";
+      const interval: number = 3;
+      const user: Awaited<User> = await prisma.user.findFirst();
+      const response: Awaited<supertest.Request | supertest.Response> =
+        await supertest(app.getHttpServer())
+          .post("/payment-history/" + user.name)
+          .send({
+            plan,
+            order_date: new Date(),
+            order_id: "plxmoid-" + falso.randUuid(),
+            interval_count: interval,
+          })
+          .set("Content-Type", "application/json")
+          .set("Accept", "application/json");
+      const body: AddUserPaymentHistoryResponseDTO =
+        response.body as AddUserPaymentHistoryResponseDTO;
+
+      expect(body.history.amount.USD).toEqual(
+        SubscriptionConstant.PLAN_PRICES[plan.toLowerCase()] * interval
+      );
     });
   });
 
