b57ee7f1a73f07abef4af1d5b09f96df209197d8
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-04-26 20:39:42 +0700

    Changed gallery field types

--- apps/server/v2/src/cores/picture/providers/delete-picture/delete-picture.dto.ts
+++ apps/server/v2/src/cores/picture/providers/delete-picture/delete-picture.dto.ts
@@ -1,11 +1,12 @@
-import { Gallery, Picture, User } from "@prisma/client";
+import { Picture, User } from "@prisma/client";
 import { WebResponse } from "@/model/app.model";
+import { UserGallery } from "@/model/app";
 
 export class DeletePictureResponseDTO extends WebResponse {
   deleted_picture: Picture;
 }
 
 export class RemovesPicturesResponseDTO extends WebResponse {
-  gallery: Gallery;
+  gallery: UserGallery;
   owner: User;
 }
