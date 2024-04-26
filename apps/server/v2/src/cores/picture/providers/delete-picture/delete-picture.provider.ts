6ae131467db53a3b6d9ab780509185263c794726
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-04-26 20:40:39 +0700

    Added removes user pictures api core logic

--- apps/server/v2/src/cores/picture/providers/delete-picture/delete-picture.provider.ts
+++ apps/server/v2/src/cores/picture/providers/delete-picture/delete-picture.provider.ts
@@ -6,12 +6,18 @@ import {
 } from "./delete-picture.dto";
 import { RetrieveUserPictureProvider } from "../retrieve-picture/retrieve-picture.provider";
 import { RetrieveUserPictureResponseDTO } from "../retrieve-picture/retrieve-picture.dto";
+import { RetrieveUserProvider } from "@/cores/user/providers";
+import { RetrieveUserResponseDto } from "@/cores/user/providers/retrieve-user/retrieve-user.dto";
+import { GalleryRepository } from "@/cores/gallery/gallery.repository";
+import { Gallery } from "@prisma/client";
 
 @Injectable()
 export class DeletePictureProvider {
   constructor(
     private readonly pictureRepo: PictureRepository,
-    private readonly retrievePictureService: RetrieveUserPictureProvider
+    private readonly retrievePictureService: RetrieveUserPictureProvider,
+    private readonly retrieveUserService: RetrieveUserProvider,
+    private readonly galleryRepo: GalleryRepository
   ) {}
 
   public async deletePicture(
@@ -32,6 +38,19 @@ export class DeletePictureProvider {
   public async RemovesPictures(
     name: string
   ): Promise<RemovesPicturesResponseDTO> {
-    return null;
+    const { user }: Awaited<RetrieveUserResponseDto> =
+      await this.retrieveUserService.retrieveSingleUser(name);
+
+    const userGallery: Awaited<Gallery> =
+      await this.galleryRepo.findUniqueByUserId(user.id);
+
+    await this.pictureRepo.removesPictures(userGallery.id);
+
+    return {
+      owner: user,
+      gallery: { ...userGallery, pictures: [] },
+      code: HttpStatus.OK,
+      status: "OK",
+    };
   }
 }
