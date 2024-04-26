a7defd5f5facf9eeb72fb26eaa5b9c329733da30
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-04-26 17:33:31 +0700

    Added delete picture method

--- apps/server/v2/src/cores/picture/providers/delete-picture/delete-picture.provider.ts
+++ apps/server/v2/src/cores/picture/providers/delete-picture/delete-picture.provider.ts
@@ -1,4 +1,12 @@
 import { Injectable } from "@nestjs/common";
+import { PictureRepository } from "../../picture.repository";
+import { DeletePictureResponseDTO } from "./delete-picture.dto";
 
 @Injectable()
-export class DeletePictureProvider {}
+export class DeletePictureProvider {
+  constructor(private readonly pictureRepo: PictureRepository) {}
+
+  public async deletePicture(): Promise<DeletePictureResponseDTO> {
+    return null;
+  }
+}
