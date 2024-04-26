954b5b2a2376cd606345fd3d332a2f4120395703
ItsNvaa <kadeknovacahayaputra@gmail.com>
2024-04-26 17:29:39 +0700

    Registered delete picture provider

--- apps/server/v2/src/cores/picture/providers/index.ts
+++ apps/server/v2/src/cores/picture/providers/index.ts
@@ -1,6 +1,15 @@
 import { RetrieveUserPictureProvider } from "./retrieve-picture/retrieve-picture.provider";
 import { DownloadUserPictureProvider } from "./download-picture/download-picture.provider";
+import { DeletePictureProvider } from "./delete-picture/delete-picture.provider";
 
-export { RetrieveUserPictureProvider, DownloadUserPictureProvider };
+export {
+  RetrieveUserPictureProvider,
+  DownloadUserPictureProvider,
+  DeletePictureProvider,
+};
 
-export default [RetrieveUserPictureProvider, DownloadUserPictureProvider];
+export default [
+  RetrieveUserPictureProvider,
+  DownloadUserPictureProvider,
+  DeletePictureProvider,
+];
